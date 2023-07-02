8f14e8131f5708e90050fecbeb992777e836b9c3
silva01 <silva20102@gmail.com>
2023-07-02 18:39:43 -0300

    Created command to Solicitation balance

--- src/main/java/com/example/api_de_lancamentos/domain/account/command/SolicitationBalanceCommand.java
+++ src/main/java/com/example/api_de_lancamentos/domain/account/command/SolicitationBalanceCommand.java
@@ -1,4 +1,13 @@
 package com.example.api_de_lancamentos.domain.account.command;
 
 public final class SolicitationBalanceCommand {
+    private final long accountNumber;
+
+    public SolicitationBalanceCommand(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
 }
