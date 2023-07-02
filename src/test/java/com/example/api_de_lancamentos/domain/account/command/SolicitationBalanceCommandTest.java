be9e1e5d7ad2d2b809a23e84f620c7ea2f60e60e
silva01 <silva20102@gmail.com>
2023-07-02 18:39:57 -0300

    Created unit tests

---
+++ src/test/java/com/example/api_de_lancamentos/domain/account/command/SolicitationBalanceCommandTest.java
@@ -0,0 +1,37 @@
+package com.example.api_de_lancamentos.domain.account.command;
+
+import com.example.api_de_lancamentos.domain.account.aggregate.Account;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.api_de_lancamentos.domain.account.object_value.BalanceResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+
+//@SpringBootTest(classes = {SolicitationBalanceCommand.class, Account.class})
+public class SolicitationBalanceCommandTest {
+    @Test
+    public void deve_retornar_saldo_da_conta_corrente() {
+
+        boolean isCredit = false;
+        SolicitationBalanceCommand solicitationBalanceCommand = new SolicitationBalanceCommand(4321111L);
+
+        Account account = new Account(new BigDecimal("3000.00"), new BigDecimal("5000.00"), 4321111L, "Daniel");
+        BalanceResponseDTO balance = account.solicitationBalanceCommand(solicitationBalanceCommand);
+
+        assertEquals(new BigDecimal("3000.00"), balance.getBalance());
+    }
+
+    @Test
+    void deve_retornar_saldo_da_conta_credito() {
+        boolean isCredit = true;
+        SolicitationBalanceCommand solicitationBalanceCommand = new SolicitationBalanceCommand(4321111L);
+
+        Account account = new Account(new BigDecimal("3000.00"), new BigDecimal("5000.00"), 4321111L, "Daniel");
+        BalanceResponseDTO balance = account.solicitationBalanceCommand(solicitationBalanceCommand);
+
+        assertEquals(new BigDecimal("5000.00"), balance.getCreditBalance());
+    }
+}
