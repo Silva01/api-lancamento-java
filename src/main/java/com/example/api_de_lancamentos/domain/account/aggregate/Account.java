35dac928665054d6f70541f283153872696fac67
silva01 <silva20102@gmail.com>
2023-07-02 18:38:49 -0300

    Created aggregate of the account

--- src/main/java/com/example/api_de_lancamentos/domain/account/aggregate/Account.java
+++ src/main/java/com/example/api_de_lancamentos/domain/account/aggregate/Account.java
@@ -1,4 +1,36 @@
 package com.example.api_de_lancamentos.domain.account.aggregate;
 
-public class Account {
+import com.example.api_de_lancamentos.domain.account.command.SolicitationBalanceCommand;
+import com.example.api_de_lancamentos.domain.account.object_value.BalanceResponseDTO;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public final class Account {
+
+    private final BigDecimal balance;
+    private final BigDecimal creditBalance;
+    private final long accountNumber;
+    private final String name;
+    private final UUID id;
+
+    public Account(BigDecimal balance, BigDecimal creditBalance, long accountNumber, String name) {
+        this.balance = balance;
+        this.creditBalance = creditBalance;
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.id = UUID.randomUUID();
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public BigDecimal getCreditBalance() {
+        return creditBalance;
+    }
+
+    public BalanceResponseDTO solicitationBalanceCommand(SolicitationBalanceCommand solicitationBalanceCommand) {
+        return new BalanceResponseDTO(this.creditBalance, this.balance);
+    }
 }
