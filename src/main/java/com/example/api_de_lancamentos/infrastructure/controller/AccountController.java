22007c84f52f794352edd250f247ba33ff068426
silva01 <silva20102@gmail.com>
2023-07-04 19:42:38 -0300

    Removed imports not used

--- src/main/java/com/example/api_de_lancamentos/infrastructure/controller/AccountController.java
+++ src/main/java/com/example/api_de_lancamentos/infrastructure/controller/AccountController.java
@@ -12,8 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequestMapping("/account")
 public class AccountController {
