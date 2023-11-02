2a6b1c64eeb4702485c3f13aba9a65ca8fb0e368
silva01 <silva20102@gmail.com>
2023-11-02 07:57:51 -0300

    Created Aggregate for account and transaction

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/Account.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/Account.java
@@ -3,9 +3,37 @@ package br.net.silva.daniel.entity;
 import br.net.silva.daniel.interfaces.AggregateRoot;
 import br.net.silva.daniel.validation.Validation;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account extends Validation implements AggregateRoot {
+
+    private final Integer number;
+    private final Integer bankAgencyNumber;
+    private final BigDecimal balance;
+    private final String password;
+    private boolean active;
+    private final String cpf;
+    private final List<Transaction> transactions;
+
+    public Account(Integer number, Integer bankAgencyNumber, BigDecimal balance, String password, boolean active, String cpf, List<Transaction> transactions) {
+        this.number = number;
+        this.bankAgencyNumber = bankAgencyNumber;
+        this.balance = balance;
+        this.password = password;
+        this.active = active;
+        this.cpf = cpf;
+        this.transactions = transactions;
+        validate();
+    }
+
+    public Account(Integer number, Integer bankAgencyNumber, String password, String cpf) {
+        this(number, bankAgencyNumber, BigDecimal.ZERO, password, true, cpf, new ArrayList<>());
+    }
+
     @Override
     public void validate() {
-
+        // criar método de validação para numeros
     }
 }
---
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/Transaction.java
@@ -0,0 +1,4 @@
+package br.net.silva.daniel.entity;
+
+public class Transaction {
+}
