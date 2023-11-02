f35f3d55ae3ca293be5d7c948508f6b54ff28d69
silva01 <silva20102@gmail.com>
2023-11-02 07:45:22 -0300

    Modified access modifies for methods

---
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/Account.java
@@ -0,0 +1,11 @@
+package br.net.silva.daniel.entity;
+
+import br.net.silva.daniel.interfaces.AggregateRoot;
+import br.net.silva.daniel.validation.Validation;
+
+public class Account extends Validation implements AggregateRoot {
+    @Override
+    public void validate() {
+
+    }
+}
--- core-module/shared-business-module/src/main/java/br/net/silva/daniel/validation/Validation.java
+++ core-module/shared-business-module/src/main/java/br/net/silva/daniel/validation/Validation.java
@@ -6,19 +6,19 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public abstract class Validation {
-    protected abstract void validate();
+    public abstract void validate();
 
-    public void validateAttributeNonNull(Object attribute, String messageError) {
+    protected void validateAttributeNonNull(Object attribute, String messageError) {
         ValidateUtils.isNotNull(attribute, messageError);
     }
 
-    public void validateAttributeNotEmpty(String attribute, String messageError) {
+    protected void validateAttributeNotEmpty(String attribute, String messageError) {
         ValidateUtils.isNotEmpty(attribute, messageError);
     }
-    public void validateAttributeNotNullAndNotEmpty(String attribute, String messageError) {
+    protected void validateAttributeNotNullAndNotEmpty(String attribute, String messageError) {
         ValidateUtils.isTextNotNullAndNotEmpty(attribute, messageError);
     }
-    public void validateAttributeLessThanZero(BigDecimal attribute, String messageError) {
+    protected void validateAttributeLessThanZero(BigDecimal attribute, String messageError) {
         ValidateUtils.isLessThanZero(attribute, messageError);
     }
 }
