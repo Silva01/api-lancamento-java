665b747689e1a2146ac79e78181c7b2e557f9aaf
silva01 <silva20102@gmail.com>
2023-11-02 07:17:19 -0300

    Created unit test for ValidateUtils

--- core-module/shared-business-module/src/main/java/br/net/silva/daniel/utils/ValidateUtils.java
+++ core-module/shared-business-module/src/main/java/br/net/silva/daniel/utils/ValidateUtils.java
@@ -1,12 +1,12 @@
 package br.net.silva.daniel.utils;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class ValidateUtils {
     public static void balance(BigDecimal balance, BigDecimal value) {
-        if (balance == null) {
-            throw new IllegalArgumentException("Balance is null");
-        }
+        Objects.requireNonNull(balance, "Balance is null");
+        Objects.requireNonNull(value, "Value is null");
 
         if (balance.compareTo(value) < 0) {
             throw new IllegalArgumentException("Balance is less than value");
--- core-module/shared-business-module/src/test/java/br/net/silva/daniel/utils/ValidateUtilsTest.java
+++ core-module/shared-business-module/src/test/java/br/net/silva/daniel/utils/ValidateUtilsTest.java
@@ -6,11 +6,42 @@ import java.math.BigDecimal;
 
 public class ValidateUtilsTest extends TestCase {
 
+    public void testShouldValidateBalance() {
+        ValidateUtils.balance(new BigDecimal(10), new BigDecimal(10));
+
+        // If no exception is thrown, the test passes
+        assertTrue(true);
+    }
+
     public void testShouldErrorValidateBalanceNull() {
         try {
             ValidateUtils.balance(null, new BigDecimal(10));
-        } catch (IllegalArgumentException e) {
+        } catch (NullPointerException e) {
             assertEquals("Balance is null", e.getMessage());
+        }
+    }
+
+    public void testShouldErrorValidateBalanceAndValueNull() {
+        try {
+            ValidateUtils.balance(null, null);
+        } catch (NullPointerException e) {
+            assertEquals("Balance is null", e.getMessage());
+        }
+    }
+
+    public void testShouldErrorValidateBalanceWithValueNull() {
+        try {
+            ValidateUtils.balance(new BigDecimal(10), null);
+        } catch (NullPointerException e) {
+            assertEquals("Value is null", e.getMessage());
+        }
+    }
+
+    public void testShouldErrorValidateBalanceLessThanValue() {
+        try {
+            ValidateUtils.balance(new BigDecimal(10), new BigDecimal(20));
+        } catch (IllegalArgumentException e) {
+            assertEquals("Balance is less than value", e.getMessage());
         }
     }
 }
