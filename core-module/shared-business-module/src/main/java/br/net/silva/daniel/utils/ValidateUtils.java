09dcb175235fac6d03980bff2059d772b8e2cc0d
silva01 <silva20102@gmail.com>
2023-11-02 07:31:27 -0300

    Added new methods for validate

--- core-module/shared-business-module/src/main/java/br/net/silva/daniel/utils/ValidateUtils.java
+++ core-module/shared-business-module/src/main/java/br/net/silva/daniel/utils/ValidateUtils.java
@@ -12,4 +12,25 @@ public class ValidateUtils {
             throw new IllegalArgumentException("Balance is less than value");
         }
     }
+
+    public static void isNotEmpty(String attribute, String messageError) {
+        if (attribute.isEmpty()) {
+            throw new IllegalArgumentException(messageError);
+        }
+    }
+
+    public static void isNotNull(Object attribute, String messageError) {
+        Objects.requireNonNull(attribute, messageError);
+    }
+
+    public static void isTextNotNullAndNotEmpty(String attribute, String messageError) {
+        isNotNull(attribute, messageError);
+        isNotEmpty(attribute, messageError);
+    }
+
+    public static void isLessThanZero(BigDecimal attribute, String messageError) {
+        if (attribute.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException(messageError);
+        }
+    }
 }
