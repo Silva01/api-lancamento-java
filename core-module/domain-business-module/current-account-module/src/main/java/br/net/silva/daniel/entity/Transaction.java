5fa52dfda713ce91b4e80b594fe2bb8e01017e96
silva01 <silva20102@gmail.com>
2023-11-21 07:14:52 -0300

    Created aggregate to transaction

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/Transaction.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/Transaction.java
@@ -1,4 +1,94 @@
 package br.net.silva.daniel.entity;
 
-public class Transaction {
+import br.net.silva.daniel.interfaces.Aggregate;
+import br.net.silva.daniel.validation.Validation;
+
+import java.math.BigDecimal;
+
+public class Transaction extends Validation implements Aggregate {
+
+    private final Long id;
+    private final String description;
+    private final BigDecimal price;
+    private final Integer quantity;
+    private final String type;
+    private final Integer originAccountNumber;
+    private final Integer destinationAccountNumber;
+    private final Long idempotencyId;
+    private String creditCardNumber;
+    private Integer creditCardCvv;
+
+    public Transaction(Long id, String description, BigDecimal price, Integer quantity, String type, Integer originAccountNumber, Integer destinationAccountNumber, Long idempotencyId, String creditCardNumber, Integer creditCardCvv) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.type = type;
+        this.originAccountNumber = originAccountNumber;
+        this.destinationAccountNumber = destinationAccountNumber;
+        this.idempotencyId = idempotencyId;
+        this.creditCardNumber = creditCardNumber;
+        this.creditCardCvv = creditCardCvv;
+        validate();
+    }
+
+    @Override
+    public void validate() {
+        validateAttributeNotNullAndNotEmpty(description, "Description is null or empty");
+        validateAttributeNonNull(price, "Price is null");
+        validateAttributeLessThanZero(price, "Price is less than zero");
+        validateAttributeNonNull(quantity, "Quantity is null");
+        validateAttributeLessThanZero(BigDecimal.valueOf(quantity), "Quantity is less than zero");
+        validateAttributeNotNullAndNotEmpty(type, "Type is null or empty");
+        validateAttributeNonNull(originAccountNumber, "Origin account number is null");
+        validateAttributeLessThanZero(BigDecimal.valueOf(originAccountNumber), "Origin account number is less than zero");
+        validateAttributeNonNull(destinationAccountNumber, "Destination account number is null");
+        validateAttributeLessThanZero(BigDecimal.valueOf(destinationAccountNumber), "Destination account number is less than zero");
+        validateAttributeNonNull(idempotencyId, "Idempotency id is null");
+
+        if (type.equals("CREDIT_CARD")) {
+            validateAttributeNotNullAndNotEmpty(creditCardNumber, "Credit card number is null or empty");
+            validateAttributeNotNullAndNotEmpty(creditCardNumber, "Credit card cvv is null or empty");
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Integer getOriginAccountNumber() {
+        return originAccountNumber;
+    }
+
+    public Integer getDestinationAccountNumber() {
+        return destinationAccountNumber;
+    }
+
+    public Long getIdempotencyId() {
+        return idempotencyId;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public Integer getCreditCardCvv() {
+        return creditCardCvv;
+    }
 }
