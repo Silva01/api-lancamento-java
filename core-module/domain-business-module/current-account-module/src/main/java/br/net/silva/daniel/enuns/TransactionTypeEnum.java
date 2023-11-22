2f8e65e04dc30c74d28f9854190960ac78687b76
silva01 <silva20102@gmail.com>
2023-11-22 07:06:43 -0300

    Added enum for type of transaction

---
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/enuns/TransactionTypeEnum.java
@@ -0,0 +1,15 @@
+package br.net.silva.daniel.enuns;
+
+public enum TransactionTypeEnum {
+    DEBIT("DEBIT"), CREDIT("CREDIT");
+
+    private final String value;
+
+    TransactionTypeEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
