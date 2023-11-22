941f456c25b582edcf932e93176283655f035d1a
silva01 <silva20102@gmail.com>
2023-11-22 07:33:37 -0300

    Created DTO to Account

---
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/dto/AccountDTO.java
@@ -0,0 +1,15 @@
+package br.net.silva.daniel.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record AccountDTO(
+        Integer number,
+        Integer bankAgencyNumber,
+        BigDecimal balance,
+        String password,
+        boolean active,
+        String cpf,
+        List<TransactionDTO>transactions
+) {
+}
