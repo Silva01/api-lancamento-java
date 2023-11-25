e469667e140210afde025e88e8dcef8a5cd2dbec
silva01 <silva20102@gmail.com>
2023-11-25 20:51:16 -0300

    Added DTO for Credit Card

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/dto/CreditCardDTO.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/dto/CreditCardDTO.java
@@ -1,4 +1,14 @@
 package br.net.silva.daniel.dto;
 
-public record CreditCardDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CreditCardDTO(
+        String number,
+        Integer secretKey,
+        String flag,
+        BigDecimal balance,
+        LocalDate expirationDate,
+        boolean active
+) {
 }
