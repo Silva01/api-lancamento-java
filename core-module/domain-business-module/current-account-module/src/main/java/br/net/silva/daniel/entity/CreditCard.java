aa05339112be5098c7053e4cd64aa01e77b36c75
silva01 <silva20102@gmail.com>
2023-11-28 07:29:37 -0300

    Refactoring aggregate at Credit Card

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/CreditCard.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/entity/CreditCard.java
@@ -47,7 +47,7 @@ public class CreditCard extends Validation implements Aggregate, IFactory<Credit
         validateAttributeNonNull(cvv, "Key secret of credit card cannot be null");
         validateAttributeEqualsZero(BigDecimal.valueOf(cvv), "Key secret of credit card cannot be zero");
         validateAttributeLessThanZero(BigDecimal.valueOf(cvv), "Key secret of credit card cannot be less than zero");
-        validateAttributeNonNull(flag, "This flag of credit card cannot be null or empty");
+        validateAttributeNonNull(flag, "This flag of credit card cannot be null");
         validateAttributeNonNull(balance, "This balance of credit card cannot be null");
         validateAttributeNonNull(expirationDate, "Date of expiration of credit card cannot be null");
     }
