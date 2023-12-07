1b6f26648176fe2c45fa987f4218c8522a59c522
silva01 <silva20102@gmail.com>
2023-12-07 07:23:54 -0300

    Reviewed class with sonar

--- core-module/shared-business-module/src/main/java/br/net/silva/daniel/utils/GeneratorRandomNumber.java
+++ core-module/shared-business-module/src/main/java/br/net/silva/daniel/utils/GeneratorRandomNumber.java
@@ -4,6 +4,10 @@ import java.util.Random;
 
 public class GeneratorRandomNumber {
 
+    private GeneratorRandomNumber() {
+        throw new IllegalStateException("Utility class");
+    }
+
     private static final Random RANDOM = new Random();
 
     public static Integer generate(int max) {
