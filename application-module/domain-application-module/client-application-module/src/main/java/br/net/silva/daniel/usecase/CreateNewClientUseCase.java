52850eadf61eadd6fee757ede5914bef15709f89
silva01 <silva20102@gmail.com>
2023-12-09 11:53:28 -0300

    Created a basic use case for create a new client

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewClientUseCase.java
@@ -1,4 +1,30 @@
 package br.net.silva.daniel.usecase;
 
-public class CreateNewClientUseCase {
+import br.net.silva.daniel.GenericException;
+import br.net.silva.daniel.dto.ClientDTO;
+import br.net.silva.daniel.entity.Client;
+import br.net.silva.daniel.exception.ExistsClientRegistredException;
+import br.net.silva.daniel.factory.CreateNewAddressFactory;
+import br.net.silva.daniel.factory.CreateNewClientFactory;
+import br.net.silva.daniel.repository.SaveRepository;
+import br.net.silva.daniel.template.BasicUseCase;
+
+public class CreateNewClientUseCase extends BasicUseCase<ClientDTO> {
+    private final SaveRepository<Client> saveRepository;
+    private final CreateNewClientFactory createNewClientFactory;
+
+    public CreateNewClientUseCase(SaveRepository<Client> saveRepository) {
+        this.saveRepository = saveRepository;
+        this.createNewClientFactory = new CreateNewClientFactory(new CreateNewAddressFactory());
+    }
+
+    @Override
+    public void exec(ClientDTO dto) throws GenericException {
+        try {
+            var aggregate = createNewClientFactory.create(dto);
+            saveRepository.save(aggregate);
+        } catch (Exception e) {
+            throw new ExistsClientRegistredException(e.getMessage());
+        }
+    }
 }
