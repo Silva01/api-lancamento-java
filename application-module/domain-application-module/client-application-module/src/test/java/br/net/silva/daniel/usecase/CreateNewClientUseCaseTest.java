3e0762c35682f3d7ff5ca167158dabba201fa843
silva01 <silva20102@gmail.com>
2023-12-09 23:34:56 -0300

    Added initial test

---
+++ application-module/domain-application-module/client-application-module/src/test/java/br/net/silva/daniel/usecase/CreateNewClientUseCaseTest.java
@@ -0,0 +1,11 @@
+package br.net.silva.daniel.usecase;
+
+import junit.framework.TestCase;
+
+public class CreateNewClientUseCaseTest extends TestCase {
+
+    public void testInit() {
+        assertTrue(true);
+    }
+
+}
---
+++ application-module/shared-application-module/src/test/java/br/net/silva/daniel/template/UseCaseTest.java
@@ -0,0 +1,10 @@
+package br.net.silva.daniel.template;
+
+import junit.framework.TestCase;
+
+public class UseCaseTest extends TestCase {
+        public void testInit() {
+            assertTrue(true);
+        }
+
+}
