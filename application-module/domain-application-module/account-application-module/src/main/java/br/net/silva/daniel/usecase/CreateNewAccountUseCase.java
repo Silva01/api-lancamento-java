93682ca10b54634149b5dc46e2b4a085ec2693da
silva01 <silva20102@gmail.com>
2023-12-11 07:02:17 -0300

    Created initial use case for module at application account

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewAccountUseCase.java
@@ -0,0 +1,12 @@
+package br.net.silva.daniel.usecase;
+
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.template.UseCase;
+
+public class CreateNewAccountUseCase extends UseCase<AccountDTO> {
+    @Override
+    public void exec(AccountDTO dto) throws GenericException {
+
+    }
+}
