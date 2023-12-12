946490655a71fdc35f9f03de7b32f24e8a9466ea
silva01 <silva20102@gmail.com>
2023-12-12 07:32:46 -0300

    Refactored interface for use case

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/template/UseCase.java
+++
@@ -1,8 +0,0 @@
-package br.net.silva.daniel.template;
-
-import br.net.silva.daniel.exception.GenericException;
-
-public abstract class UseCase<D> {
-
-    public abstract void exec(D dto) throws GenericException;
-}
