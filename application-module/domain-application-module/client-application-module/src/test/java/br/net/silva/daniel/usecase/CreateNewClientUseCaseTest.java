9c05a17ea1e44358ab94c8f6df1863165a47ddac
silva01 <silva20102@gmail.com>
2023-12-13 07:44:14 -0300

    Refactored unit test for use case for create a new Client

--- application-module/domain-application-module/client-application-module/src/test/java/br/net/silva/daniel/usecase/CreateNewClientUseCaseTest.java
+++ application-module/domain-application-module/client-application-module/src/test/java/br/net/silva/daniel/usecase/CreateNewClientUseCaseTest.java
@@ -1,9 +1,9 @@
 package br.net.silva.daniel.usecase;
 
-import br.net.silva.daniel.dto.AddressDTO;
-import br.net.silva.daniel.dto.ClientDTO;
+import br.net.silva.daniel.dto.AddressRequestDTO;
+import br.net.silva.daniel.dto.ClientRequestDTO;
 import br.net.silva.daniel.entity.Client;
-import br.net.silva.daniel.repository.SaveRepository;
+import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Address;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,24 +15,24 @@ import static org.mockito.Mockito.*;
 class CreateNewClientUseCaseTest {
 
     @InjectMocks
-    private SaveRepository<Client> saveRepository = mock(SaveRepository.class);
+    private Repository<Client> saveRepository = mock(Repository.class);
 
     private CreateNewClientUseCase createNewClientUseCase;
 
     @BeforeEach
     protected void setUp() {
-        when(saveRepository.save(any(Client.class))).thenReturn(createClient());
+        when(saveRepository.exec(any(Client.class))).thenReturn(createClient());
         createNewClientUseCase = new CreateNewClientUseCase(saveRepository);
     }
 
     @Test
     void testShouldCreateANewClientWithSuccess() throws Exception {
-        var address = new AddressDTO("Street 1", "1234", "Brazil", "Brasilia", "DF", "Brasilia", "12344-556");
-        var clientDto = new ClientDTO("1234", "00099988877", "Daniel", "61933334444", true, address);
+        var address = new AddressRequestDTO("Street 1", "1234", "Brazil", "Brasilia", "DF", "Brasilia", "12344-556");
+        var clientDto = new ClientRequestDTO("1234", "00099988877", "Daniel", "61933334444", true, 1234, address);
 
         createNewClientUseCase.exec(clientDto);
 
-        verify(saveRepository, times(1)).save(any(Client.class));
+        verify(saveRepository, times(1)).exec(any(Client.class));
     }
 
     private Client createClient() {
