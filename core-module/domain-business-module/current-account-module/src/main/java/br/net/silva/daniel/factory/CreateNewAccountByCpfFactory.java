1c6cb818521e17e4aa95d6fb11076b4a956bfd0b
silva01 <silva20102@gmail.com>
2023-12-14 07:01:07 -0300

    Added Factory for create new account with cpf

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/factory/CreateNewAccountByCpfFactory.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/factory/CreateNewAccountByCpfFactory.java
@@ -3,7 +3,7 @@ package br.net.silva.daniel.factory;
 import br.net.silva.daniel.dto.AccountDTO;
 import br.net.silva.daniel.entity.Account;
 
-public class CreateNewAccountFactory implements IFactoryAggregate<Account, AccountDTO> {
+public class CreateNewAccountByCpfFactory implements IFactoryAggregate<Account, AccountDTO> {
     @Override
     public Account create(AccountDTO accountDTO) {
         return new Account(accountDTO.bankAgencyNumber(), accountDTO.password(), accountDTO.cpf());
