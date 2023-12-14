23f3e0b1e26bd4b90a433da363f244f1154e1760
silva01 <silva20102@gmail.com>
2023-12-14 07:02:08 -0300

    Added new unit test for create new Account with CPF

---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/daniel/usecase/CreateNewAccountPeerCPFUseCaseTest.java
@@ -0,0 +1,64 @@
+package br.net.silva.daniel.usecase;
+
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.dto.CreateNewAccountForCpfDTO;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.AccountExistsForCPFInformatedException;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.repository.Repository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CreateNewAccountPeerCPFUseCaseTest {
+    @Mock
+    private Repository<Boolean> findIsExistsPeerCPFRepository;
+
+    @Mock
+    private Repository<Account> saveRepository;
+
+    private CreateNewAccountPeerCPFUseCase useCase;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        useCase = new CreateNewAccountPeerCPFUseCase(findIsExistsPeerCPFRepository, saveRepository);
+    }
+
+    @Test
+    public void testExec_accountDoesNotExist_createsNewAccount() throws GenericException {
+        // Arrange
+        String cpf = "12345678901";
+        CreateNewAccountForCpfDTO dto = new CreateNewAccountForCpfDTO(cpf, 1234);
+        Mockito.when(findIsExistsPeerCPFRepository.exec(cpf)).thenReturn(false);
+        Mockito.when(saveRepository.exec(ArgumentMatchers.any(Account.class))).thenReturn(buildMockAccount());
+
+        // Act
+        AccountDTO result = useCase.exec(dto);
+
+        // Assert
+        assertNotNull(result);
+        Mockito.verify(saveRepository, Mockito.times(1)).exec(ArgumentMatchers.any(Account.class));
+    }
+
+    @Test
+    public void testExec_accountExists_throwsException() {
+        // Arrange
+        String cpf = "12345678901";
+        CreateNewAccountForCpfDTO dto = new CreateNewAccountForCpfDTO(cpf, 1234);
+        Mockito.when(findIsExistsPeerCPFRepository.exec(cpf)).thenReturn(true);
+
+        // Act and Assert
+        assertThrows(AccountExistsForCPFInformatedException.class, () -> useCase.exec(dto));
+    }
+
+    private Account buildMockAccount() {
+        return new Account(1234, "1234567", "12345678901");
+    }
+}
