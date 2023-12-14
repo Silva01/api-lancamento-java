b0e84ef86cddc428a9b74e7f6f277cc94034c388
silva01 <silva20102@gmail.com>
2023-12-14 07:02:27 -0300

    Added new test in shared module

---
+++ application-module/shared-application-module/src/test/java/br/net/silva/daniel/repository/RepositoryTest.java
@@ -0,0 +1,33 @@
+package br.net.silva.daniel.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+class RepositoryTest {
+
+    @Mock
+    Repository<String> repositoryMock;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testExec() {
+        String expected = "test";
+        Mockito.when(repositoryMock.exec(any())).thenReturn(expected);
+
+        // Act
+        String result = repositoryMock.exec("any argument");
+
+        // Assert
+        assertEquals(expected, result);
+    }
+}
