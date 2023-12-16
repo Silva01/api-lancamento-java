9beec9f85984e337154b438b9525e1582af6400f
silva01 <silva20102@gmail.com>
2023-12-16 11:54:59 -0300

    Created facades for create account and client

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/facade/CreateNewAccountByCpfFacade.java
@@ -0,0 +1,23 @@
+package br.net.silva.daniel.facade;
+
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.dto.CreateNewAccountForCpfDTO;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.IFacade;
+import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.usecase.CreateNewAccountPeerCPFUseCase;
+
+public class CreateNewAccountByCpfFacade implements IFacade<CreateNewAccountForCpfDTO, AccountDTO> {
+    private final UseCase<CreateNewAccountForCpfDTO, AccountDTO> createNewAccountPeerCPFUseCase;
+
+    public CreateNewAccountByCpfFacade(Repository<Boolean> findIsExistsPeerCPFRepository, Repository<Account> saveRepository) {
+        this.createNewAccountPeerCPFUseCase = new CreateNewAccountPeerCPFUseCase(findIsExistsPeerCPFRepository, saveRepository);
+    }
+
+    @Override
+    public AccountDTO execute(CreateNewAccountForCpfDTO dto) throws GenericException { //TODO: Refatorar exception
+        return createNewAccountPeerCPFUseCase.exec(dto);
+    }
+}
