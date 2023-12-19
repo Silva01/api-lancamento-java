85c514dff4c9e14706ecd3a1336f663631c92ceb
silva01 <silva20102@gmail.com>
2023-12-19 06:11:36 -0300

    Created Exception for while client not exists

---
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/exception/ClientNotExistsException.java
@@ -0,0 +1,7 @@
+package br.net.silva.daniel.exception;
+
+public class ClientNotExistsException extends GenericException{
+    public ClientNotExistsException(String message) {
+        super(message);
+    }
+}
