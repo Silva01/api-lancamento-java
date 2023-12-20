d1ca2e72ca199d7d8d34bb3c0fbd467d1e36f261
silva01 <silva20102@gmail.com>
2023-12-20 06:25:33 -0300

    Refactored name of DTO

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/facade/CreateNewAccountByCpfFacade.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/facade/CreateNewAccountByCpfFacade.java
@@ -1,7 +1,7 @@
 package br.net.silva.daniel.facade;
 
 import br.net.silva.daniel.dto.AccountDTO;
-import br.net.silva.daniel.dto.CreateNewAccountForCpfDTO;
+import br.net.silva.daniel.dto.CreateNewAccountByCpfDTO;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.IFacade;
@@ -9,15 +9,15 @@ import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.usecase.CreateNewAccountPeerCPFUseCase;
 
-public class CreateNewAccountByCpfFacade implements IFacade<CreateNewAccountForCpfDTO, AccountDTO> {
-    private final UseCase<CreateNewAccountForCpfDTO, AccountDTO> createNewAccountPeerCPFUseCase;
+public class CreateNewAccountByCpfFacade implements IFacade<CreateNewAccountByCpfDTO, AccountDTO> {
+    private final UseCase<CreateNewAccountByCpfDTO, AccountDTO> createNewAccountPeerCPFUseCase;
 
     public CreateNewAccountByCpfFacade(Repository<Boolean> findIsExistsPeerCPFRepository, Repository<Account> saveRepository) {
         this.createNewAccountPeerCPFUseCase = new CreateNewAccountPeerCPFUseCase(findIsExistsPeerCPFRepository, saveRepository);
     }
 
     @Override
-    public AccountDTO execute(CreateNewAccountForCpfDTO dto) throws GenericException {
+    public AccountDTO execute(CreateNewAccountByCpfDTO dto) throws GenericException {
         return createNewAccountPeerCPFUseCase.exec(dto);
     }
 }
