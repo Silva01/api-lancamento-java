b9f48d2c2d25a16b5612c4ff98f82950cd79caaa
silva01 <silva20102@gmail.com>
2023-12-23 21:28:44 -0300

    Refactored FindAccountUseCase

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/usecase/FindAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/usecase/FindAccountUseCase.java
@@ -1,16 +1,17 @@
 package br.net.silva.daniel.usecase;
 
-import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.SharedParamDelegate;
 import br.net.silva.daniel.dto.FindAccountDTO;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.AccountNotExistsException;
 import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.IProcessResponse;
 import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.repository.Repository;
 
 import java.util.Optional;
 
-public class FindAccountUseCase implements UseCase<FindAccountDTO, AccountDTO> {
+public class FindAccountUseCase implements UseCase<SharedParamDelegate, IProcessResponse> {
 
     private final Repository<Optional<Account>> findAccountRepository;
 
@@ -19,9 +20,11 @@ public class FindAccountUseCase implements UseCase<FindAccountDTO, AccountDTO> {
     }
 
     @Override
-    public AccountDTO exec(FindAccountDTO param) throws GenericException {
-        var accountOptional = findAccountRepository.exec(param.accountNumber(), param.agency());
+    public IProcessResponse exec(SharedParamDelegate param) throws GenericException {
+        var findAccountDTO = (FindAccountDTO) param.getParam();
+        var accountOptional = findAccountRepository.exec(findAccountDTO.accountNumber(), findAccountDTO.agency());
         var account = accountOptional.orElseThrow(() -> new AccountNotExistsException("Account not found"));
-        return account.create();
+        param.addResponse(account);
+        return param;
     }
 }
