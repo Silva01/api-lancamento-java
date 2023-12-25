3c3814772fc4028edc4545b32bb3fbf81498bec1
silva01 <silva20102@gmail.com>
2023-12-25 15:31:31 -0300

    Refactored abstract facade for support the new interface for validations

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/interfaces/AbstractFacade.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/interfaces/AbstractFacade.java
@@ -2,27 +2,36 @@ package br.net.silva.daniel.interfaces;
 
 import br.net.silva.daniel.SharedParamDelegate;
 import br.net.silva.daniel.exception.GenericException;
-import br.net.silva.daniel.exception.ProcessExecErrorException;
 
+import java.util.List;
 import java.util.Queue;
 
 public abstract class AbstractFacade<U extends UseCase<SharedParamDelegate, IProcessResponse>> {
 
     private final Queue<U> useCases;
+    private final List<IValidations<SharedParamDelegate>> validationsList;
 
-    protected AbstractFacade(Queue<U> useCases) {
+    protected AbstractFacade(Queue<U> useCases, List<IValidations<SharedParamDelegate>> validationsList) {
         this.useCases = useCases;
+        this.validationsList = validationsList;
     }
 
-    public IProcessResponse exec(SharedParamDelegate delegate) {
-        useCases.forEach(useCase -> {
-            try {
-                var response = useCase.exec(delegate);
-                delegate.addResponse(response);
-            } catch (GenericException e) {
-                throw new ProcessExecErrorException(e);
-            }
-        });
+    public IProcessResponse exec(SharedParamDelegate delegate) throws GenericException {
+        validate(delegate);
+        return execProcess(delegate);
+    }
+
+    private void validate(SharedParamDelegate delegate) throws GenericException {
+        for (IValidations<SharedParamDelegate> validations : validationsList) {
+            validations.validate(delegate);
+        }
+    }
+
+    private IProcessResponse execProcess(SharedParamDelegate delegate) throws GenericException {
+        for (U useCase : useCases) {
+            var response = useCase.exec(delegate);
+            delegate.addResponse(response);
+        }
 
         return delegate;
     }
