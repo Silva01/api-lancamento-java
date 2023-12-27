2b848d30aa01b76d4e296871456846a23901ff38
silva01 <silva20102@gmail.com>
2023-12-27 06:45:36 -0300

    Refactored for work with generic params

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/facade/AccountFacade.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/facade/AccountFacade.java
@@ -1,18 +1,17 @@
 package br.net.silva.daniel.facade;
 
-import br.net.silva.daniel.SharedParamDelegate;
-import br.net.silva.daniel.interfaces.AbstractFacade;
-import br.net.silva.daniel.interfaces.IProcessResponse;
-import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.*;
 
+import java.util.List;
 import java.util.Queue;
 
 public class AccountFacade extends AbstractFacade {
-    protected AccountFacade(Queue<? extends UseCase<?, IProcessResponse>> useCases) {
-        super(useCases);
+    protected AccountFacade(Queue<? extends UseCase<?, IProcessResponse<?>>> useCases, List<IValidations<? extends IGenericPort>> validationsList) {
+        super(useCases, validationsList);
     }
 
-    public IProcessResponse execute(SharedParamDelegate delegate) {
-        return exec(delegate);
+    public IProcessResponse<?> execute(IGenericPort port) throws GenericException {
+        return exec(port);
     }
 }
