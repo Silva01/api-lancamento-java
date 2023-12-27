3d2b4846c035852143fb73976315095062a40190
silva01 <silva20102@gmail.com>
2023-12-27 06:44:47 -0300

    Refactored for implements IGenericPort

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/dto/CreditCardDTO.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/dto/CreditCardDTO.java
@@ -1,6 +1,7 @@
 package br.net.silva.daniel.dto;
 
 import br.net.silva.daniel.enuns.FlagEnum;
+import br.net.silva.daniel.interfaces.IGenericPort;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,5 +13,15 @@ public record CreditCardDTO(
         BigDecimal balance,
         LocalDate expirationDate,
         boolean active
-) {
+) implements IGenericPort {
+    @Override
+    public void accept(Class<?> clazz) {
+        if (!clazz.isInstance(CreditCardDTO.class))
+            throw new IllegalArgumentException("Class must be assignable from IAccountPort");
+    }
+
+    @Override
+    public CreditCardDTO get() {
+        return this;
+    }
 }
