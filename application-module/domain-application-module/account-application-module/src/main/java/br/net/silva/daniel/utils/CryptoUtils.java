ca36a3ace2b3311203ee7b0622a76309bd5359c7
silva01 <silva20102@gmail.com>
2023-12-28 06:31:18 -0300

    Added class utils for cryptography password to SHA-256 or MD5

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/utils/CryptoUtils.java
@@ -0,0 +1,45 @@
+package br.net.silva.daniel.utils;
+
+import br.net.silva.daniel.exception.ProblemConvertPasswordToCryptoException;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public final class CryptoUtils {
+
+    private CryptoUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String convertToSHA256(String pass) {
+        try {
+            return convertTo(pass, "SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new ProblemConvertPasswordToCryptoException("SHA-256 algorithm not found", e);
+        }
+    }
+
+    public static String convertToMD5(String pass) {
+        try {
+            return convertTo(pass, "MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new ProblemConvertPasswordToCryptoException("SHA-256 algorithm not found", e);
+        }
+    }
+
+    private static String convertTo(String password, String algorithm) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+        byte[] hash = messageDigest.digest(password.getBytes());
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xFF & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+}
