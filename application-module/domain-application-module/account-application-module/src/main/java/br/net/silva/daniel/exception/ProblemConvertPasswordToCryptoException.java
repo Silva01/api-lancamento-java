86f8ab249aab4f41688f9bfa6ddcb77ab7a643c3
silva01 <silva20102@gmail.com>
2023-12-28 06:32:06 -0300

    Added exception for cryptography password error

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/exception/ProblemConvertPasswordToCryptoException.java
@@ -0,0 +1,8 @@
+package br.net.silva.daniel.exception;
+
+public class ProblemConvertPasswordToCryptoException extends RuntimeException {
+
+    public ProblemConvertPasswordToCryptoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
