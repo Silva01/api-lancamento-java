9846561a390715629e7f0309718c35e965c8fe6c
silva01 <silva20102@gmail.com>
2023-12-29 19:06:57 -0300

    Created Generic mapper

---
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/mapper/GenericMapper.java
@@ -0,0 +1,45 @@
+package br.net.silva.daniel.mapper;
+
+import br.net.silva.daniel.exception.MapperNotConvertErrorException;
+import br.net.silva.daniel.interfaces.IGenericPort;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+public class GenericMapper<T extends IGenericPort> implements IMapper<T, IGenericPort>{
+
+    private final Class<T> clazz;
+
+    public GenericMapper(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public T map(IGenericPort param) {
+        return toMap(param.get(), this.clazz);
+    }
+
+    @SuppressWarnings("unchecked")
+    private T toMap(Object object, Class<T> clazz) {
+        try {
+            Class<T> targetClass = (Class<T>) object.getClass();
+            // Find the constructor of the targetClass with the appropriate parameters
+            Constructor<T> constructor = targetClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            T dto = constructor.newInstance();
+
+            for (Field targetField : targetClass.getDeclaredFields()) {
+                Field sourceField = clazz.getDeclaredField(targetField.getName());
+                sourceField.setAccessible(true);
+                targetField.setAccessible(true);
+
+                Object value = sourceField.get(object);
+                targetField.set(dto, value);
+            }
+
+            return dto;
+        } catch (Exception e) {
+            throw new MapperNotConvertErrorException(e);
+        }
+    }
+}
