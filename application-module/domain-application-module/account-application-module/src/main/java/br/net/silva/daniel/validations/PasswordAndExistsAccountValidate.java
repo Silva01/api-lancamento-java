c40f543da00b685150d52cf7a8c6ab928fc80d3f
silva01 <silva20102@gmail.com>
2023-12-29 19:08:46 -0300

    Refactored for support news generic mappers

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/validations/PasswordAndExistsAccountValidate.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/validations/PasswordAndExistsAccountValidate.java
@@ -1,6 +1,5 @@
 package br.net.silva.daniel.validations;
 
-import br.net.silva.daniel.dto.ChangePasswordDTO;
 import br.net.silva.daniel.dto.FindAccountDTO;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
@@ -8,20 +7,23 @@ import br.net.silva.daniel.interfaces.IGenericPort;
 import br.net.silva.daniel.interfaces.IProcessResponse;
 import br.net.silva.daniel.interfaces.IValidations;
 import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.mapper.GenericMapper;
 import br.net.silva.daniel.utils.CryptoUtils;
 
-public class PasswordAndExistsAccountValidate implements IValidations<ChangePasswordDTO> {
+public class PasswordAndExistsAccountValidate implements IValidations<IGenericPort> {
 
     private final UseCase<FindAccountDTO, IProcessResponse<? extends IGenericPort>> findAccountUseCase;
+    private final GenericMapper<FindAccountDTO> mapper;
 
     public PasswordAndExistsAccountValidate(UseCase<FindAccountDTO, IProcessResponse<? extends IGenericPort>> findAccountUseCase) {
         this.findAccountUseCase = findAccountUseCase;
+        this.mapper = new GenericMapper<>(FindAccountDTO.class);
     }
 
     @Override
-    public void validate(ChangePasswordDTO param) throws GenericException {
-        var findAccountParamDTO = new FindAccountDTO(param.cpf(), param.account(), param.agency());
+    public void validate(IGenericPort param) throws GenericException {
+        var findAccountParamDTO = mapper.map(param);
         var account = (Account) findAccountUseCase.exec(findAccountParamDTO).build();
-        account.validatePassword(CryptoUtils.convertToSHA256(param.get().oldPassword()));
+        account.validatePassword(CryptoUtils.convertToSHA256(findAccountParamDTO.password()));
     }
 }
