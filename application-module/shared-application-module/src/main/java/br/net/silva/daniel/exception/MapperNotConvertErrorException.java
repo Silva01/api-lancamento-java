e9e697f044eeb3a1c3cdef7b2ca4af18c151885c
silva01 <silva20102@gmail.com>
2023-12-29 19:07:23 -0300

    Created Exception for problem when convert data

---
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/exception/MapperNotConvertErrorException.java
@@ -0,0 +1,7 @@
+package br.net.silva.daniel.exception;
+
+public class MapperNotConvertErrorException extends RuntimeException {
+    public MapperNotConvertErrorException(Throwable cause) {
+        super(cause);
+    }
+}
