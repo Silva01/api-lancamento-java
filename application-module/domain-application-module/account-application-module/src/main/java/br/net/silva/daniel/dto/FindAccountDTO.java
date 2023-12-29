84ca7f684802285ed567403b2a1ed38c6646d58a
silva01 <silva20102@gmail.com>
2023-12-29 19:07:52 -0300

    Implemented IGenericPort

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/dto/FindAccountDTO.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/daniel/dto/FindAccountDTO.java
@@ -1,8 +1,21 @@
 package br.net.silva.daniel.dto;
 
+import br.net.silva.daniel.interfaces.IGenericPort;
+import br.net.silva.daniel.utils.ValidateUtils;
+
 public record FindAccountDTO(
         String cpf,
         Integer agency,
-        Integer accountNumber
-) {
+        Integer accountNumber,
+        String password
+) implements IGenericPort {
+    @Override
+    public void accept(Class<?> clazz) {
+        ValidateUtils.isTypeOf(clazz, FindAccountDTO.class, "Type of class is incompatible with FindAccountDTO");
+    }
+
+    @Override
+    public Object get() {
+        return this;
+    }
 }
