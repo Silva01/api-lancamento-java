2d88499a838608543889ec795b271afc0930301a
silva01 <silva20102@gmail.com>
2024-01-01 16:00:53 -0300

    Refactored for use Genericmapper

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewClientUseCase.java
@@ -1,5 +1,6 @@
 package br.net.silva.daniel.usecase;
 
+import br.net.silva.daniel.interfaces.IGenericPort;
 import br.net.silva.daniel.mapper.ClientDtoMapper;
 import br.net.silva.daniel.dto.ClientDTO;
 import br.net.silva.daniel.dto.ClientRequestDTO;
@@ -9,24 +10,28 @@ import br.net.silva.daniel.factory.CreateNewAddressFactory;
 import br.net.silva.daniel.factory.CreateNewClientFactory;
 import br.net.silva.daniel.factory.IFactoryAggregate;
 import br.net.silva.daniel.interfaces.UseCase;
-import br.net.silva.daniel.mapper.IMapper;
+import br.net.silva.daniel.interfaces.IMapper;
+import br.net.silva.daniel.mapper.GenericMapper;
 import br.net.silva.daniel.repository.Repository;
 
-public class CreateNewClientUseCase implements UseCase<ClientRequestDTO, ClientDTO> {
+public class CreateNewClientUseCase implements UseCase<ClientDTO> {
     private final Repository<Client> saveRepository;
     private final IFactoryAggregate<Client, ClientDTO> createNewClientFactory;
     private final IMapper<ClientDTO, ClientRequestDTO> clientMapper;
+    private final IMapper<ClientRequestDTO, IGenericPort> genericMapper;
 
     public CreateNewClientUseCase(Repository<Client> saveRepository) {
         this.saveRepository = saveRepository;
         this.createNewClientFactory = new CreateNewClientFactory(new CreateNewAddressFactory());
         this.clientMapper = new ClientDtoMapper();
+        this.genericMapper = new GenericMapper<>(ClientRequestDTO.class);
     }
 
     @Override
-    public ClientDTO exec(ClientRequestDTO request) throws ExistsClientRegistredException {
+    public ClientDTO exec(IGenericPort request) throws ExistsClientRegistredException {
         try {
-            return saveRepository.exec(buildClient(request)).build();
+            var clientRequestDto = genericMapper.map(request);
+            return saveRepository.exec(buildClient(clientRequestDto)).build();
         } catch (Exception e) {
             throw new ExistsClientRegistredException(e.getMessage());
         }
