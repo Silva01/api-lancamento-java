0df54168db80024897104a1c3633a685e0641be8
silva01 <silva20102@gmail.com>
2024-01-01 15:58:36 -0300

    Implement Interface IGenericPort

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/dto/ClientRequestDTO.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/dto/ClientRequestDTO.java
@@ -1,5 +1,8 @@
 package br.net.silva.daniel.dto;
 
+import br.net.silva.daniel.interfaces.IGenericPort;
+import br.net.silva.daniel.utils.ValidateUtils;
+
 public record ClientRequestDTO(
         String id,
         String cpf,
@@ -8,5 +11,14 @@ public record ClientRequestDTO(
         boolean active,
         Integer agencyNumber,
         AddressRequestDTO addressRequestDTO
-) {
+) implements IGenericPort {
+    @Override
+    public void accept(Class<?> clazz) {
+        ValidateUtils.isTypeOf(clazz, ClientRequestDTO.class, "Type of class is not ClientRequestDTO");
+    }
+
+    @Override
+    public Object get() {
+        return this;
+    }
 }
