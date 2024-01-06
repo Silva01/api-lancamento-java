c0accf212207f9539598f6e3397d9f2872b833ee
silva01 <silva20102@gmail.com>
2024-01-06 15:13:48 -0300

    Refactored validation rule

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsValidate.java
@@ -1,25 +1,38 @@
 package br.net.silva.daniel.validation;
 
 import br.net.silva.business.dto.ChangePasswordDTO;
-import br.net.silva.daniel.dto.ClientDTO;
+import br.net.silva.daniel.entity.Client;
 import br.net.silva.daniel.exception.GenericException;
-import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
 import br.net.silva.daniel.interfaces.IValidations;
 import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
 import br.net.silva.daniel.shared.business.mapper.GenericMapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientExistsValidate implements IValidations {
 
     private final GenericMapper<ChangePasswordDTO> mapper;
-    private final UseCase<ClientDTO> findClientUseCase;
+    private final UseCase<Client> findClientUseCase;
 
-    public ClientExistsValidate(UseCase<ClientDTO> findClientUseCase) {
+    public ClientExistsValidate(UseCase<Client> findClientUseCase) {
         this.findClientUseCase = findClientUseCase;
         this.mapper = new GenericMapper<>(ChangePasswordDTO.class);
     }
 
     @Override
     public void validate(IGenericPort param) throws GenericException {
-        findClientUseCase.exec(param);
+        List<GenericException> errors = new ArrayList<>();
+
+        try {
+            findClientUseCase.exec(param);
+        } catch (GenericException e) {
+           errors.add(e);
+        }
+
+        if (errors.isEmpty()) {
+            throw new GenericException("Client exists in database");
+        }
     }
 }
