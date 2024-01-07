5d0c814df8319f6f61a5ac8394733ff0424aeaaa
silva01 <silva20102@gmail.com>
2024-01-06 23:09:04 -0300

    Refactored for use ValidateUtils

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/utils/AccountUtils.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/utils/AccountUtils.java
@@ -1,15 +1,15 @@
 package br.net.silva.daniel.utils;
 
-import br.net.silva.daniel.shared.business.validation.Validation;
+import br.net.silva.daniel.shared.business.utils.ValidateUtils;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class AccountUtils extends Validation {
+public class AccountUtils {
 
-    public void validatePassword(String password) {
-        validateAttributeNotNullAndNotEmpty(password, "Password is required");
-        validateAttributeEqualsZero(password.length(), "Password must be greater than 6 Characters");
+    public static void validatePassword(String password) {
+        ValidateUtils.isTextNotNullAndNotEmpty(password, "Password is required");
+        ValidateUtils.isEqualsZero(password.length(), "Password must be greater than 6 Characters");
         validateRepeatedNumberPassword(password);
         validateIfPasswordOnlyNumbers(password);
         if (password.length() < 6) {
@@ -17,7 +17,7 @@ public class AccountUtils extends Validation {
         }
     }
 
-    private void validateRepeatedNumberPassword(String password) {
+    private static void validateRepeatedNumberPassword(String password) {
         Set<Character> seenCharacters = new HashSet<>();
         for (char character : password.toCharArray()) {
             if (Character.isDigit(character) && !seenCharacters.add(character)) {
@@ -26,16 +26,11 @@ public class AccountUtils extends Validation {
         }
     }
 
-    private void validateIfPasswordOnlyNumbers(String password) {
+    private static void validateIfPasswordOnlyNumbers(String password) {
         var isOnlyNumbers =  password.chars().allMatch(Character::isDigit);
 
         if (!isOnlyNumbers) {
             throw new IllegalArgumentException("Password cannot have only numbers");
         }
-    }
-
-    @Override
-    public void validate() {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
