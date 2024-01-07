b9eb94400fbbfb59453a89aa559b4939507929a0
silva01 <silva20102@gmail.com>
2024-01-07 10:01:07 -0300

    Created new tests for DTO in module account

---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/dto/ChangePasswordDTOTest.java
@@ -0,0 +1,30 @@
+package br.net.silva.business.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ChangePasswordDTOTest {
+
+    @Test
+    void mustCreateDTOWithSuccess() {
+        var change = new ChangePasswordDTO("44455566677", 1234, 23333, "123456", "432121");
+        change.accept(ChangePasswordDTO.class);
+
+        assertEquals("44455566677", change.cpf());
+        assertEquals(1234, change.agency());
+        assertEquals(23333, change.account());
+        assertEquals("123456", change.password());
+        assertEquals("432121", change.newPassword());
+    }
+
+    @Test
+    void mustErrorCreateDTO() {
+        var change = new ChangePasswordDTO("44455566677", 1234, 23333, "123456", "432121");
+        var exceptionResponse = assertThrows(IllegalArgumentException.class, () -> change.accept(null));
+
+        assertEquals("Type of class is incompatible with ChangePasswordDTO", exceptionResponse.getMessage());
+    }
+
+}
---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/dto/CreateNewAccountByCpfDTOTest.java
@@ -0,0 +1,27 @@
+package br.net.silva.business.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CreateNewAccountByCpfDTOTest {
+
+    @Test
+    void mustCreateDTOWithSuccess() {
+        var create = new CreateNewAccountByCpfDTO("44455566677", 1234, "123456");
+        create.accept(CreateNewAccountByCpfDTO.class);
+
+        assertEquals("44455566677", create.cpf());
+        assertEquals(1234, create.agency());
+        assertEquals("123456", create.password());
+    }
+
+    @Test
+    void mustErrorCreateDTO() {
+        var create = new CreateNewAccountByCpfDTO("44455566677", 1234, "123456");
+        var exceptionResponse = assertThrows(IllegalArgumentException.class, () -> create.accept(null));
+
+        assertEquals("Type of class is not CreateNewAccountByCpfDTO", exceptionResponse.getMessage());
+    }
+}
