69755a5d9ed5e4c952ff9765eda0a330842e00c0
silva01 <silva20102@gmail.com>
2024-01-06 23:11:25 -0300

    Refactored for validation password information for the client

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/PasswordAndExistsAccountValidate.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/PasswordAndExistsAccountValidate.java
@@ -1,14 +1,16 @@
 package br.net.silva.business.validations;
 
-import br.net.silva.daniel.shared.business.utils.CryptoUtils;
 import br.net.silva.business.dto.FindAccountDTO;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
-import br.net.silva.daniel.interfaces.*;
+import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
 import br.net.silva.daniel.shared.business.interfaces.IMapper;
 import br.net.silva.daniel.shared.business.interfaces.IProcessResponse;
 import br.net.silva.daniel.shared.business.mapper.GenericMapper;
+import br.net.silva.daniel.shared.business.utils.CryptoUtils;
+import br.net.silva.daniel.utils.AccountUtils;
 
 public class PasswordAndExistsAccountValidate implements IValidations {
 
@@ -23,6 +25,7 @@ public class PasswordAndExistsAccountValidate implements IValidations {
     @Override
     public void validate(IGenericPort param) throws GenericException {
         var findAccountParamDTO = this.mapper.map(param);
+        AccountUtils.validatePassword(findAccountParamDTO.password());
         var account = (Account) findAccountUseCase.exec(findAccountParamDTO);
         account.validatePassword(CryptoUtils.convertToSHA256(findAccountParamDTO.password()));
     }
