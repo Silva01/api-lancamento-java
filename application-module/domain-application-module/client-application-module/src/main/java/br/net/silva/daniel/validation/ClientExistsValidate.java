7c936f98087b789b6f4a6bbbf93cafa24baac336
silva01 <silva20102@gmail.com>
2024-01-08 06:24:21 -0300

    Refactored validate when client exists

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsValidate.java
@@ -1,38 +1,24 @@
 package br.net.silva.daniel.validation;
 
-import br.net.silva.business.dto.ChangePasswordDTO;
 import br.net.silva.daniel.entity.Client;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.IValidations;
 import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
-import br.net.silva.daniel.shared.business.mapper.GenericMapper;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClientExistsValidate implements IValidations {
-
-    private final GenericMapper<ChangePasswordDTO> mapper;
     private final UseCase<Client> findClientUseCase;
 
     public ClientExistsValidate(UseCase<Client> findClientUseCase) {
         this.findClientUseCase = findClientUseCase;
-        this.mapper = new GenericMapper<>(ChangePasswordDTO.class);
     }
 
     @Override
     public void validate(IGenericPort param) throws GenericException {
-        List<GenericException> errors = new ArrayList<>();
-
         try {
             findClientUseCase.exec(param);
         } catch (GenericException e) {
-           errors.add(e);
-        }
-
-        if (errors.isEmpty()) {
-            throw new GenericException("Client exists in database");
+            throw new GenericException("Client not exists in database");
         }
     }
 }
