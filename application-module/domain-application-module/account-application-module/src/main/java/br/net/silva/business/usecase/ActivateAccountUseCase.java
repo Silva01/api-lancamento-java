25807c82261cc992ae6b6f0f24b074850d13f091
silva01 <silva20102@gmail.com>
2024-01-09 07:10:57 -0300

    Added Use case for active account

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
@@ -0,0 +1,36 @@
+package br.net.silva.business.usecase;
+
+import br.net.silva.business.dto.FindAccountDTO;
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
+import br.net.silva.daniel.shared.business.interfaces.IProcessResponse;
+import br.net.silva.daniel.shared.business.mapper.GenericMapper;
+
+public class ActivateAccountUseCase implements UseCase<IProcessResponse<AccountDTO>> {
+
+    private final Repository<Account> activateAccountRepository;
+    private final Repository<Account> findAccountRepository;
+    private final GenericMapper<FindAccountDTO> mapper;
+
+    public ActivateAccountUseCase(Repository<Account> activateAccountRepository, Repository<Account> findAccountRepository) {
+        this.activateAccountRepository = activateAccountRepository;
+        this.findAccountRepository = findAccountRepository;
+        this.mapper = new GenericMapper<>(FindAccountDTO.class);
+    }
+
+    @Override
+    public IProcessResponse<AccountDTO> exec(IGenericPort param) throws GenericException {
+        try {
+            var dto = mapper.map(param);
+            var account = findAccountRepository.exec(dto.accountNumber(), dto.agency(), dto.cpf());
+            account.activate();
+            return activateAccountRepository.exec(account);
+        } catch (Exception e) {
+            throw new GenericException(e.getMessage());
+        }
+    }
+}
