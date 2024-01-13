730bae94158cf3a7864ed139472b6f70004e4549
silva01 <silva20102@gmail.com>
2024-01-13 05:59:25 -0300

    Created annotations for mapstruct

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/annotations/Cvv.java
@@ -0,0 +1,14 @@
+package br.net.silva.business.annotations;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Cvv {
+}
