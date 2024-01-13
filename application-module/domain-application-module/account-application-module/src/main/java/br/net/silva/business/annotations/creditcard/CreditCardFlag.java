e7d47e3ee231c18e063cd92e70a1b1f166a2b715
silva01 <silva20102@gmail.com>
2024-01-13 07:22:25 -0300

    Added annotations for credit card

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/annotations/creditcard/CreditCardFlag.java
@@ -0,0 +1,14 @@
+package br.net.silva.business.annotations.creditcard;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface CreditCardFlag {
+}
