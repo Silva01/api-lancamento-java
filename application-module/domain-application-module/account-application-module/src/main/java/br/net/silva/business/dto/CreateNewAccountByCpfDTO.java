6869db21c7a806f534fadd4509ebb5ef5b3ad43d
silva01 <silva20102@gmail.com>
2024-01-13 14:20:52 -0300

    Refactored to record

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/dto/CreateNewAccountByCpfDTO.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/dto/CreateNewAccountByCpfDTO.java
@@ -3,33 +3,11 @@ package br.net.silva.business.dto;
 import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
 import br.net.silva.daniel.shared.business.utils.ValidateUtils;
 
-public class CreateNewAccountByCpfDTO implements IGenericPort {
-
-    private String cpf;
-    private Integer agency;
-    private String password;
-
-    public CreateNewAccountByCpfDTO(String cpf, Integer agency, String password) {
-        this.cpf = cpf;
-        this.agency = agency;
-        this.password = password;
-    }
-
-    public CreateNewAccountByCpfDTO() {
-        this(null, 0, null);
-    }
-
-    public String cpf() {
-        return cpf;
-    }
-
-    public Integer agency() {
-        return agency;
-    }
-
-    public String password() {
-        return password;
-    }
+public record CreateNewAccountByCpfDTO(
+        String cpf,
+        Integer agency,
+        String password
+) implements IGenericPort {
 
     @Override
     public void accept(Class<?> clazz) {
