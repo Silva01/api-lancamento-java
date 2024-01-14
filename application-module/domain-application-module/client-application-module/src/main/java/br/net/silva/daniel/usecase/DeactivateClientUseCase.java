297139b2d4b56455ee2082f7a54864e9f8f6ec40
silva01 <silva20102@gmail.com>
2024-01-14 13:58:26 -0300

    Refactored Use case for new Pattern

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/DeactivateClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/DeactivateClientUseCase.java
@@ -1,36 +1,35 @@
 package br.net.silva.daniel.usecase;
 
-import br.net.silva.daniel.dto.ClientDTO;
-import br.net.silva.daniel.dto.ClientRequestDTO;
 import br.net.silva.daniel.entity.Client;
+import br.net.silva.daniel.enums.TypeClientMapperEnum;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.mapper.ToClientMapper;
 import br.net.silva.daniel.repository.Repository;
-import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
-import br.net.silva.daniel.shared.business.interfaces.IProcessResponse;
-import br.net.silva.daniel.shared.business.mapper.GenericMapper;
+import br.net.silva.daniel.utils.ConverterUtils;
+import br.net.silva.daniel.value_object.Source;
 
 import java.util.Optional;
 
-public class DeactivateClientUseCase implements UseCase<IProcessResponse<ClientDTO>> {
+public class DeactivateClientUseCase implements UseCase {
 
     private final FindClientUseCase findClientUseCase;
     private final Repository<Client> saveRepository;
-
-    private final GenericMapper<ClientRequestDTO> mapper;
+    private final ToClientMapper mapper;
 
     public DeactivateClientUseCase(Repository<Optional<Client>> findClientRepository, Repository<Client> saveRepository) {
         this.findClientUseCase = new FindClientUseCase(findClientRepository);
         this.saveRepository = saveRepository;
-        this.mapper = new GenericMapper<>(ClientRequestDTO.class);
+        this.mapper = ToClientMapper.INSTANCE;
     }
 
     @Override
-    public Client exec(IGenericPort param) throws GenericException {
-        var request = mapper.map(param);
+    public void exec(Source param) throws GenericException {
+        var request = mapper.toClientRequestDTO(param);
         var client = findClientUseCase.exec(request);
         client.deactivate();
 
-        return saveRepository.exec(client);
+        var clientUpdated = saveRepository.exec(client);
+        param.map().put(TypeClientMapperEnum.CLIENT.name(), ConverterUtils.convertJsonToMap(ConverterUtils.convertObjectToJson(clientUpdated.build())));
     }
 }
--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/FindClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/FindClientUseCase.java
@@ -1,31 +1,31 @@
 package br.net.silva.daniel.usecase;
 
-import br.net.silva.daniel.dto.ClientDTO;
 import br.net.silva.daniel.entity.Client;
+import br.net.silva.daniel.enums.TypeClientMapperEnum;
 import br.net.silva.daniel.exception.ClientNotExistsException;
-import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
-import br.net.silva.daniel.shared.business.interfaces.IMapper;
 import br.net.silva.daniel.interfaces.UseCase;
-import br.net.silva.daniel.shared.business.interfaces.IProcessResponse;
-import br.net.silva.daniel.shared.business.mapper.GenericMapper;
+import br.net.silva.daniel.mapper.ToClientMapper;
 import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.utils.ConverterUtils;
+import br.net.silva.daniel.value_object.Source;
 
 import java.util.Optional;
 
-public class FindClientUseCase implements UseCase<IProcessResponse<ClientDTO>> {
+public class FindClientUseCase implements UseCase {
 
     private final Repository<Optional<Client>> findClientRepository;
-    private final IMapper<ClientDTO, IGenericPort> genericMapper;
+    private final ToClientMapper mapper;
 
     public FindClientUseCase(Repository<Optional<Client>> findClientRepository) {
         this.findClientRepository = findClientRepository;
-        this.genericMapper = new GenericMapper<>(ClientDTO.class);
+        this.mapper = ToClientMapper.INSTANCE;
     }
 
     @Override
-    public Client exec(IGenericPort param) throws ClientNotExistsException {
-        var clientDto = genericMapper.map(param);
-        var optionalClient = findClientRepository.exec(clientDto.cpf());
-        return optionalClient.orElseThrow(() -> new ClientNotExistsException("Client not exists in database"));
+    public void exec(Source param) throws ClientNotExistsException {
+        var clientRequestDTO = mapper.toClientRequestDTO(param);
+        var optionalClient = findClientRepository.exec(clientRequestDTO.cpf());
+        var client = optionalClient.orElseThrow(() -> new ClientNotExistsException("Client not exists in database"));
+        param.map().put(TypeClientMapperEnum.CLIENT.name(), ConverterUtils.convertJsonToMap(ConverterUtils.convertObjectToJson(client)));
     }
 }
