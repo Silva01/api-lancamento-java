47961541c8c33a3943befdc3bf8941b2921135cb
silva01 <silva20102@gmail.com>
2024-01-14 12:49:47 -0300

    Refactored for support domain class

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/mapper/ToAddessMapper.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/mapper/ToAddessMapper.java
@@ -2,6 +2,7 @@ package br.net.silva.daniel.mapper;
 
 import br.net.silva.daniel.annotations.address.*;
 import br.net.silva.daniel.dto.AddressDTO;
+import br.net.silva.daniel.dto.AddressRequestDTO;
 import br.net.silva.daniel.utils.AddressMapperUtils;
 import br.net.silva.daniel.value_object.Source;
 import org.mapstruct.Mapper;
@@ -12,6 +13,15 @@ import org.mapstruct.factory.Mappers;
 public interface ToAddessMapper {
 
     ToAddessMapper INSTANCE = Mappers.getMapper(ToAddessMapper.class);
+
+    @Mapping(target = "street", source = "map", qualifiedBy = AddressStreet.class)
+    @Mapping(target = "city", source = "map", qualifiedBy = AddressCity.class)
+    @Mapping(target = "complement", source = "map", qualifiedBy = AddressComplement.class)
+    @Mapping(target = "neighborhood", source = "map", qualifiedBy = AddressNeighborhood.class)
+    @Mapping(target = "number", source = "map", qualifiedBy = AddressNumber.class)
+    @Mapping(target = "state", source = "map", qualifiedBy = AddressState.class)
+    @Mapping(target = "zipCode", source = "map", qualifiedBy = AddressZipCode.class)
+    AddressRequestDTO toAddressRequestDTO(Source source);
 
     @Mapping(target = "street", source = "map", qualifiedBy = AddressStreet.class)
     @Mapping(target = "city", source = "map", qualifiedBy = AddressCity.class)
--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/mapper/ToClientMapper.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/mapper/ToClientMapper.java
@@ -2,16 +2,25 @@ package br.net.silva.daniel.mapper;
 
 import br.net.silva.daniel.annotations.client.*;
 import br.net.silva.daniel.dto.ClientDTO;
-import br.net.silva.daniel.utils.ClientMapperUtils;
+import br.net.silva.daniel.dto.ClientRequestDTO;
+import br.net.silva.daniel.utils.ClientRequestMapperUtils;
 import br.net.silva.daniel.value_object.Source;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = {ClientMapperUtils.class})
+@Mapper(uses = {ClientRequestMapperUtils.class})
 public interface ToClientMapper {
 
     ToClientMapper INSTANCE = Mappers.getMapper(ToClientMapper.class);
+
+    @Mapping(target = "id", source = "map", qualifiedBy = ClientId.class)
+    @Mapping(target = "cpf", source = "map", qualifiedBy = ClientCpf.class)
+    @Mapping(target = "name", source = "map", qualifiedBy = ClientName.class)
+    @Mapping(target = "active", source = "map", qualifiedBy = ClientActive.class)
+    @Mapping(target = "telephone", source = "map", qualifiedBy = ClientTelephone.class)
+    @Mapping(target = "addressRequestDTO", source = "map", qualifiedBy = ClientAddressRequest.class)
+    ClientRequestDTO toClientRequestDTO(Source source);
 
     @Mapping(target = "id", source = "map", qualifiedBy = ClientId.class)
     @Mapping(target = "cpf", source = "map", qualifiedBy = ClientCpf.class)
