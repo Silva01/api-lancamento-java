3da7ca1b40f8b7946761e878bce47d4d44172572
silva01 <silva20102@gmail.com>
2024-01-14 11:15:54 -0300

    Refactored Use Case for suit new pattern

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
@@ -1,34 +1,35 @@
 package br.net.silva.business.usecase;
 
-import br.net.silva.business.dto.FindAccountDTO;
-import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.business.enums.TypeAccountMapperEnum;
+import br.net.silva.business.mapper.MapToFindAccountMapper;
+import br.net.silva.business.mapper.MapToFindAccountMapperImpl;
+import br.net.silva.business.utils.ConverterUtils;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.repository.Repository;
-import br.net.silva.daniel.shared.business.interfaces.IGenericPort;
-import br.net.silva.daniel.shared.business.interfaces.IProcessResponse;
-import br.net.silva.daniel.shared.business.mapper.GenericMapper;
+import br.net.silva.daniel.value_object.Source;
 
-public class ActivateAccountUseCase implements UseCase<IProcessResponse<AccountDTO>> {
+public class ActivateAccountUseCase implements UseCase {
 
     private final Repository<Account> activateAccountRepository;
     private final Repository<Account> findAccountRepository;
-    private final GenericMapper<FindAccountDTO> mapper;
+    private final MapToFindAccountMapper mapper;
 
     public ActivateAccountUseCase(Repository<Account> activateAccountRepository, Repository<Account> findAccountRepository) {
         this.activateAccountRepository = activateAccountRepository;
         this.findAccountRepository = findAccountRepository;
-        this.mapper = new GenericMapper<>(FindAccountDTO.class);
+        this.mapper = new MapToFindAccountMapperImpl();
     }
 
     @Override
-    public IProcessResponse<AccountDTO> exec(IGenericPort param) throws GenericException {
+    public void exec(Source param) throws GenericException {
         try {
-            var dto = mapper.map(param);
+            var dto = mapper.mapToFindAccountDto(param.input());
             var account = findAccountRepository.exec(dto.account(), dto.agency(), dto.cpf());
             account.activate();
-            return activateAccountRepository.exec(account);
+            var accountActive = activateAccountRepository.exec(account);
+            param.map().put(TypeAccountMapperEnum.ACCOUNT.name(), ConverterUtils.convertJsonToMap(ConverterUtils.convertObjectToJson(accountActive.build())));
         } catch (Exception e) {
             throw new GenericException(e.getMessage());
         }
