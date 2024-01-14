a884c3ac5eef67a1693e1f63bb9e7231cc082f74
silva01 <silva20102@gmail.com>
2024-01-14 11:16:40 -0300

    Added new map for param input

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/value_object/Source.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/value_object/Source.java
@@ -1,8 +1,13 @@
 package br.net.silva.daniel.value_object;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public record Source(
-        Map<String, Object> map
+        Map<String, Object> map,
+        Map<String, String> input
 ) {
+    public Source(Map<String, Object> map) {
+        this(map, new HashMap<>());
+    }
 }
