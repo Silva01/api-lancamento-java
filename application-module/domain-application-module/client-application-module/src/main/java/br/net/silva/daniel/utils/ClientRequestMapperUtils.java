b995e72b2eb5ae1803f445ed17b3883f026fd68f
silva01 <silva20102@gmail.com>
2024-01-14 12:50:19 -0300

    Added new conversion for domain class

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/utils/ClientRequestMapperUtils.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/utils/ClientRequestMapperUtils.java
@@ -2,13 +2,14 @@ package br.net.silva.daniel.utils;
 
 import br.net.silva.daniel.annotations.client.*;
 import br.net.silva.daniel.dto.AddressDTO;
+import br.net.silva.daniel.dto.AddressRequestDTO;
 import br.net.silva.daniel.enums.TypeClientMapperEnum;
 import br.net.silva.daniel.mapper.ToAddessMapper;
 import br.net.silva.daniel.value_object.Source;
 
 import java.util.Map;
 
-public class ClientMapperUtils {
+public class ClientRequestMapperUtils {
 
     @ClientId
     public static String id(Map<String, Object> map) {
@@ -40,13 +41,26 @@ public class ClientMapperUtils {
         return ExtractMapUtils.extractMapValue(map, TypeClientMapperEnum.CLIENT.name(), "agency", Integer.class);
     }
 
+    @ClientAddressRequest
+    public static AddressRequestDTO addressRequest(Map<String, Object> map) {
+        return convertAddress(map, AddressRequestDTO.class);
+    }
+
     @ClientAddress
-    @SuppressWarnings("unchecked")
     public static AddressDTO address(Map<String, Object> map) {
+        return convertAddress(map, AddressDTO.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T convertAddress(Map<String, Object> map, Class<T> clazz) {
         if (map.containsKey(TypeClientMapperEnum.CLIENT.name())) {
             var mapClient = (Map<String, Object>) map.get(TypeClientMapperEnum.CLIENT.name());
             if (mapClient.containsKey(TypeClientMapperEnum.ADDRESS.name())) {
-                return ToAddessMapper.INSTANCE.toAddressDTO(new Source(map, null));
+                if (clazz.equals(AddressRequestDTO.class)) {
+                    return clazz.cast(ToAddessMapper.INSTANCE.toAddressRequestDTO(new Source(map, null))) ;
+                } else if (clazz.equals(AddressDTO.class)) {
+                    return clazz.cast(ToAddessMapper.INSTANCE.toAddressDTO(new Source(map, null)));
+                }
             }
         }
         return null;
