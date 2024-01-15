9fa970d1427ded51fed7ee233c83384786612352
silva01 <silva20102@gmail.com>
2024-01-15 06:53:25 -0300

    Refactored for avoid error in conversion type

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/utils/AccountMapperUtils.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/utils/AccountMapperUtils.java
@@ -7,22 +7,23 @@ import br.net.silva.daniel.utils.ExtractMapUtils;
 
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.Objects;
 
 public class AccountMapperUtils {
 
     @Number
     public Integer number(Map<String, Object> map) {
-        return ExtractMapUtils.extractMapValue(map, TypeAccountMapperEnum.ACCOUNT.name(), "number", Integer.class);
+        return Objects.requireNonNull(ExtractMapUtils.extractMapValue(map, TypeAccountMapperEnum.ACCOUNT.name(), "number", Long.class)).intValue();
     }
 
     @Agency
     public Integer agency(Map<String, Object> map) {
-        return ExtractMapUtils.extractMapValue(map, TypeAccountMapperEnum.ACCOUNT.name(), "agency", Integer.class);
+        return Objects.requireNonNull(ExtractMapUtils.extractMapValue(map, TypeAccountMapperEnum.ACCOUNT.name(), "agency", Long.class)).intValue();
     }
 
     @Balance
     public BigDecimal balance(Map<String, Object> map) {
-        return ExtractMapUtils.extractMapValue(map, TypeAccountMapperEnum.ACCOUNT.name(), "balance", BigDecimal.class);
+        return BigDecimal.valueOf(ExtractMapUtils.extractMapValue(map, TypeAccountMapperEnum.ACCOUNT.name(), "balance", Long.class));
     }
 
     @Password
