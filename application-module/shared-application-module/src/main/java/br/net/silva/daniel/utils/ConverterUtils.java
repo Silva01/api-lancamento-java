f5d12b9078d74a1bd2fb4c37bd9579f853916b55
silva01 <silva20102@gmail.com>
2024-01-15 06:53:40 -0300

    Refactored for avoid error in conversion type

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/utils/ConverterUtils.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/utils/ConverterUtils.java
@@ -1,15 +1,20 @@
 package br.net.silva.daniel.utils;
 
+import br.net.silva.daniel.adapter.LocalDateTypeAdapter;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 public class ConverterUtils {
     private static Gson gson;
     
     static {
-        gson = new Gson();
+        gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+                .create();
     }
     
     public static String convertObjectToJson(Object object) {
@@ -17,10 +22,23 @@ public class ConverterUtils {
     }
 
     public static Map<String, Object> convertJsonToMap(String json) {
-        return gson.fromJson(json, new TypeToken<Map<String, Object>>() {}.getType());
+        return dealWithValuesDoubleToInteger(gson.fromJson(json, new TypeToken<Map<String, Object>>() {}.getType()));
     }
 
     public static Map<String, String> convertJsonToInputMap(String json) {
         return gson.fromJson(json, new TypeToken<Map<String, String>>() {}.getType());
+    }
+
+    private static Map<String, Object> dealWithValuesDoubleToInteger(Map<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() instanceof Double) {
+                Double value = (Double) entry.getValue();
+                if (value % 1 == 0) {
+                    entry.setValue(value.longValue());
+                }
+            }
+        }
+
+        return map;
     }
 }
