1b17a74a063674e1fab8d137c7ef70e190117c0f
silva01 <silva20102@gmail.com>
2024-01-16 07:13:49 -0300

    Adjusted tests that were failing

---
+++ application-module/shared-application-module/src/test/java/br/net/silva/daniel/adapter/LocalDateTypeAdapterTest.java
@@ -0,0 +1,47 @@
+package br.net.silva.daniel.adapter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalDateTypeAdapterTest {
+
+    @Test
+    public void testSerialization() {
+        // Create a LocalDate
+        LocalDate localDate = LocalDate.of(2022, 1, 1);
+
+        // Create an instance of the Gson builder with the LocalDateTypeAdapter
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+                .create();
+
+        // Serialize the LocalDate to JSON
+        String json = gson.toJson(localDate);
+
+        // Check if the JSON string is as expected
+        assertEquals("\"2022-01-01\"", json);
+    }
+
+    @Test
+    public void testDeserialization() {
+        // JSON representing a LocalDate
+        String json = "\"2022-01-01\"";
+
+        // Create an instance of the Gson builder with the LocalDateTypeAdapter
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+                .create();
+
+        // Deserialize the JSON to LocalDate
+        LocalDate localDate = gson.fromJson(json, LocalDate.class);
+
+        // Check if the LocalDate is as expected
+        assertEquals(LocalDate.of(2022, 1, 1), localDate);
+    }
+
+}
