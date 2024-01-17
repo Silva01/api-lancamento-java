145ce5f1fdff37355741e6a35b91c0173f40f20c
silva01 <silva20102@gmail.com>
2024-01-17 07:04:27 -0300

    Added validate for account need exists and need be activated

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountExistsAndActiveValidate.java
@@ -0,0 +1,39 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.business.exception.AccountAlreadyActiveException;
+import br.net.silva.business.exception.AccountNotExistsException;
+import br.net.silva.business.mapper.MapToFindAccountMapper;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.shared.business.value_object.Source;
+
+import java.util.Optional;
+
+public class AccountExistsAndActiveValidate implements IValidations {
+
+    private final Repository<Optional<Account>> findAccountRepository;
+    private final MapToFindAccountMapper mapper;
+
+    public AccountExistsAndActiveValidate(Repository<Optional<Account>> findAccountRepository) {
+        this.findAccountRepository = findAccountRepository;
+        this.mapper = MapToFindAccountMapper.INSTANCE;
+    }
+
+    @Override
+    public void validate(Source input) throws GenericException {
+        var dto = this.mapper.mapToFindAccountDto(input.input());
+        var optionalAccount = findAccountRepository.exec(dto.account(), dto.agency(), dto.cpf());
+
+        if (optionalAccount.isEmpty()) {
+            throw new AccountNotExistsException("Account not exists");
+        }
+
+        var accountDto = optionalAccount.get().build();
+        if (accountDto.active()) {
+            throw new AccountAlreadyActiveException("Account already active");
+        }
+
+    }
+}
