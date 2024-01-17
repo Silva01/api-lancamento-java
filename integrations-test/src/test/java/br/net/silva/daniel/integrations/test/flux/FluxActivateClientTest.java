d2d734590bc8a301111e5a8d92468736d76ff693
silva01 <silva20102@gmail.com>
2024-01-17 07:43:58 -0300

    Added integration test for flux activate client

---
+++ integrations-test/src/test/java/br/net/silva/daniel/integrations/test/flux/FluxActivateClientTest.java
@@ -0,0 +1,93 @@
+package br.net.silva.daniel.integrations.test.flux;
+
+import br.net.silva.daniel.dto.ClientDTO;
+import br.net.silva.daniel.entity.Client;
+import br.net.silva.daniel.enums.TypeClientMapperEnum;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.integrations.test.interfaces.AbstractBuilder;
+import br.net.silva.daniel.interfaces.GenericFacadeDelegate;
+import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.shared.business.value_object.Source;
+import br.net.silva.daniel.usecase.ActivateClientUseCase;
+import br.net.silva.daniel.usecase.FindClientUseCase;
+import br.net.silva.daniel.validation.ClientExistsValidate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class FluxActivateClientTest extends AbstractBuilder {
+
+    private UseCase activeClientUseCase;
+
+    private UseCase findClientUseCase;
+
+    private IValidations clientExistsValidation;
+
+    @Mock
+    private Repository<Optional<Client>> findClientRepository;
+
+    @Mock
+    private Repository<Client> activateClientRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(findClientRepository.exec(anyString())).thenReturn(Optional.of(buildMockClient(false)));
+        when(activateClientRepository.exec(anyString())).thenReturn(buildMockClient(true));
+
+        // Use Cases
+        this.activeClientUseCase = new ActivateClientUseCase(activateClientRepository);
+        this.findClientUseCase = new FindClientUseCase(findClientRepository);
+
+        // Validations
+        this.clientExistsValidation = new ClientExistsValidate(findClientUseCase);
+    }
+
+    @Test
+    void shouldActivateClientWithSuccess() throws GenericException {
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("cpf", "12345678900");
+
+        var source = new Source(new HashMap<>(), inputMap);
+
+        Queue<UseCase> queue = new LinkedList<>();
+        queue.add(activeClientUseCase);
+
+        List<IValidations> validationsList = List.of(clientExistsValidation);
+        var facade = new GenericFacadeDelegate<>(queue, validationsList);
+
+        facade.exec(source);
+
+        var processedDto = (ClientDTO) source.map().get(TypeClientMapperEnum.CLIENT.name());
+        assertionClient(buildMockClient(true).build(), processedDto);
+    }
+
+    @Test
+    void shouldErrorDeactivateClientClientNotExists() {
+        when(findClientRepository.exec(Mockito.anyString())).thenReturn(Optional.empty());
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("cpf", "12345678900");
+
+        var source = new Source(new HashMap<>(), inputMap);
+
+        Queue<UseCase> queue = new LinkedList<>();
+        queue.add(activeClientUseCase);
+
+        List<IValidations> validationsList = List.of(clientExistsValidation);
+        var facade = new GenericFacadeDelegate<>(queue, validationsList);
+
+        var exceptionResponse = assertThrows(GenericException.class, () -> facade.exec(source));
+        Assertions.assertEquals("Client not exists in database", exceptionResponse.getMessage());
+    }
+}
