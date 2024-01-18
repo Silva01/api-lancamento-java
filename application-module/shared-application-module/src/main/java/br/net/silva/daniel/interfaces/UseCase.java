d21322d348eb885f1eece189ced8e620d3d7d914
silva01 <silva20102@gmail.com>
2024-01-18 08:19:16 -0300

    Refactored Interface Use Case for return responses

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/interfaces/UseCase.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/interfaces/UseCase.java
@@ -3,6 +3,6 @@ package br.net.silva.daniel.interfaces;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.value_object.Source;
 
-public interface UseCase {
-    void exec(Source param) throws GenericException;
+public interface UseCase<R> {
+    R exec(Source param) throws GenericException;
 }
