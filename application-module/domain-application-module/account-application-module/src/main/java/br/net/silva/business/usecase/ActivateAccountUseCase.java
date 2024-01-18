5a5933fb066f5a73badf0b9c77145c3ea2132936
silva01 <silva20102@gmail.com>
2024-01-18 08:19:52 -0300

    Refactored for return EmptyOutput

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
@@ -3,11 +3,12 @@ package br.net.silva.business.usecase;
 import br.net.silva.business.value_object.input.ActivateAccount;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.EmptyOutput;
 import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
 
-public class ActivateAccountUseCase implements UseCase {
+public class ActivateAccountUseCase implements UseCase<EmptyOutput> {
 
     private final Repository<Account> activateAccountRepository;
     private final Repository<Account> findAccountRepository;
@@ -18,12 +19,13 @@ public class ActivateAccountUseCase implements UseCase {
     }
 
     @Override
-    public void exec(Source param) throws GenericException {
+    public EmptyOutput exec(Source param) throws GenericException {
         try {
             var dto = (ActivateAccount) param.input();
             var account = findAccountRepository.exec(dto.accountNumber(), dto.agency(), dto.cpf());
             account.activate();
             activateAccountRepository.exec(account);
+            return EmptyOutput.INSTANCE;
         } catch (Exception e) {
             throw new GenericException(e.getMessage());
         }
