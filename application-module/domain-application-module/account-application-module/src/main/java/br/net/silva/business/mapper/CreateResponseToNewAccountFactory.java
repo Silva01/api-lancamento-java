6002d6446f0d7bdbf0512f7852792c0eac7ed6a6
silva01 <silva20102@gmail.com>
2024-01-19 19:16:39 -0300

    Added factory for to fill in response objects

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/mapper/CreateResponseToNewAccountFactory.java
@@ -0,0 +1,25 @@
+package br.net.silva.business.mapper;
+
+import br.net.silva.business.value_object.output.NewAccountResponse;
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.interfaces.IMapperResponse;
+import br.net.silva.daniel.interfaces.Output;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateResponseToNewAccountFactory implements IMapperResponse<AccountDTO, Output> {
+    @Override
+    public boolean accept(AccountDTO input, Output output) {
+        if (input == null || output == null) {
+            return false;
+        }
+
+        return input instanceof AccountDTO && output instanceof NewAccountResponse;
+    }
+
+    @Override
+    public void toFillIn(AccountDTO input, Output output) {
+        ((NewAccountResponse) output).setAgency(input.agency());
+        ((NewAccountResponse) output).setAccountNumber(input.number());
+    }
+}
