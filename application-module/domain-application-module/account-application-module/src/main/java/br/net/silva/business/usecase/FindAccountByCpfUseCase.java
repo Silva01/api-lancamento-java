e356ee8e6d6be2340115480a2582bfdf91f72906
silva01 <silva20102@gmail.com>
2024-01-19 19:32:07 -0300

    Refactored for use new pattern

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/FindAccountByCpfUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/FindAccountByCpfUseCase.java
@@ -2,10 +2,10 @@ package br.net.silva.business.usecase;
 
 import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.interfaces.IAccountParam;
-import br.net.silva.business.mapper.MapToFindAccountMapper;
 import br.net.silva.daniel.dto.AccountDTO;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.factory.GenericResponseFactory;
 import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
@@ -15,11 +15,11 @@ import java.util.Optional;
 public class FindAccountByCpfUseCase implements UseCase<AccountDTO> {
 
     private final Repository<Optional<Account>> findActiveAccountRepository;
-    private final MapToFindAccountMapper mapper;
+    private final GenericResponseFactory factory;
 
-    public FindAccountByCpfUseCase(Repository<Optional<Account>> findActiveAccountRepository) {
+    public FindAccountByCpfUseCase(Repository<Optional<Account>> findActiveAccountRepository, GenericResponseFactory factory) {
         this.findActiveAccountRepository = findActiveAccountRepository;
-        this.mapper = MapToFindAccountMapper.INSTANCE;
+        this.factory = factory;
     }
 
     @Override
@@ -27,8 +27,7 @@ public class FindAccountByCpfUseCase implements UseCase<AccountDTO> {
         var findAccountDto = (IAccountParam) param.input();
         var accountOptional = findActiveAccountRepository.exec(findAccountDto.cpf());
         var accountAggregate =  accountOptional.orElseThrow(() -> new AccountNotExistsException("Account not found"));
-        //TODO: aqui precisa implementar um mapper para preenchimento do objeto de retorno
-
+        factory.fillIn(accountAggregate.build(), param.output());
         return accountAggregate.build();
     }
 }
