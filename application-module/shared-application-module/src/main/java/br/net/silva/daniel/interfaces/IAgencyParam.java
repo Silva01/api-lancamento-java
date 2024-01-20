26f2e3a607f22342505138292f380c4b571c60b3
silva01 <silva20102@gmail.com>
2024-01-20 15:33:02 -0300

    Added param sharad between modules

---
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/interfaces/IAgencyParam.java
@@ -0,0 +1,5 @@
+package br.net.silva.daniel.interfaces;
+
+public interface IAgencyParam extends Input {
+    Integer agency();
+}
---
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/interfaces/ICpfParam.java
@@ -0,0 +1,5 @@
+package br.net.silva.daniel.interfaces;
+
+public interface ICpfParam extends Input {
+    String cpf();
+}
