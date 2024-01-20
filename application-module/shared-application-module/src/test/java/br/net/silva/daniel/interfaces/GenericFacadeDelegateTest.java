4e4ad621ce5570855b5d0be0e55a43e1a88b1e1f
silva01 <silva20102@gmail.com>
2024-01-20 15:56:00 -0300

    Created test for genericFacade

--- application-module/shared-application-module/src/test/java/br/net/silva/daniel/interfaces/GenericFacadeDelegateTest.java
+++ application-module/shared-application-module/src/test/java/br/net/silva/daniel/interfaces/GenericFacadeDelegateTest.java
@@ -1,7 +1,6 @@
 package br.net.silva.daniel.interfaces;
 
 import br.net.silva.daniel.exception.GenericException;
-import br.net.silva.daniel.shared.business.interfaces.IGenericOutput;
 import br.net.silva.daniel.value_object.Source;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,100 +11,91 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GenericFacadeDelegateTest {
 
     private GenericFacadeDelegate genericFacadeDelegate;
 
     @Mock
-    private UseCase useCase;
+    private UseCase<String> useCase;
 
     @Mock
     private IValidations stringNotNullValidation;
 
-//    @BeforeEach
-//    void setup() {
-//        var basicInput = new BasicTest();
-//        basicInput.setName("Teste com input");
-//        // Next create a object that type is UseCase and return the processResponse
-//        useCase = (p) -> {
-//            p.map().put("Test", basicInput);
-//        };
-//
-//        // Next create a object that type is IValidations and validate if the string is not null
-//        stringNotNullValidation = (p) -> {
-//            var basicTest = (BasicTest) p.map().get("Test");
-//            if (basicTest.getName().isEmpty()) {
-//                throw new GenericException("String must not be null");
-//            }
-//        };
-//
-//        // Next create a object that type is Queue and add the useCase
-//        Queue<UseCase> useCases = new LinkedList<>();
-//        useCases.add(useCase);
-//
-//        // Next create a object that type is List and add the stringNotNullValidation
-//        List<IValidations> validationsList = List.of(stringNotNullValidation);
-//
-//        // Finally create a object that type is GenericFacadeDelegate and pass the useCases and validationsList
-//        genericFacadeDelegate = new GenericFacadeDelegate(useCases, validationsList);
-//
-//    }
-//    @Test
-//    void mustExecuteFacadeWithSucess() throws GenericException {
-//
-//        var basicInput = new BasicTest();
-//        basicInput.setName("Teste com input");
-//
-//        var source = new Source();
-//        source.input().put("Test", "Teste com input");
-//        source.map().put("Test", basicInput);
-//
-//        genericFacadeDelegate.exec(source);
-//
-//        assertTrue(source.map().containsKey("Test"));
-//        assertNotNull(source.map().get("Test"));
-//
-//        var basicTest = (BasicTest) source.map().get("Test");
-//
-//        assertNotNull(basicTest);
-//        assertEquals("Teste com input", basicTest.getName());
-//    }
-//
-//    @Test
-//    void mustExecuteFacadeWithException() throws GenericException {
-//
-//        var basicInput = new BasicTest();
-//        basicInput.setName("");
-//
-//        var source = new Source();
-//        source.input().put("Test", "");
-//        source.map().put("Test", basicInput);
-//
-//        var exceptionResponse = Assertions.assertThrows(GenericException.class, () -> genericFacadeDelegate.exec(source));
-//        assertEquals("String must not be null", exceptionResponse.getMessage());
-//    }
-//
-//    @Test
-//    void mustExecuteFacadeWithExceptionWhenPortIsNull() throws GenericException {
-//
-//        var source = new Source();
-//        source.input().put("Test", null);
-//
-//        Assertions.assertThrows(NullPointerException.class, () -> genericFacadeDelegate.exec(source));
-//    }
-//
-//    private class BasicTest implements IGenericOutput {
-//        private String name;
-//
-//        public String getName() {
-//            return name;
-//        }
-//
-//        public void setName(String name) {
-//            this.name = name;
-//        }
-//    }
+    @BeforeEach
+    void setup() {
+        var basicInput = new BasicTest();
+        basicInput.setName("Teste com input");
+        // Next create a object that type is UseCase and return the processResponse
+        useCase = (p) -> {
+            ((BasicTest) p.output()).setName("Test List");
+            return "return test";
+        };
+
+        // Next create a object that type is IValidations and validate if the string is not null
+        stringNotNullValidation = (p) -> {
+            var basicTest = (BasicTest) p.input();
+            if (basicTest.getName().isEmpty()) {
+                throw new GenericException("String must not be null");
+            }
+        };
+
+        // Next create a object that type is Queue and add the useCase
+        Queue<UseCase<?>> useCases = new LinkedList<>();
+        useCases.add(useCase);
+
+        // Next create a object that type is List and add the stringNotNullValidation
+        List<IValidations> validationsList = List.of(stringNotNullValidation);
+
+        // Finally create a object that type is GenericFacadeDelegate and pass the useCases and validationsList
+        genericFacadeDelegate = new GenericFacadeDelegate<>(useCases, validationsList);
+
+    }
+    @Test
+    void mustExecuteFacadeWithSucess() throws GenericException {
+
+        var basicInput = new BasicTest();
+        basicInput.setName("Teste com input");
+
+        var source = new Source(new BasicTest(), basicInput);
+
+        genericFacadeDelegate.exec(source);
+
+        var basicTest = (BasicTest) source.output();
+
+        assertNotNull(basicTest);
+        assertEquals("Test List", basicTest.getName());
+    }
+
+    @Test
+    void mustExecuteFacadeWithException() {
+
+        var basicInput = new BasicTest();
+        basicInput.setName("");
+
+        var source = new Source(new BasicTest(), basicInput);
+        var exceptionResponse = Assertions.assertThrows(GenericException.class, () -> genericFacadeDelegate.exec(source));
+        assertEquals("String must not be null", exceptionResponse.getMessage());
+    }
+
+    @Test
+    void mustExecuteFacadeWithExceptionWhenPortIsNull() {
+        var source = new Source(new BasicTest(), null);
+        Assertions.assertThrows(NullPointerException.class, () -> genericFacadeDelegate.exec(source));
+    }
+
+    private class BasicTest implements Input, Output {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 
 }
