65c8d420b9c12ea35c89abf2c03d226cd3104a55
silva01 <silva20102@gmail.com>
2024-01-21 07:40:32 -0300

    Refactored business logic

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/GetInformationAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/GetInformationAccountUseCase.java
@@ -2,6 +2,8 @@ package br.net.silva.business.usecase;
 
 import br.net.silva.daniel.dto.AccountDTO;
 import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.entity.CreditCard;
+import br.net.silva.daniel.entity.Transaction;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.mapper.GenericResponseMapper;
 import br.net.silva.daniel.interfaces.ICpfParam;
@@ -9,20 +11,35 @@ import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
 
+import java.util.List;
+import java.util.Objects;
+
 public class GetInformationAccountUseCase implements UseCase<AccountDTO> {
 
     private final Repository<Account> repository;
+    private final Repository<List<Transaction>> transactionRepository;
     private final GenericResponseMapper factory;
 
-    public GetInformationAccountUseCase(Repository<Account> repository, GenericResponseMapper factory) {
+    private static final int NUMBER_OF_TRANSACTIONS = 10;
+
+    public GetInformationAccountUseCase(Repository<Account> repository, Repository<List<Transaction>> transactionRepository, GenericResponseMapper factory) {
         this.repository = repository;
+        this.transactionRepository = transactionRepository;
         this.factory = factory;
     }
 
     @Override
     public AccountDTO exec(Source param) throws GenericException {
         var cpf = (ICpfParam) param.input();
-        var account = repository.exec(cpf.cpf());
+        var accountDto = repository.exec(cpf.cpf()).build();
+        var transactions = transactionRepository.exec(cpf.cpf(), NUMBER_OF_TRANSACTIONS);
+        CreditCard creditCard = null;
+
+        if (Objects.nonNull(accountDto.creditCard())) {
+            creditCard = new CreditCard(accountDto.creditCard().number(), accountDto.creditCard().cvv(), accountDto.creditCard().flag(), accountDto.creditCard().balance(), accountDto.creditCard().expirationDate());
+        }
+
+        var account = new Account(accountDto.number(), accountDto.agency(), accountDto.balance(), accountDto.password(), accountDto.active(), accountDto.cpf(), creditCard, transactions);
         factory.fillIn(account.build(), param.output());
         return account.build();
     }
