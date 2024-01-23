e90709b8493b0864988df32084af88128c1b88e7
silva01 <silva20102@gmail.com>
2024-01-23 06:18:42 -0300

    Added new test for pass in jacoco

--- application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/dto/CreateNewAccountByCpfDTOTest.java
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/dto/CreateNewAccountByCpfDTOTest.java
@@ -25,4 +25,54 @@ class CreateNewAccountByCpfDTOTest {
 
         assertEquals("Type of class is not CreateNewAccountByCpfDTO", exceptionResponse.getMessage());
     }
+
+    @Test
+    public void testAcceptMethod() {
+        // Create an instance of CreateNewAccountByCpfDTO
+        CreateNewAccountByCpfDTO dto = new CreateNewAccountByCpfDTO("00099988877", 1234, "secretpassword");
+
+        // Call the accept method
+        dto.accept(CreateNewAccountByCpfDTO.class);
+
+        // No exception should be thrown if the accept method works as expected
+    }
+
+    @Test
+    public void testGet() {
+        // Create an instance of CreateNewAccountByCpfDTO
+        CreateNewAccountByCpfDTO dto = new CreateNewAccountByCpfDTO("00099988877", 1234, "secretpassword");
+
+        // Call the get method
+        Object result = dto.get();
+
+        // Validate that the result is the same instance
+        assertEquals(dto, result);
+    }
+
+    @Test
+    public void testAccountNumber() {
+        // Create an instance of CreateNewAccountByCpfDTO
+        CreateNewAccountByCpfDTO dto = new CreateNewAccountByCpfDTO("00099988877", 1234, "secretpassword");
+
+        // Call the accountNumber method
+        assertThrows(Exception.class, dto::accountNumber);
+    }
+
+    @Test
+    public void testBalance() {
+        // Create an instance of CreateNewAccountByCpfDTO
+        CreateNewAccountByCpfDTO dto = new CreateNewAccountByCpfDTO("00099988877", 1234, "secretpassword");
+
+        // Call the balance method
+        assertThrows(Exception.class, dto::balance);
+    }
+
+    @Test
+    public void testActive() {
+        // Create an instance of CreateNewAccountByCpfDTO
+        CreateNewAccountByCpfDTO dto = new CreateNewAccountByCpfDTO("00099988877", 1234, "secretpassword");
+
+        // Call the active method
+        assertThrows(Exception.class, dto::active);
+    }
 }
