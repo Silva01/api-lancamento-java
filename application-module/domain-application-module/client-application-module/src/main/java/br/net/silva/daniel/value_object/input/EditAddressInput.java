e3dc00dbed7176f40945ec66cc8314346b06389b
silva01 <silva20102@gmail.com>
2024-01-25 06:43:00 -0300

    Added value object of input for adit address

---
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/value_object/input/EditAddressInput.java
@@ -0,0 +1,15 @@
+package br.net.silva.daniel.value_object.input;
+
+import br.net.silva.daniel.interfaces.Input;
+
+public record EditAddressInput(
+        String cpf,
+        String street,
+        String number,
+        String complement,
+        String neighborhood,
+        String city,
+        String state,
+        String zipCode
+) implements Input {
+}
