c912e96e085f655e96e8d4ce62be6efbaa34d2b2
silva01 <silva20102@gmail.com>
2024-01-26 19:43:49 -0300

    Implemented validation for validate if exists same account with account number and new agency number and active

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountWithNewAgencyAlreadyExistsValidate.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountWithNewAgencyAlreadyExistsValidate.java
@@ -1,12 +1,28 @@
 package br.net.silva.business.validations;
 
+import br.net.silva.business.value_object.input.ChangeAgencyInput;
+import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
 
+import java.util.Optional;
+
 public class AccountWithNewAgencyAlreadyExistsValidate implements IValidations {
+
+    private final Repository<Optional<Account>> findAccountByNewAgencyNumberAndAccountNumberAndActiveRepository;
+
+    public AccountWithNewAgencyAlreadyExistsValidate(Repository<Optional<Account>> findAccountByNewAgencyNumberAndAccountNumberAndActiveRepository) {
+        this.findAccountByNewAgencyNumberAndAccountNumberAndActiveRepository = findAccountByNewAgencyNumberAndAccountNumberAndActiveRepository;
+    }
+
     @Override
     public void validate(Source input) throws GenericException {
+        var changeAgencyInput = (ChangeAgencyInput) input.input();
+        var optionalAccount = findAccountByNewAgencyNumberAndAccountNumberAndActiveRepository.exec(changeAgencyInput.accountNumber(), changeAgencyInput.newAgencyNumber());
 
+        if (optionalAccount.isPresent())
+            throw new GenericException("Account with new agency already exists");
     }
 }
