9539d6214e216ca2acdc2ff9c45ca76f530ac8eb
silva01 <silva20102@gmail.com>
2024-01-26 19:40:26 -0300

    Added class of input data for change agency

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/value_object/input/ChangeAgencyInput.java
@@ -0,0 +1,33 @@
+package br.net.silva.business.value_object.input;
+
+import br.net.silva.daniel.interfaces.IAccountParam;
+import br.net.silva.daniel.shared.business.utils.GenericErrorUtils;
+
+import java.math.BigDecimal;
+
+public record ChangeAgencyInput(
+        String cpf,
+        Integer accountNumber,
+        Integer oldAgencyNumber,
+        Integer newAgencyNumber
+) implements IAccountParam {
+    @Override
+    public BigDecimal balance() {
+        throw GenericErrorUtils.executeExceptionNotPermissionOperation();
+    }
+
+    @Override
+    public String password() {
+        throw GenericErrorUtils.executeExceptionNotPermissionOperation();
+    }
+
+    @Override
+    public boolean active() {
+        throw GenericErrorUtils.executeExceptionNotPermissionOperation();
+    }
+
+    @Override
+    public Integer agency() {
+        return oldAgencyNumber;
+    }
+}
