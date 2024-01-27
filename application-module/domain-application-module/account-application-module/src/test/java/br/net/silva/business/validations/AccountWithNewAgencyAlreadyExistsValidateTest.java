abca0db667b61b7480ec572cb54139246d691ec0
silva01 <silva20102@gmail.com>
2024-01-27 06:35:27 -0300

    Added unit test for validation AccountWithNewAgencyAlreadyExistsValidateTest

---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/validations/AccountWithNewAgencyAlreadyExistsValidateTest.java
@@ -0,0 +1,63 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.business.value_object.input.ChangeAgencyInput;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.EmptyOutput;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.shared.business.utils.CryptoUtils;
+import br.net.silva.daniel.value_object.Source;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+class AccountWithNewAgencyAlreadyExistsValidateTest {
+
+    private AccountWithNewAgencyAlreadyExistsValidate accountWithNewAgencyAlreadyExistsValidate;
+
+    @Mock
+    private Repository<Optional<Account>> findAccountByNewAgencyNumberAndAccountNumberRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(findAccountByNewAgencyNumberAndAccountNumberRepository.exec(anyInt(), anyInt())).thenReturn(Optional.empty());
+
+        accountWithNewAgencyAlreadyExistsValidate = new AccountWithNewAgencyAlreadyExistsValidate(findAccountByNewAgencyNumberAndAccountNumberRepository);
+    }
+
+    @Test
+    void shouldGiveSuccessInTheValidation() {
+        var input = new ChangeAgencyInput("99988877766", 45678, 1234, 4321);
+        var source = new Source(EmptyOutput.INSTANCE, input);
+
+        assertDoesNotThrow(() -> accountWithNewAgencyAlreadyExistsValidate.validate(source));
+        verify(findAccountByNewAgencyNumberAndAccountNumberRepository, times(1)).exec(45678, 4321);
+    }
+
+    @Test
+    void shouldGiveErrorWhenAccountWithNewAgencyAlreadyExists() {
+        var input = new ChangeAgencyInput("99988877766", 45678, 1234, 4321);
+        var source = new Source(EmptyOutput.INSTANCE, input);
+
+        when(findAccountByNewAgencyNumberAndAccountNumberRepository.exec(anyInt(), anyInt())).thenReturn(Optional.of(buildMockAccount(true)));
+
+        var exceptionResponse = assertThrows(GenericException.class, () -> accountWithNewAgencyAlreadyExistsValidate.validate(source));
+        assertEquals("Account with new agency already exists", exceptionResponse.getMessage());
+        verify(findAccountByNewAgencyNumberAndAccountNumberRepository, times(1)).exec(45678, 4321);
+    }
+
+    private Account buildMockAccount(boolean active) {
+        return new Account(1, 45678, BigDecimal.valueOf(1000), CryptoUtils.convertToSHA256("978534"), active, "99988877766", null, Collections.emptyList());
+    }
+
+}
