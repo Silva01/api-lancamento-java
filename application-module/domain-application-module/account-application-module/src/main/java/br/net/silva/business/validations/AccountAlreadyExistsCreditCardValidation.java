b427f7f1508feaa7ef427721aee4238a82654d0c
silva01 <silva20102@gmail.com>
2024-01-28 07:26:08 -0300

    Implemented validation for validate if exists credit card for account of client

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountAlreadyExistsCreditCardValidation.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountAlreadyExistsCreditCardValidation.java
@@ -1,4 +1,35 @@
 package br.net.silva.business.validations;
 
-public class AccountAlreadyExistsCreditCardValidation {
+import br.net.silva.business.value_object.input.CreateCreditCardInput;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.value_object.Source;
+
+import java.util.Optional;
+
+public class AccountAlreadyExistsCreditCardValidation implements IValidations {
+
+    private final Repository<Optional<Account>> findAccountRepository;
+
+    public AccountAlreadyExistsCreditCardValidation(Repository<Optional<Account>> findAccountRepository) {
+        this.findAccountRepository = findAccountRepository;
+    }
+
+    @Override
+    public void validate(Source input) throws GenericException {
+        var createCreditCardInput = (CreateCreditCardInput) input.input();
+        var optionalAccount = findAccountRepository.exec(createCreditCardInput.cpf(), createCreditCardInput.accountNumber(), createCreditCardInput.agency());
+
+        if (optionalAccount.isEmpty()) {
+            throw new GenericException("Account not found");
+        }
+
+        var account = optionalAccount.get();
+
+        if (account.isHaveCreditCard()) {
+            throw new GenericException("This account already have a credit card");
+        }
+    }
 }
