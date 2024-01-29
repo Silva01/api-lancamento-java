1806cbe4c8c6efdc06ca8d102bf9e1aaabc2c5a9
silva01 <silva20102@gmail.com>
2024-01-29 06:20:42 -0300

    Implemented rules for create credit card and linking in account

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/CreateNewCreditCardUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/CreateNewCreditCardUseCase.java
@@ -1,4 +1,37 @@
 package br.net.silva.business.usecase;
 
-public class CreateNewCreditCardUseCase {
+import br.net.silva.business.value_object.input.CreateCreditCardInput;
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.entity.CreditCard;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.value_object.Source;
+
+public class CreateNewCreditCardUseCase implements UseCase<AccountDTO> {
+
+    private final Repository<Account> findAccountByCpfAndAgencyAndAccountNumberRepository;
+    private final Repository<Account> saveAccountRepository;
+
+    public CreateNewCreditCardUseCase(Repository<Account> findAccountByCpfAndAgencyAndAccountNumberRepository, Repository<Account> saveAccountRepository) {
+        this.findAccountByCpfAndAgencyAndAccountNumberRepository = findAccountByCpfAndAgencyAndAccountNumberRepository;
+        this.saveAccountRepository = saveAccountRepository;
+    }
+
+    @Override
+    public AccountDTO exec(Source param) throws GenericException {
+        try {
+            var newCreditCardInput = (CreateCreditCardInput) param.input();
+            var creditCard = new CreditCard();
+
+            var account = findAccountByCpfAndAgencyAndAccountNumberRepository.exec(
+                    newCreditCardInput.cpf(), newCreditCardInput.agency(), newCreditCardInput.accountNumber());
+
+            account.vinculateCreditCard(creditCard);
+            return saveAccountRepository.exec(account).build();
+        } catch (Exception e) {
+            throw new GenericException("Generic error", e);
+        }
+    }
 }
