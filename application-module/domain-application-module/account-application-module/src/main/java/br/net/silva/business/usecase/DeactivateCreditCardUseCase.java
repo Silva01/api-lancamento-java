b55f32dbe2a531a018319e35737655c8fd85e920
silva01 <silva20102@gmail.com>
2024-01-30 19:39:25 -0300

    Implemented use case for deactivate credit card

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateCreditCardUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateCreditCardUseCase.java
@@ -1,4 +1,34 @@
 package br.net.silva.business.usecase;
 
-public class DeactivateCreditCardUseCase {
+import br.net.silva.business.exception.CreditCardNumberDifferentException;
+import br.net.silva.business.value_object.input.DeactivateCreditCardInput;
+import br.net.silva.daniel.dto.AccountDTO;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.value_object.Source;
+
+public class DeactivateCreditCardUseCase implements UseCase<AccountDTO> {
+
+    private final Repository<Account> findAccountRepository;
+    private final Repository<Account> saveAccountRepository;
+
+    public DeactivateCreditCardUseCase(Repository<Account> findAccountRepository, Repository<Account> saveAccountRepository) {
+        this.findAccountRepository = findAccountRepository;
+        this.saveAccountRepository = saveAccountRepository;
+    }
+
+    @Override
+    public AccountDTO exec(Source param) throws GenericException {
+        var input = (DeactivateCreditCardInput) param.input();
+        var account = findAccountRepository.exec(input.accountNumber(), input.agency(), input.cpf());
+
+        if (!account.build().creditCard().number().equals(input.creditCardNumber())) {
+            throw new CreditCardNumberDifferentException("Credit card number is different the credit card informed for the user");
+        }
+
+        account.deactivateCreditCard();
+        return saveAccountRepository.exec(account).build();
+    }
 }
