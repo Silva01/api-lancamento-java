4d6b3b14a8aff62159d837b70a0f525d3f2561bb
silva01 <silva20102@gmail.com>
2024-01-31 06:35:01 -0300

    Added unit test for validate credit card number

---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/validations/CreditCardNumberExistsValidateTest.java
@@ -0,0 +1,77 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.business.exception.CreditCardNotExistsException;
+import br.net.silva.business.exception.CreditCardNumberDifferentException;
+import br.net.silva.business.interfaces.AbstractAccountBuilder;
+import br.net.silva.business.value_object.input.DeactivateCreditCardInput;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.value_object.Source;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CreditCardNumberExistsValidateTest extends AbstractAccountBuilder {
+
+    private CreditCardNumberExistsValidate validate;
+
+    @Mock
+    private Repository<Account> findAccountByCpfAndAccountNumberAndAgencyRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(findAccountByCpfAndAccountNumberAndAgencyRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(buildMockAccount(true, buildMockCreditCard(true)));
+        validate = new CreditCardNumberExistsValidate(findAccountByCpfAndAccountNumberAndAgencyRepository);
+    }
+
+    @Test
+    void shouldValidateCreditCardNumberExistsWithSuccess() {
+        var input = new DeactivateCreditCardInput("99988877766", 1, 45678, "99988877766");
+        var source = new Source(input);
+
+        assertDoesNotThrow(() -> validate.validate(source));
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(input.accountNumber(), input.agency(), input.cpf());
+    }
+
+    @Test
+    void shouldErrorAtValidateCreditCardNumberNotExistsInTheAccount() {
+        when(findAccountByCpfAndAccountNumberAndAgencyRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(buildMockAccount(true, null));
+        var input = new DeactivateCreditCardInput("99988877766", 1, 45678, "99988877766");
+        var source = new Source(input);
+
+        var responseException = assertThrows(CreditCardNotExistsException.class, () -> validate.validate(source));
+        assertEquals("Credit card not exists in the account", responseException.getMessage());
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(input.accountNumber(), input.agency(), input.cpf());
+    }
+
+    @Test
+    void shouldErrorAtValidateCreditCardNumberDifferentWithAccountHave() {
+        var input = new DeactivateCreditCardInput("99988877766", 1, 45678, "123456");
+        var source = new Source(input);
+
+        var responseException = assertThrows(CreditCardNumberDifferentException.class, () -> validate.validate(source));
+        assertEquals("Credit Card number is different at register in account", responseException.getMessage());
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(input.accountNumber(), input.agency(), input.cpf());
+    }
+
+    @Test
+    void shouldErrorAtValidateCreditCardNumberDeactivatedInTheAccount() {
+        when(findAccountByCpfAndAccountNumberAndAgencyRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(buildMockAccount(true, buildMockCreditCard(false)));
+        var input = new DeactivateCreditCardInput("99988877766", 1, 45678, "99988877766");
+        var source = new Source(input);
+
+        var responseException = assertThrows(CreditCardNotExistsException.class, () -> validate.validate(source));
+        assertEquals("Credit card deactivated in the account", responseException.getMessage());
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(input.accountNumber(), input.agency(), input.cpf());
+    }
+
+}
