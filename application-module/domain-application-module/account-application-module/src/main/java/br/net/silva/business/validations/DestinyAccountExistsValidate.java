52c305aada0b6342248ef652328b0f87e6d63dc0
silva01 <silva20102@gmail.com>
2024-02-01 19:29:46 -0300

    Created validation to validate if account of destiny exists in database

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/DestinyAccountExistsValidate.java
@@ -0,0 +1,12 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.value_object.Source;
+
+public class DestinyAccountExistsValidate implements IValidations {
+    @Override
+    public void validate(Source input) throws GenericException {
+
+    }
+}
