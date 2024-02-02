1ac9c002a02b8c9378acbe04b289feb9f39752f8
silva01 <silva20102@gmail.com>
2024-02-02 06:00:00 -0300

    Implemented validation of validate destiny current account

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/DestinyAccountExistsValidate.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/DestinyAccountExistsValidate.java
@@ -1,12 +1,30 @@
 package br.net.silva.business.validations;
 
+import br.net.silva.business.exception.AccountNotExistsException;
+import br.net.silva.business.value_object.input.RegisterTransactionInput;
+import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
 
-public class DestinyAccountExistsValidate implements IValidations {
-    @Override
-    public void validate(Source input) throws GenericException {
+import java.util.Optional;
 
+public class DestinyAccountExistsValidate implements IValidations {
+
+    private final Repository<Optional<Account>> findDetinyAccountRepository;
+
+    public DestinyAccountExistsValidate(Repository<Optional<Account>> findDetinyAccountRepository) {
+        this.findDetinyAccountRepository = findDetinyAccountRepository;
+    }
+
+    @Override
+    public void validate(Source param) throws GenericException {
+        var input = (RegisterTransactionInput) param.input();
+        var destinyAccount = findDetinyAccountRepository.exec(input.destinyAccountNumber(), input.destinyAgency());
+
+        if (destinyAccount.isEmpty()) {
+            throw new AccountNotExistsException("Destiny account not found");
+        }
     }
 }
