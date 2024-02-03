fb365f50f8cc1258b50fa47e0ed7a9c12035ca39
silva01 <silva20102@gmail.com>
2024-02-03 14:27:20 -0300

    Refactored interface for receive transaction type

--- core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/strategy/ICalculation.java
+++ core-module/domain-business-module/current-account-module/src/main/java/br/net/silva/daniel/strategy/ICalculation.java
@@ -1,11 +1,12 @@
 package br.net.silva.daniel.strategy;
 
 import br.net.silva.daniel.dto.TransactionDTO;
+import br.net.silva.daniel.enuns.TransactionTypeEnum;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @FunctionalInterface
 public interface ICalculation {
-    BigDecimal calculate(List<TransactionDTO> transactions);
+    BigDecimal calculate(List<TransactionDTO> transactions, TransactionTypeEnum type);
 }
