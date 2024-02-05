3ca1629f09acce1af40d4aa6818864ac0a936c5e
silva01 <silva20102@gmail.com>
2024-02-05 06:13:13 -0300

    Added validation for validate credit card if transactions are going of kind credit

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/TransactionIfCreditCardValidation.java
@@ -0,0 +1,28 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.business.value_object.input.BatchTransactionInput;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.enuns.TransactionTypeEnum;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.interfaces.IValidations;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.value_object.Source;
+
+public class TransactionIfCreditCardValidation implements IValidations {
+
+    private final CreditCardNumberExistsValidate creditCardNumberExistsValidate;
+
+    public TransactionIfCreditCardValidation(Repository<Account> findAccountByCpfAndAccountNumberAndAgencyRepository) {
+        this.creditCardNumberExistsValidate = new CreditCardNumberExistsValidate(findAccountByCpfAndAccountNumberAndAgencyRepository);
+    }
+
+    @Override
+    public void validate(Source param) throws GenericException {
+        var input = (BatchTransactionInput) param.input();
+        final boolean hasTransactionCredit = input.batchTransaction().stream().anyMatch(transaction -> TransactionTypeEnum.CREDIT.equals(transaction.type()));
+
+        if (hasTransactionCredit) {
+            creditCardNumberExistsValidate.validate(param);
+        }
+    }
+}
