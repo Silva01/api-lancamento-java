c515f5b952e83ac84be677f1d7412bf6a471c018
silva01 <silva20102@gmail.com>
2024-02-05 06:47:31 -0300

    Added unit test for validation of transactions with credit card

---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/validations/TransactionIfCreditCardValidationTest.java
@@ -0,0 +1,129 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.business.interfaces.AbstractAccountBuilder;
+import br.net.silva.business.value_object.input.AccountInput;
+import br.net.silva.business.value_object.input.BatchTransactionInput;
+import br.net.silva.daniel.entity.Account;
+import br.net.silva.daniel.enuns.TransactionTypeEnum;
+import br.net.silva.daniel.exception.GenericException;
+import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.value_object.Source;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class TransactionIfCreditCardValidationTest extends AbstractAccountBuilder {
+
+    private TransactionIfCreditCardValidation transactionIfCreditCardValidation;
+
+    @Mock
+    private Repository<Account> findAccountByCpfAndAccountNumberAndAgencyRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(findAccountByCpfAndAccountNumberAndAgencyRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(buildMockAccount(true, buildMockCreditCard(true)));
+        transactionIfCreditCardValidation = new TransactionIfCreditCardValidation(findAccountByCpfAndAccountNumberAndAgencyRepository);
+    }
+
+    @Test
+    void shouldValidateTransactionIfCreditCardWithSuccess() {
+        var sourceAccount = new AccountInput(1, 45678, "978534");
+        var destinyAccount = new AccountInput(2, 99999, "978534");
+
+        var transaction1 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45678);
+        var transaction2 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45678);
+        var transaction3 = buildMockInputTransaction(TransactionTypeEnum.DEBIT, null, null);
+
+        var input = new BatchTransactionInput(sourceAccount, destinyAccount, List.of(transaction1, transaction2, transaction3));
+
+        var source = new Source(input);
+
+        assertDoesNotThrow(() -> transactionIfCreditCardValidation.validate(source));
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(2)).exec(anyInt(), anyInt(), anyString());
+    }
+
+    @Test
+    void shouldValidateTransactionOnlyDebitWithSuccess() {
+        var sourceAccount = new AccountInput(1, 45678, "978534");
+        var destinyAccount = new AccountInput(2, 99999, "978534");
+
+        var transaction3 = buildMockInputTransaction(TransactionTypeEnum.DEBIT, null, null);
+
+        var input = new BatchTransactionInput(sourceAccount, destinyAccount, List.of(transaction3));
+
+        var source = new Source(input);
+
+        assertDoesNotThrow(() -> transactionIfCreditCardValidation.validate(source));
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, never()).exec(anyInt(), anyInt(), anyString());
+    }
+
+    @Test
+    void shouldErrorAtValidateTransactionCreditCardNumberDifferent() {
+        var sourceAccount = new AccountInput(1, 45678, "978534");
+        var destinyAccount = new AccountInput(2, 99999, "978534");
+
+        var transaction1 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877788", 45678);
+        var transaction2 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45678);
+        var transaction3 = buildMockInputTransaction(TransactionTypeEnum.DEBIT, null, null);
+
+        var input = new BatchTransactionInput(sourceAccount, destinyAccount, List.of(transaction1, transaction2, transaction3));
+
+        var source = new Source(input);
+
+        var responseException = assertThrows(GenericException.class, () -> transactionIfCreditCardValidation.validate(source));
+        assertEquals("Credit Card number is different at register in account", responseException.getMessage());
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(anyInt(), anyInt(), anyString());
+    }
+
+    @Test
+    void shouldErrorAtValidateTransactionCreditCardDeactivated() {
+        when(findAccountByCpfAndAccountNumberAndAgencyRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(buildMockAccount(true, buildMockCreditCard(false)));
+        var sourceAccount = new AccountInput(1, 45678, "978534");
+        var destinyAccount = new AccountInput(2, 99999, "978534");
+
+        var transaction1 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45678);
+        var transaction2 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45678);
+        var transaction3 = buildMockInputTransaction(TransactionTypeEnum.DEBIT, null, null);
+
+        var input = new BatchTransactionInput(sourceAccount, destinyAccount, List.of(transaction1, transaction2, transaction3));
+
+        var source = new Source(input);
+
+        var responseException = assertThrows(GenericException.class, () -> transactionIfCreditCardValidation.validate(source));
+        assertEquals("Credit card deactivated in the account", responseException.getMessage());
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(anyInt(), anyInt(), anyString());
+    }
+
+    @Test
+    void shouldErrorAtValidateTransactionCvvIsDifferent() {
+        var sourceAccount = new AccountInput(1, 45678, "978534");
+        var destinyAccount = new AccountInput(2, 99999, "978534");
+
+        var transaction1 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45699);
+        var transaction2 = buildMockInputTransaction(TransactionTypeEnum.CREDIT, "99988877766", 45678);
+        var transaction3 = buildMockInputTransaction(TransactionTypeEnum.DEBIT, null, null);
+
+        var input = new BatchTransactionInput(sourceAccount, destinyAccount, List.of(transaction1, transaction2, transaction3));
+
+        var source = new Source(input);
+
+        var responseException = assertThrows(GenericException.class, () -> transactionIfCreditCardValidation.validate(source));
+        assertEquals("Credit Card CVV is different at register in account", responseException.getMessage());
+
+        verify(findAccountByCpfAndAccountNumberAndAgencyRepository, times(1)).exec(anyInt(), anyInt(), anyString());
+    }
+
+}
