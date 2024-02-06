77eeb609db4d938ba8ed3b78688893f51c4e7595
silva01 <silva20102@gmail.com>
2024-02-06 06:02:29 -0300

    Refactored validation for validate credit card in use in the transaction

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/TransactionIfCreditCardValidation.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/TransactionIfCreditCardValidation.java
@@ -1,7 +1,10 @@
 package br.net.silva.business.validations;
 
+import br.net.silva.business.exception.AccountNotExistsException;
+import br.net.silva.business.exception.CreditCardNotExistsException;
 import br.net.silva.business.value_object.input.BatchTransactionInput;
-import br.net.silva.business.value_object.input.DeactivateCreditCardInput;
+import br.net.silva.business.value_object.input.TransactionInput;
+import br.net.silva.daniel.dto.CreditCardDTO;
 import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.enuns.TransactionTypeEnum;
 import br.net.silva.daniel.exception.GenericException;
@@ -9,30 +12,55 @@ import br.net.silva.daniel.interfaces.IValidations;
 import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
 
+import java.util.Optional;
+
 public class TransactionIfCreditCardValidation implements IValidations {
 
-    private final CreditCardNumberExistsValidate creditCardNumberExistsValidate;
+    private final Repository<Optional<Account>> findAccountRepository;
 
-    public TransactionIfCreditCardValidation(Repository<Account> findAccountByCpfAndAccountNumberAndAgencyRepository) {
-        this.creditCardNumberExistsValidate = new CreditCardNumberExistsValidate(findAccountByCpfAndAccountNumberAndAgencyRepository);
+    public TransactionIfCreditCardValidation(Repository<Optional<Account>> findAccountRepository) {
+        this.findAccountRepository = findAccountRepository;
     }
+
 
     @Override
     public void validate(Source param) throws GenericException {
         var input = (BatchTransactionInput) param.input();
-        final var transactionCreditList = input.batchTransaction().stream().filter(transaction -> TransactionTypeEnum.CREDIT.equals(transaction.type())).toList();
+        var optionalAccount = findAccountRepository.exec(input.sourceAccount().accountNumber(), input.sourceAccount().agency(), input.sourceAccount().cpf());
 
-        if (!transactionCreditList.isEmpty()) {
-            for (var transaction : transactionCreditList) {
-                var inputValidation = new DeactivateCreditCardInput(
-                        input.sourceAccount().cpf(),
-                        input.sourceAccount().accountNumber(),
-                        input.sourceAccount().agency(),
-                        transaction.creditCardNumber());
+        var creditCardDto = validate(optionalAccount);
 
-                var source = new Source(inputValidation);
-                creditCardNumberExistsValidate.validate(source);
-            }
+        var transactionsWithError = input.batchTransaction()
+                .stream()
+                .filter(transaction -> TransactionTypeEnum.CREDIT.equals(transaction.type()))
+                .filter(transaction -> validateInfoCreditCard(transaction, creditCardDto))
+                .toList();
+
+        if (!transactionsWithError.isEmpty()) {
+            throw new GenericException("Credit card number or cvv is different at register in account");
         }
+    }
+
+    private static CreditCardDTO validate(Optional<Account> optionalAccount) throws AccountNotExistsException, CreditCardNotExistsException {
+        if (optionalAccount.isEmpty()) {
+            throw new AccountNotExistsException("Account not exists");
+        }
+
+        var account = optionalAccount.get();
+
+        if (!account.isHaveCreditCard()) {
+            throw new CreditCardNotExistsException("Credit card not exists in the account");
+        }
+
+        var creditCardDto = account.build().creditCard();
+
+        if (!creditCardDto.active()) {
+            throw new CreditCardNotExistsException("Credit card deactivated in the account");
+        }
+        return creditCardDto;
+    }
+
+    private boolean validateInfoCreditCard(TransactionInput transaction, CreditCardDTO creditCardDto) {
+        return !creditCardDto.number().equals(transaction.creditCardNumber()) || !creditCardDto.cvv().equals(transaction.creditCardCvv());
     }
 }
