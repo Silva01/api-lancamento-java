9cd36871cb0177f4a7f83ee5cd132e2064fd2efb
silva01 <silva20102@gmail.com>
2024-02-25 18:54:46 -0300

    Created unit test for create Transaction output

---
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/factory/TransactionOutputFactoryTest.java
@@ -0,0 +1,36 @@
+package br.net.silva.business.factory;
+
+import br.net.silva.business.build.TransactionOutputBuilder;
+import br.net.silva.business.interfaces.AbstractAccountBuilder;
+import br.net.silva.daniel.enuns.TransactionTypeEnum;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionOutputFactoryTest extends AbstractAccountBuilder {
+
+    @Test
+    void shouldCreateTransactionOutputWithSuccess() {
+        var transactionMock = buildMockTransaction(TransactionTypeEnum.DEBIT).build();
+        var transactionOutput = TransactionOutputBuilder.buildFullTransactionsOutput().createFrom(List.of(transactionMock));
+
+        assertNotNull(transactionOutput);
+        assertEquals(1, transactionOutput.size());
+
+        transactionOutput.forEach(output -> {
+            assertEquals(transactionMock.id(), output.id());
+            assertEquals(transactionMock.description(), output.description());
+            assertEquals(transactionMock.price(), output.price());
+            assertEquals(transactionMock.quantity(), output.quantity());
+            assertEquals(transactionMock.type(), output.type());
+            assertEquals(transactionMock.originAccountNumber(), output.originAccountNumber());
+            assertEquals(transactionMock.destinationAccountNumber(), output.destinationAccountNumber());
+            assertEquals(transactionMock.idempotencyId(), output.idempotencyId());
+            assertEquals(transactionMock.creditCardNumber(), output.creditCardNumber());
+            assertEquals(transactionMock.creditCardCvv(), output.creditCardCvv());
+        });
+    }
+
+}
