f6e50fb32fc8ffa9518dbc127e57f703af2ea9e9
silva01 <silva20102@gmail.com>
2024-02-25 18:15:04 -0300

    Created output objects in module account application for response

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/value_object/output/AccountOutput.java
@@ -0,0 +1,20 @@
+package br.net.silva.business.value_object.output;
+
+import br.net.silva.daniel.dto.CreditCardDTO;
+import br.net.silva.daniel.dto.TransactionDTO;
+import br.net.silva.daniel.interfaces.Output;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record AccountOutput(
+        Integer number,
+        Integer agency,
+        BigDecimal balance,
+        String password,
+        boolean active,
+        String cpf,
+        List<TransactionDTO> transactions,
+        CreditCardDTO creditCard
+) implements Output {
+}
