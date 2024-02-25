6cda351d068bc08279781ad16979436d4beecd53
silva01 <silva20102@gmail.com>
2024-02-25 18:25:27 -0300

    Created director for create objects of type transaction output

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/build/TransactionOutputBuilder.java
@@ -0,0 +1,32 @@
+package br.net.silva.business.build;
+
+import br.net.silva.business.factory.TransactionOutputFactory;
+import br.net.silva.business.value_object.output.TransactionOutput;
+import br.net.silva.daniel.dto.TransactionDTO;
+
+import java.util.List;
+
+public final class TransactionOutputBuilder {
+
+    private TransactionOutputBuilder() {
+    }
+
+    public static List<TransactionOutput> buildFullTransactionsOutput(List<TransactionDTO> transactions) {
+        return transactions
+                .stream()
+                .map(transaction -> TransactionOutputFactory
+                        .createOutput()
+                        .withId(transaction.id())
+                        .withDescription(transaction.description())
+                        .withPrice(transaction.price())
+                        .withQuantity(transaction.quantity())
+                        .withType(transaction.type())
+                        .withOriginAccountNumber(transaction.originAccountNumber())
+                        .withDestinationAccountNumber(transaction.destinationAccountNumber())
+                        .withIdempotencyId(transaction.idempotencyId())
+                        .withCreditCardNumber(transaction.creditCardNumber())
+                        .andWithCreditCardCvv(transaction.creditCardCvv())
+                        .build())
+                .toList();
+    }
+}
