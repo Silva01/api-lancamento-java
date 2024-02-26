e56ac205e29a168e56a0c7ca40bbe74f08c198c8
silva01 <silva20102@gmail.com>
2024-02-26 05:30:39 -0300

    Removed imports not used

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/value_object/output/AccountOutput.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/value_object/output/AccountOutput.java
@@ -1,7 +1,5 @@
 package br.net.silva.business.value_object.output;
 
-import br.net.silva.daniel.dto.CreditCardDTO;
-import br.net.silva.daniel.dto.TransactionDTO;
 import br.net.silva.daniel.interfaces.Output;
 
 import java.math.BigDecimal;
