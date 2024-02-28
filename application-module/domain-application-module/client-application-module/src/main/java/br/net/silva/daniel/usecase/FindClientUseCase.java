4bed7fc934151c7888bea1b5e949eb93adff08e8
silva01 <silva20102@gmail.com>
2024-02-28 05:40:46 -0300

    Refactored for use new output objects from slayer application

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/FindClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/FindClientUseCase.java
@@ -1,17 +1,18 @@
 package br.net.silva.daniel.usecase;
 
-import br.net.silva.daniel.dto.ClientDTO;
+import br.net.silva.daniel.build.ClientBuilder;
 import br.net.silva.daniel.entity.Client;
 import br.net.silva.daniel.exception.ClientNotExistsException;
-import br.net.silva.daniel.mapper.GenericResponseMapper;
 import br.net.silva.daniel.interfaces.ICpfParam;
 import br.net.silva.daniel.interfaces.UseCase;
+import br.net.silva.daniel.mapper.GenericResponseMapper;
 import br.net.silva.daniel.repository.Repository;
 import br.net.silva.daniel.value_object.Source;
+import br.net.silva.daniel.value_object.output.ClientOutput;
 
 import java.util.Optional;
 
-public class FindClientUseCase implements UseCase<ClientDTO> {
+public class FindClientUseCase implements UseCase<ClientOutput> {
 
     private final Repository<Optional<Client>> findClientRepository;
     private final GenericResponseMapper factory;
@@ -22,11 +23,11 @@ public class FindClientUseCase implements UseCase<ClientDTO> {
     }
 
     @Override
-    public ClientDTO exec(Source param) throws ClientNotExistsException {
+    public ClientOutput exec(Source param) throws ClientNotExistsException {
         var clientRequestDTO = (ICpfParam) param.input();
         var optionalClient = findClientRepository.exec(clientRequestDTO.cpf());
         var client = optionalClient.orElseThrow(() -> new ClientNotExistsException("Client not exists in database"));
         factory.fillIn(client.build(), param.output());
-        return client.build();
+        return ClientBuilder.buildFullClientOutput().createFrom(client.build());
     }
 }
