199b33f2c006f7e71877b1384249258025850398
silva01 <silva20102@gmail.com>
2024-03-02 07:17:17 -0300

    Added initial template for build api

---
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/AppModuleApplication.java
@@ -0,0 +1,13 @@
+package silva.daniel.project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppModuleApplication.class, args);
+    }
+
+}
---
+++ driver-module/api-module/app-module/src/test/java/silva/daniel/project/app/AppModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package silva.daniel.project.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
