59a1645356a851d78ef63c67f61c936f0516705c
silva01 <silva20102@gmail.com>
2024-03-09 10:50:35 -0300

    Refactored method for find by id

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/repository/FindApplicationBaseRepository.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/repository/FindApplicationBaseRepository.java
@@ -1,8 +1,9 @@
 package br.net.silva.daniel.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FindApplicationBaseRepository<R> {
-    R findById();
+    Optional<R> findById();
     List<R> findAll();
 }
