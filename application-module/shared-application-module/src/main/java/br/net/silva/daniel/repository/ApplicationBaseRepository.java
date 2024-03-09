da9ca46db67e079ee0513fbdefb351e1c60c2428
silva01 <silva20102@gmail.com>
2024-03-09 10:45:31 -0300

    Added base interface for repository

---
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/repository/ApplicationBaseRepository.java
@@ -0,0 +1,4 @@
+package br.net.silva.daniel.repository;
+
+public interface ApplicationBaseRepository<T> extends FindApplicationBaseRepository<T>, SaveApplicationBaseRepository<T>, DeleteApplicationBaseRepository {
+}
