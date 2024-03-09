a64df9cc5a27fddd6dd00a36e45c12f0253a1652
silva01 <silva20102@gmail.com>
2024-03-09 11:15:11 -0300

    Refactored EditClientUseCase for use new repository base

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/EditClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/EditClientUseCase.java
@@ -5,21 +5,21 @@ import br.net.silva.daniel.exception.ClientNotExistsException;
 import br.net.silva.daniel.exception.GenericException;
 import br.net.silva.daniel.interfaces.UseCase;
 import br.net.silva.daniel.mapper.GenericResponseMapper;
-import br.net.silva.daniel.repository.Repository;
+import br.net.silva.daniel.repository.ApplicationBaseRepository;
+import br.net.silva.daniel.repository.FindApplicationBaseRepository;
+import br.net.silva.daniel.repository.SaveApplicationBaseRepository;
 import br.net.silva.daniel.value_object.Source;
 import br.net.silva.daniel.value_object.input.EditClientInput;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
-import java.util.Optional;
-
 public class EditClientUseCase implements UseCase<ClientOutput> {
-    private final Repository<Optional<ClientOutput>> findRepository;
-    private final Repository<ClientOutput> saveRepository;
+    private final FindApplicationBaseRepository<ClientOutput> findRepository;
+    private final SaveApplicationBaseRepository<ClientOutput> saveRepository;
     private final GenericResponseMapper mapper;
 
-    public EditClientUseCase(Repository<Optional<ClientOutput>> findRepository, Repository<ClientOutput> saveRepository, GenericResponseMapper mapper) {
-        this.findRepository = findRepository;
-        this.saveRepository = saveRepository;
+    public EditClientUseCase(ApplicationBaseRepository<ClientOutput> baseRepository, GenericResponseMapper mapper) {
+        this.findRepository = baseRepository;
+        this.saveRepository = baseRepository;
         this.mapper = mapper;
     }
 
@@ -27,13 +27,13 @@ public class EditClientUseCase implements UseCase<ClientOutput> {
     public ClientOutput exec(Source param) throws GenericException {
         try {
             var input = (EditClientInput) param.input();
-            var clientOutput = findRepository.exec(input.cpf()).orElseThrow(() -> new ClientNotExistsException("Client not exists"));
+            var clientOutput = findRepository.findById(input).orElseThrow(() -> new ClientNotExistsException("Client not exists"));
 
             var client = ClientBuilder.buildAggregate().createFrom(clientOutput);
             client.editName(input.name());
             client.editTelephone(input.telephone());
 
-            var response = saveRepository.exec(client);
+            var response = saveRepository.save(ClientBuilder.buildFullClientOutput().createFrom(client.build()));
 
             mapper.fillIn(ClientBuilder.buildFullClientDto().createFrom(response), param.output());
             return response;
