90b549c10fe6c9a3599559fae9a56de47ca98b0f
silva01 <silva20102@gmail.com>
2024-03-09 21:02:02 -0300

    Refactored UseCaseBuilder for support new pattern

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/build/UseCaseBuilder.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/build/UseCaseBuilder.java
@@ -1,16 +1,54 @@
 package br.net.silva.daniel.build;
 
-import br.net.silva.daniel.interfaces.Output;
-import br.net.silva.daniel.interfaces.UseCase;
-import br.net.silva.daniel.utils.ObjectCreatorUtils;
+import br.net.silva.daniel.interfaces.spec.MapperSpec;
+import br.net.silva.daniel.interfaces.spec.PrepareUseCaseSpec;
+import br.net.silva.daniel.interfaces.spec.RepositorySpec;
+import br.net.silva.daniel.mapper.GenericResponseMapper;
+import br.net.silva.daniel.repository.ApplicationBaseRepository;
+import br.net.silva.daniel.shared.business.utils.GenericErrorUtils;
+
+import java.lang.reflect.Constructor;
 
 public abstract class UseCaseBuilder {
 
-    private UseCaseBuilder() {
-        throw new IllegalStateException("Utility class");
+    public static PrepareUseCaseSpec make() {
+        return new Builder();
     }
 
-    public static <T extends ObjectBuilder<UseCase<Output>>> T buildUseCase(Class<T> type) throws Exception {
-        return ObjectCreatorUtils.createObjectWithEmptyConstructor(type);
+    public static class Builder implements PrepareUseCaseSpec, RepositorySpec, MapperSpec {
+
+        private Class<?> clazz;
+        private ApplicationBaseRepository<?> baseRepository;
+        private GenericResponseMapper mapper;
+
+        @Override
+        public <T> br.net.silva.daniel.build.Builder<T> withGenericMapper(GenericResponseMapper mapper) {
+            return () -> {
+                Constructor<T>[] constructors = (Constructor<T>[]) clazz.getConstructors();
+                for (Constructor<T> constructor : constructors) {
+                    if (constructor.getParameterCount() == 1) {
+                        return (T) constructor.newInstance(baseRepository);
+                    } else if (constructor.getParameterCount() == 2) {
+                        return (T) constructor.newInstance(baseRepository, mapper);
+                    }
+
+                    throw GenericErrorUtils.executeException("Invalid constructor for use case");
+                }
+
+                throw GenericErrorUtils.executeException("Class does not have a constructor with the required parameters");
+            };
+        }
+
+        @Override
+        public <T> RepositorySpec prepareUseCaseFrom(Class<T> clazz) {
+            this.clazz = clazz;
+            return this;
+        }
+
+        @Override
+        public <T> MapperSpec withBaseRepository(ApplicationBaseRepository<T> baseRepository) {
+            this.baseRepository = baseRepository;
+            return this;
+        }
     }
 }
