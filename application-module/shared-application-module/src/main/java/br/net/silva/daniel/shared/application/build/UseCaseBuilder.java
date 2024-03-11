9a51d8959c50bb72997e8dd77b67bd1f4ba8dd73
silva01 <silva20102@gmail.com>
2024-03-11 07:12:07 -0300

    Refactored use case builder for support create only 1 use case

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/build/UseCaseBuilder.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/build/UseCaseBuilder.java
@@ -9,53 +9,43 @@ import br.net.silva.daniel.shared.application.repository.ApplicationBaseReposito
 import br.net.silva.daniel.shared.business.utils.GenericErrorUtils;
 
 import java.lang.reflect.Constructor;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
-public abstract class UseCaseBuilder {
+public interface UseCaseBuilder {
 
-    private UseCaseBuilder() {
-    }
-
-    public static PrepareUseCaseSpec make() {
+    static PrepareUseCaseSpec make() {
         return new Builder();
     }
 
-    public static class Builder implements PrepareUseCaseSpec, RepositorySpec, MapperSpec {
+    class Builder implements PrepareUseCaseSpec, RepositorySpec, MapperSpec<UseCase> {
 
-        private List<Class<?>> clazzList;
+        private Class clazz;
         private ApplicationBaseRepository<?> baseRepository;
         private GenericResponseMapper mapper;
 
         @Override
-        public br.net.silva.daniel.shared.application.build.Builder<Queue<UseCase>> withGenericMapper(GenericResponseMapper mapper) {
+        public br.net.silva.daniel.shared.application.build.Builder<UseCase> withGenericMapper(GenericResponseMapper mapper) {
             this.mapper = mapper;
             return () -> {
-                Queue<UseCase> useCaseQueue = new LinkedList<>();
-                clazzList.forEach(clazz -> {
-                    try {
-                        Constructor[] constructors = clazz.getConstructors();
-                        for (Constructor constructor : constructors) {
-                            if (constructor.getParameterCount() == 1) {
-                                useCaseQueue.add((UseCase) constructor.newInstance(baseRepository));
-                            } else if (constructor.getParameterCount() == 2) {
-                                useCaseQueue.add((UseCase) constructor.newInstance(baseRepository, this.mapper));
-                            }
+                try {
+                    Constructor[] constructors = clazz.getConstructors();
+                    for (Constructor constructor : constructors) {
+                        if (constructor.getParameterCount() == 1) {
+                            return (UseCase) constructor.newInstance(baseRepository);
+                        } else if (constructor.getParameterCount() == 2) {
+                            return (UseCase) constructor.newInstance(baseRepository, this.mapper);
                         }
-                    } catch (Exception e) {
-                        throw GenericErrorUtils.executeException("Invalid constructor for use case");
                     }
-                });
+                } catch (Exception e) {
+                    throw GenericErrorUtils.executeException("Invalid constructor for use case");
+                }
 
-                return useCaseQueue;
+                throw GenericErrorUtils.executeException("Invalid constructor for use case");
             };
         }
 
         @Override
-        public RepositorySpec prepareUseCasesFrom(Class... clazz) {
-            this.clazzList = Arrays.asList(clazz);
+        public RepositorySpec prepareUseCaseFrom(Class clazz) {
+            this.clazz = clazz;
             return this;
         }
 
