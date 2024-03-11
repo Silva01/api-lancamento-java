83a26f3e363cea3fb1b30099aaa9f99973d9caea
silva01 <silva20102@gmail.com>
2024-03-11 07:10:50 -0300

    Refactored for support generics

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/MapperSpec.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/MapperSpec.java
@@ -3,6 +3,6 @@ package br.net.silva.daniel.shared.application.interfaces.spec;
 import br.net.silva.daniel.shared.application.build.Builder;
 import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
 
-public interface MapperSpec {
-    <T> Builder<T> withGenericMapper(GenericResponseMapper mapper);
+public interface MapperSpec<T> {
+    Builder<T> withGenericMapper(GenericResponseMapper mapper);
 }
