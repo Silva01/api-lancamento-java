5cac904426a416524f521d675a9e3162ecb47b27
silva01 <silva20102@gmail.com>
2024-03-11 07:09:06 -0300

    Added component with implementation repository for account

---
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/domain/account/component/AccountComponent.java
@@ -0,0 +1,55 @@
+package silva.daniel.project.app.domain.account.component;
+
+import br.net.silva.business.value_object.output.AccountOutput;
+import br.net.silva.daniel.shared.application.repository.ApplicationBaseRepository;
+import br.net.silva.daniel.shared.application.repository.ParamRepository;
+import org.springframework.stereotype.Component;
+import silva.daniel.project.app.domain.account.entity.Account;
+import silva.daniel.project.app.domain.account.entity.AccountKey;
+import silva.daniel.project.app.domain.account.repository.AccountRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class AccountComponent implements ApplicationBaseRepository<AccountOutput> {
+
+    private final AccountRepository repository;
+
+    public AccountComponent(AccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public boolean deleteById(ParamRepository param) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAll() {
+        return false;
+    }
+
+    @Override
+    public Optional<AccountOutput> findById(ParamRepository param) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<AccountOutput> findAll() {
+        return null;
+    }
+
+    @Override
+    public AccountOutput save(AccountOutput entity) {
+        var key = new AccountKey(entity.number(), entity.agency());
+        var account = new Account(key, entity.balance(), entity.password(), entity.active(), entity.cpf(), null, null);
+        var newAccount = repository.save(account);
+        return new AccountOutput(newAccount.getKeys().getNumber(), newAccount.getKeys().getBankAgencyNumber(), newAccount.getBalance(), newAccount.getPassword(), newAccount.isActive(), newAccount.getCpf(), null, null);
+    }
+
+    @Override
+    public void saveAll(List<AccountOutput> paramList) {
+
+    }
+}
