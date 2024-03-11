e5828441edec22133420c9ce7fbfaa02c9e58a41
silva01 <silva20102@gmail.com>
2024-03-11 07:07:14 -0300

    Added Dummy class for test

---
+++ application-module/shared-application-module/src/test/java/br/net/silva/daniel/commons/DummyUseCase.java
@@ -0,0 +1,16 @@
+package br.net.silva.daniel.commons;
+
+import br.net.silva.daniel.shared.application.exception.GenericException;
+import br.net.silva.daniel.shared.application.interfaces.UseCase;
+import br.net.silva.daniel.shared.application.repository.ApplicationBaseRepository;
+import br.net.silva.daniel.shared.application.value_object.Source;
+
+public class DummyUseCase implements UseCase<String> {
+
+    public DummyUseCase(ApplicationBaseRepository<String> repository) {
+    }
+    @Override
+    public String exec(Source param) throws GenericException {
+        return "Simulation Use Case";
+    }
+}
