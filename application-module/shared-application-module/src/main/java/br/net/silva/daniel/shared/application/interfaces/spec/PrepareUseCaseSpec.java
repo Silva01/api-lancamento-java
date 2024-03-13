87b3402911357c70f6db392aac9a7e079b716094
silva01 <silva20102@gmail.com>
2024-03-13 06:16:44 -0300

    Refactored for has type generic

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/PrepareUseCaseSpec.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/PrepareUseCaseSpec.java
@@ -1,5 +1,5 @@
 package br.net.silva.daniel.shared.application.interfaces.spec;
 
-public interface PrepareUseCaseSpec {
-    RepositorySpec prepareUseCaseFrom(Class<?> clazz);
+public interface PrepareUseCaseSpec<T> {
+    RepositorySpec prepareUseCaseFrom(Class<T> clazz);
 }
