71857a3224bd05794d239685b26bf60ba5d619cd
silva01 <silva20102@gmail.com>
2024-03-13 07:05:39 -0300

    Created class for create configuration of test containers

---
+++ driver-module/api-module/app-module/src/test/java/silva/daniel/project/app/commons/MysqlTestContainer.java
@@ -0,0 +1,25 @@
+package silva.daniel.project.app.commons;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class MysqlTestContainer {
+
+    @Container
+    private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:8.0.26")
+            .withDatabaseName("finance")
+            .withUsername("finance")
+            .withPassword("123456");
+
+    @DynamicPropertySource
+    static void configureTestProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",() -> MY_SQL_CONTAINER.getJdbcUrl());
+        registry.add("spring.datasource.username",() -> MY_SQL_CONTAINER.getUsername());
+        registry.add("spring.datasource.password",() -> MY_SQL_CONTAINER.getPassword());
+        registry.add("spring.jpa.hibernate.ddl-auto",() -> "create");
+    }
+}
