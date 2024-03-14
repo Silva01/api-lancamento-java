1127e115d58028614d810cba865409a0125b3da3
silva01 <silva20102@gmail.com>
2024-03-14 07:38:00 -0300

    Refactored for supporting spring validation

--- driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/web/ClientController.java
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/web/ClientController.java
@@ -2,12 +2,14 @@ package silva.daniel.project.app.web;
 
 import br.net.silva.business.value_object.output.NewAccountByNewClientResponseSuccess;
 import br.net.silva.daniel.value_object.input.ClientRequestDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import silva.daniel.project.app.domain.client.ClientRequest;
 import silva.daniel.project.app.domain.client.ClientService;
 
 @RestController
@@ -21,8 +23,17 @@ public class ClientController {
     }
 
     @PostMapping
-    public ResponseEntity<NewAccountByNewClientResponseSuccess> createNewClient(@RequestBody ClientRequestDTO newClient) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createNewClient(newClient));
+    public ResponseEntity<NewAccountByNewClientResponseSuccess> createNewClient(@RequestBody @Valid ClientRequest newClient) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createNewClient(
+                new ClientRequestDTO(
+                    newClient.id(),
+                    newClient.cpf(),
+                    newClient.name(),
+                    newClient.telephone(),
+                    newClient.active(),
+                    newClient.agency(),
+                    newClient.addressRequestDTO()
+                )));
     }
 
 }
