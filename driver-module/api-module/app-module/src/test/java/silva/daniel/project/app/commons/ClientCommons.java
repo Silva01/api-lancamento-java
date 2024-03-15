f036a1932b30d0ebd0b21b60e9edbcf7d2ce2110
silva01 <silva20102@gmail.com>
2024-03-15 07:15:22 -0300

    Added Class commons of client

---
+++ driver-module/api-module/app-module/src/test/java/silva/daniel/project/app/commons/ClientCommons.java
@@ -0,0 +1,85 @@
+package silva.daniel.project.app.commons;
+
+import br.net.silva.daniel.value_object.input.AddressRequestDTO;
+import silva.daniel.project.app.domain.client.ClientRequest;
+
+public interface ClientCommons {
+
+    static ClientRequest requestValidMock() {
+        return requestMock(
+                "123",
+                "12345678901",
+                123,
+                addressRequestDTOMock());
+    }
+
+    static ClientRequest requestInvalidAddressMock() {
+        return requestMock(
+                "123",
+                "12345678901",
+                123,
+                null);
+    }
+
+    static ClientRequest requestCpfEmptyMock() {
+        return requestMock(
+                "123",
+                "",
+                123,
+                addressRequestDTOMock());
+    }
+
+    static ClientRequest requestNullCpfMock() {
+        return requestMock(
+                "123",
+                null,
+                123,
+                addressRequestDTOMock());
+    }
+
+    static ClientRequest requestInvalidAgencyMock() {
+        return requestMock(
+                "123",
+                "12345678901",
+                null,
+                addressRequestDTOMock());
+    }
+
+    static ClientRequest requestInvalidNegativeAgencyMock() {
+        return requestMock(
+                "123",
+                "12345678901",
+                -1,
+                addressRequestDTOMock());
+    }
+
+    static ClientRequest requestInvalidZeroAgencyMock() {
+        return requestMock(
+                "123",
+                "12345678901",
+                0,
+                addressRequestDTOMock());
+    }
+    static ClientRequest requestMock(String id, String cpf, Integer agency, AddressRequestDTO address) {
+        return new ClientRequest(
+                id,
+                cpf,
+                "Test",
+                "12345678901",
+                true,
+                agency,
+                address);
+    }
+
+    static AddressRequestDTO addressRequestDTOMock() {
+        return new AddressRequestDTO(
+                "rua 1",
+                "123",
+                "test",
+                "Bairro 1",
+                "DF",
+                "Brasilia",
+                "12345678"
+        );
+    }
+}
