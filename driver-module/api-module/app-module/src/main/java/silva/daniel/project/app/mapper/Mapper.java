c25830b7f324fc28c72e4230184b4c6518ab8bb2
silva01 <silva20102@gmail.com>
2024-03-16 07:05:36 -0300

    Removed method not used

--- driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/mapper/Mapper.java
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/mapper/Mapper.java
@@ -3,7 +3,6 @@ package silva.daniel.project.app.mapper;
 import br.net.silva.daniel.shared.application.interfaces.Output;
 
 public interface Mapper<O extends Output, E> {
-    boolean accept(Class<O> output, Class<E> entity);
     O mapTo(E entity);
     E mapTo(O output);
 }
