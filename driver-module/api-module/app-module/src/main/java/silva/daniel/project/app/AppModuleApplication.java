1eed06e78e63c617f645dca5e207d9f944b3b15e
silva01 <silva20102@gmail.com>
2024-03-16 06:13:19 -0300

    Added annotation for enable inject of class GenericResponseMapper

--- driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/AppModuleApplication.java
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/AppModuleApplication.java
@@ -1,11 +1,11 @@
 package silva.daniel.project.app;
 
-import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import silva.daniel.project.app.annotation.EnableGenericMapper;
 
 @SpringBootApplication
+@EnableGenericMapper
 public class AppModuleApplication {
 
     public static void main(String[] args) {
