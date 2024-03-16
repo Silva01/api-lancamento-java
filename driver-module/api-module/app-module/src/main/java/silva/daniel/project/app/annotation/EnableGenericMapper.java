945d7a523c7b45e60dc01e039eb48a1a27408067
silva01 <silva20102@gmail.com>
2024-03-16 06:39:30 -0300

    Refactored annotation EnableGenericMapper

--- driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/annotation/EnableGenericMapper.java
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/annotation/EnableGenericMapper.java
@@ -10,6 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(GenericResponseMapper.class)
+@EnableMapper
+@Import({GenericResponseMapper.class})
 public @interface EnableGenericMapper {
 }
