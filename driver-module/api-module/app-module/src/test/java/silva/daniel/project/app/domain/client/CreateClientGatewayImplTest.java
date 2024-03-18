57b67c7de315523186c0781780288ffd621d9a16
silva01 <silva20102@gmail.com>
2024-03-18 06:14:41 -0300

    Added test for CreateClientGatewayImplTest

---
+++ driver-module/api-module/app-module/src/test/java/silva/daniel/project/app/domain/client/CreateClientGatewayImplTest.java
@@ -0,0 +1,61 @@
+package silva.daniel.project.app.domain.client;
+
+import br.net.silva.daniel.value_object.output.ClientOutput;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
+import silva.daniel.project.app.mapper.Mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static silva.daniel.project.app.commons.ClientCommons.entityMock;
+import static silva.daniel.project.app.commons.ClientCommons.outputMock;
+
+@SpringBootTest(classes = CreateClientGatewayImpl.class)
+class CreateClientGatewayImplTest {
+
+    @Autowired
+    private CreateClientGatewayImpl createClientGatewayImpl;
+
+    @MockBean
+    private ClientRepository repository;
+
+    @MockBean
+    private Mapper<ClientOutput, Client> mapper;
+
+    @Test
+    void createNewClient_WithValidData_ReturnsNewClientWithId() {
+        final var clientOutput = outputMock();
+        final var clientEntityMock = entityMock();
+        when(repository.save(any(Client.class))).thenReturn(clientEntityMock);
+        when(mapper.mapTo(any(ClientOutput.class))).thenReturn(clientEntityMock);
+        when(mapper.mapTo(any(Client.class))).thenReturn(clientOutput);
+
+        final var sut = createClientGatewayImpl.save(clientOutput);
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.id()).isEqualTo(clientOutput.id());
+        assertThat(sut.cpf()).isEqualTo(clientOutput.cpf());
+        assertThat(sut.name()).isEqualTo(clientOutput.name());
+        assertThat(sut.telephone()).isEqualTo(clientOutput.telephone());
+        assertThat(sut.active()).isEqualTo(clientOutput.active());
+        assertThat(sut.address()).isEqualTo(clientOutput.address());
+    }
+
+    @Test
+    void createNewClient_WithClientExists_ReturnsError() {
+        final var clientOutput = outputMock();
+        final var clientEntityMock = entityMock();
+        when(repository.save(any(Client.class))).thenThrow(new DataIntegrityViolationException("Key Duplicated in Database"));
+        when(mapper.mapTo(any(ClientOutput.class))).thenReturn(clientEntityMock);
+
+        assertThatThrownBy(() -> createClientGatewayImpl.save(clientOutput))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessage("Key Duplicated in Database");
+    }
+
+}
