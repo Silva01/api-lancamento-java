a031db8ae507c5700e77ed16ec2997dd11fa1d66
silva01 <silva20102@gmail.com>
2024-03-19 06:43:19 -0300

    Refactored for support bean validation of spring

--- driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/domain/client/Address.java
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/domain/client/Address.java
@@ -1,6 +1,8 @@
 package silva.daniel.project.app.domain.client;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +13,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "address")
 public class Address {
-    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    private @NotNull @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+
+    @NotNull
+    @NotEmpty
     private String street;
+
+    @NotNull
+    @NotEmpty
     private String number;
+
     private String complement;
+
+    @NotNull
+    @NotEmpty
     private String neighborhood;
+
+    @NotNull
+    @NotEmpty
     private String state;
+
+    @NotNull
+    @NotEmpty
     private String city;
+
+    @NotNull
+    @NotEmpty
     private String zipCode;
 }
