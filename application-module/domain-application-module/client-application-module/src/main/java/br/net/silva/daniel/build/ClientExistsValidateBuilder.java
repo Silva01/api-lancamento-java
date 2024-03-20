96702da9ed2f039c6286e1cd2bd92697d5e31cdf
silva01 <silva20102@gmail.com>
2024-03-20 06:35:28 -0300

    feat: #25 ClientExistsValidateBuilder.java Added builder for create ClientExistsValidate

---
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/build/ClientExistsValidateBuilder.java
@@ -0,0 +1,25 @@
+package br.net.silva.daniel.build;
+
+import br.net.silva.daniel.shared.application.build.ObjectBuilder;
+import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.interfaces.IValidations;
+import br.net.silva.daniel.validation.ClientExistsValidate;
+import br.net.silva.daniel.value_object.output.ClientOutput;
+
+import java.util.Objects;
+
+public class ClientExistsValidateBuilder implements ObjectBuilder<IValidations> {
+
+    private FindApplicationBaseGateway<ClientOutput> findClientRepository;
+
+    public ClientExistsValidateBuilder withRepository(FindApplicationBaseGateway<ClientOutput> findClientRepository) {
+        this.findClientRepository = findClientRepository;
+        return this;
+    }
+
+    @Override
+    public IValidations build() {
+        Objects.requireNonNull(findClientRepository, "UseCase is required");
+        return new ClientExistsValidate(findClientRepository);
+    }
+}
