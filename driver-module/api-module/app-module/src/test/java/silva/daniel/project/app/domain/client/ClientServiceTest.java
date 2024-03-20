e5c905d514bb1e00a8dfb2fa8207a314822062f1
silva01 <silva20102@gmail.com>
2024-03-20 06:33:16 -0300

    refactor: #25 Added new unit test for validate in service flux edit client with valid data

--- driver-module/api-module/app-module/src/test/java/silva/daniel/project/app/domain/client/ClientServiceTest.java
+++ driver-module/api-module/app-module/src/test/java/silva/daniel/project/app/domain/client/ClientServiceTest.java
@@ -5,14 +5,16 @@ import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.value_object.input.AddressRequestDTO;
 import br.net.silva.daniel.value_object.input.ClientRequestDTO;
+import br.net.silva.daniel.value_object.input.EditClientInput;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import silva.daniel.project.app.config.FluxComponent;
+import silva.daniel.project.app.service.FluxService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
@@ -24,14 +26,14 @@ class ClientServiceTest {
     private ClientService service;
 
     @Mock
-    private FluxComponent fluxComponent;
+    private FluxService fluxService;
 
     @Mock
     private GenericFacadeDelegate facade;
 
     @Test
     void createNewClient_WithValidData_ReturnsNewAccountByNewClientResponseSuccess() throws Exception {
-        when(fluxComponent.fluxCreateNewClient()).thenReturn(facade);
+        when(fluxService.fluxCreateNewClient()).thenReturn(facade);
         doAnswer((argumentsOnMock) -> {
             var source = (Source) argumentsOnMock.getArgument(0);
             final var response = (NewAccountByNewClientResponseSuccess) source.output();
@@ -64,6 +66,20 @@ class ClientServiceTest {
         assertThat(sut.getAgency()).isEqualTo(1);
         assertThat(sut.getAccountNumber()).isEqualTo(2);
         assertThat(sut.getProvisionalPassword()).isEqualTo("123456");
+    }
+
+    @Test
+    void updateClient_WithValidData_ReturnsDoesNotThrows() throws Exception {
+        when(fluxService.fluxUpdateClient()).thenReturn(facade);
+        doAnswer((argumentsOnMock) -> null).when(facade).exec(any(Source.class));
+
+        final var request = new EditClientInput(
+                "12345678901",
+                "Daniel",
+                "61999999999"
+        );
+
+        assertThatCode(() -> service.updateClient(request)).doesNotThrowAnyException();
     }
 
 }
