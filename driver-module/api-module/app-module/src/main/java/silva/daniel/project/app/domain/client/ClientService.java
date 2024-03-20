78b9e4e159fd5c347eb1d3c8363e5b7f20306172
silva01 <silva20102@gmail.com>
2024-03-20 06:33:48 -0300

    feat: #25 Added feature for edit client in service

--- driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/domain/client/ClientService.java
+++ driver-module/api-module/app-module/src/main/java/silva/daniel/project/app/domain/client/ClientService.java
@@ -1,26 +1,34 @@
 package silva.daniel.project.app.domain.client;
 
 import br.net.silva.business.value_object.output.NewAccountByNewClientResponseSuccess;
+import br.net.silva.daniel.shared.application.interfaces.EmptyOutput;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.value_object.input.ClientRequestDTO;
+import br.net.silva.daniel.value_object.input.EditClientInput;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import silva.daniel.project.app.config.FluxComponent;
+import silva.daniel.project.app.service.FluxService;
 
 @Service
 public class ClientService {
 
-    private final FluxComponent fluxComponent;
+    private final FluxService fluxService;
 
-    public ClientService(FluxComponent fluxComponent) {
-        this.fluxComponent = fluxComponent;
+    public ClientService(FluxService fluxService) {
+        this.fluxService = fluxService;
     }
 
     @Transactional
     public NewAccountByNewClientResponseSuccess createNewClient(ClientRequestDTO request) throws Exception {
         var source = new Source(new NewAccountByNewClientResponseSuccess(), request);
-        fluxComponent.fluxCreateNewClient().exec(source);
+        fluxService.fluxCreateNewClient().exec(source);
         return (NewAccountByNewClientResponseSuccess) source.output();
+    }
+
+    @Transactional
+    public void updateClient(EditClientInput request) throws Exception {
+        var source = new Source(EmptyOutput.INSTANCE, request);
+        fluxService.fluxUpdateClient().exec(source);
     }
 
 }
