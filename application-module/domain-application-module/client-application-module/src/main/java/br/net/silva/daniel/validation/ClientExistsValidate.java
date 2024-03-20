f8c277e80fe22b99f0c9cfd3cbb15b0d741889d7
silva01 <silva20102@gmail.com>
2024-03-20 06:36:14 -0300

    refactor: #25 Refactored ClientExistsValidate for using the new gateway interface

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsValidate.java
@@ -1,24 +1,27 @@
 package br.net.silva.daniel.validation;
 
+import br.net.silva.daniel.exception.ClientNotExistsException;
 import br.net.silva.daniel.shared.application.exception.GenericException;
+import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import br.net.silva.daniel.shared.application.interfaces.IValidations;
-import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
 public class ClientExistsValidate implements IValidations {
-    private final UseCase<ClientOutput> findClientUseCase;
+    private final FindApplicationBaseGateway<ClientOutput> findClientRepository;
 
-    public ClientExistsValidate(UseCase<ClientOutput> findClientUseCase) {
-        this.findClientUseCase = findClientUseCase;
+    public ClientExistsValidate(FindApplicationBaseGateway<ClientOutput> findClientRepository) {
+        this.findClientRepository = findClientRepository;
     }
 
     @Override
     public void validate(Source input) throws GenericException {
-        try {
-            findClientUseCase.exec(input);
-        } catch (GenericException e) {
-            throw new GenericException("Client not exists in database");
+        var param = (ICpfParam) input.input();
+        var optClient = findClientRepository.findById(param);
+
+        if (optClient.isEmpty()) {
+            throw new ClientNotExistsException("Client not exists in database");
         }
     }
 }
