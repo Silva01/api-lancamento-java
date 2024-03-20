392916e506f8da6642eb124d46bcd77d771336aa
silva01 <silva20102@gmail.com>
2024-03-20 06:52:05 -0300

    refactor: #25 Refactored sql statement it's not worked

--- driver-module/api-module/app-module/src/test/resources/sql/import_client.sql
+++ driver-module/api-module/app-module/src/test/resources/sql/import_client.sql
@@ -1,4 +1,4 @@
-INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES ('Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
-INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES ('Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
+INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES (1, 'Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
+INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES (2, 'Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (1, '12345678901', 'Teste', '12345678901', true, 1);
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (2, '12345678903', 'Teste', '12345678901', false, 2);
