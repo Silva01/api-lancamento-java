31705d4c76ace23e3bb62d89c7e22485176d37da
silva01 <silva20102@gmail.com>
2024-03-23 06:09:45 -0300

    refactor: #25 Refactored for support class the ApplicationBaseGateway

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/FindAccountByCpfUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/FindAccountByCpfUseCase.java
@@ -5,25 +5,23 @@ import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.dto.AccountDTO;
 import br.net.silva.daniel.entity.Account;
-import br.net.silva.daniel.shared.application.exception.GenericException;
 import br.net.silva.daniel.factory.CreateAccountByAccountDTOFactory;
-import br.net.silva.daniel.shared.application.interfaces.IAccountParam;
+import br.net.silva.daniel.shared.application.exception.GenericException;
+import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
-import br.net.silva.daniel.shared.application.gateway.Repository;
-import br.net.silva.daniel.shared.business.factory.IFactoryAggregate;
 import br.net.silva.daniel.shared.application.value_object.Source;
-
-import java.util.Optional;
+import br.net.silva.daniel.shared.business.factory.IFactoryAggregate;
 
 public class FindAccountByCpfUseCase implements UseCase<AccountOutput> {
 
-    private final Repository<Optional<AccountOutput>> findActiveAccountRepository;
+    private final FindApplicationBaseGateway<AccountOutput> findActiveAccountRepository;
     private final GenericResponseMapper factory;
 
     private final IFactoryAggregate<Account, AccountDTO> factoryAggregate;
 
-    public FindAccountByCpfUseCase(Repository<Optional<AccountOutput>> findActiveAccountRepository, GenericResponseMapper factory) {
+    public FindAccountByCpfUseCase(FindApplicationBaseGateway<AccountOutput> findActiveAccountRepository, GenericResponseMapper factory) {
         this.findActiveAccountRepository = findActiveAccountRepository;
         this.factory = factory;
         this.factoryAggregate = new CreateAccountByAccountDTOFactory();
@@ -31,11 +29,11 @@ public class FindAccountByCpfUseCase implements UseCase<AccountOutput> {
 
     @Override
     public AccountOutput exec(Source param) throws GenericException {
-        var findAccountDto = (IAccountParam) param.input();
-        var accountOptional = findActiveAccountRepository.exec(findAccountDto.cpf());
-        var accountOutput =  accountOptional.orElseThrow(() -> new AccountNotExistsException("Account not found"));
+        var accountOutput = findActiveAccountRepository.findById((ICpfParam) param.input())
+                .orElseThrow(() -> new AccountNotExistsException("Account not found"));
+
         var accountAggregate = factoryAggregate.create(AccountBuilder.buildFullAccountDto().createFrom(accountOutput));
-        factory.fillIn(accountAggregate, param.output());
+        factory.fillIn(accountAggregate.build(), param.output());
         return AccountBuilder.buildFullAccountOutput().createFrom(accountAggregate.build());
     }
 }
