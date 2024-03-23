c92c58a2b502dcc82e211fc3edcc598341c7eb52
silva01 <silva20102@gmail.com>
2024-03-23 05:44:53 -0300

    refactor: #25 Refactored class for support ApplicationBaseGateway

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateAccountUseCase.java
@@ -1,24 +1,29 @@
 package br.net.silva.business.usecase;
 
+import br.net.silva.business.build.AccountBuilder;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.shared.application.exception.GenericException;
-import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
+import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
-import br.net.silva.daniel.shared.application.gateway.Repository;
+import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
 import br.net.silva.daniel.shared.application.value_object.Source;
 
 public class DeactivateAccountUseCase implements UseCase<AccountOutput> {
 
-    private final Repository<AccountOutput> deactivateAccountRepository;
-    public DeactivateAccountUseCase(Repository<AccountOutput> deactivateAccountRepository) {
-        this.deactivateAccountRepository = deactivateAccountRepository;
+    private final ApplicationBaseGateway<AccountOutput> baseGateway;
+    private final FindAccountUseCase findAccountUseCase;
+
+    public DeactivateAccountUseCase(ApplicationBaseGateway<AccountOutput> baseGateway, GenericResponseMapper mapper) {
+        this.baseGateway = baseGateway;
+        this.findAccountUseCase = new FindAccountUseCase(baseGateway, mapper);
     }
 
     @Override
     public AccountOutput exec(Source param) throws GenericException {
         try {
-            var dto = (ICpfParam) param.input();
-            return deactivateAccountRepository.exec(dto.cpf());
+            var account = AccountBuilder.buildAggregate().createFrom(findAccountUseCase.exec(param));
+            account.deactivate();
+            return baseGateway.save(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
         } catch (Exception e) {
             throw new GenericException(e.getMessage());
         }
