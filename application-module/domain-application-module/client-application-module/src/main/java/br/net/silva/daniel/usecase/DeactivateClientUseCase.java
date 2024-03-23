3d4d9c29085c9fb4a606e5b2c38d551f65d739e0
silva01 <silva20102@gmail.com>
2024-03-23 06:09:14 -0300

    refactor: #25 Removed imports not used

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/DeactivateClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/DeactivateClientUseCase.java
@@ -3,14 +3,14 @@ package br.net.silva.daniel.usecase;
 import br.net.silva.daniel.build.ClientBuilder;
 import br.net.silva.daniel.dto.ClientDTO;
 import br.net.silva.daniel.entity.Client;
-import br.net.silva.daniel.shared.application.exception.GenericException;
 import br.net.silva.daniel.factory.CreateClientByDtoFactory;
-import br.net.silva.daniel.shared.application.interfaces.UseCase;
-import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
+import br.net.silva.daniel.shared.application.exception.GenericException;
 import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.gateway.SaveApplicationBaseGateway;
-import br.net.silva.daniel.shared.business.factory.IFactoryAggregate;
+import br.net.silva.daniel.shared.application.interfaces.UseCase;
+import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.factory.IFactoryAggregate;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
 public class DeactivateClientUseCase implements UseCase<ClientOutput> {
