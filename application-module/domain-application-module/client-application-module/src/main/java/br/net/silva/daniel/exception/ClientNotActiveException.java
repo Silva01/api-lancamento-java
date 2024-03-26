df29df1b7c2411ebc029a0304118c9010697797c
silva01 <silva20102@gmail.com>
2024-03-26 06:33:17 -0300

    refactor: #25 Removed class not used

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/exception/ClientNotActiveException.java
+++
@@ -1,9 +0,0 @@
-package br.net.silva.daniel.exception;
-
-import br.net.silva.daniel.shared.application.exception.GenericException;
-
-public class ClientNotActiveException extends GenericException {
-    public ClientNotActiveException(String message) {
-        super(message);
-    }
-}
