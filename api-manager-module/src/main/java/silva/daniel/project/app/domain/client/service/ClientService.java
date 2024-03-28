f5f29283e6a376bd0a0152265e25bc64516304ec
silva01 <silva20102@gmail.com>
2024-03-28 06:02:25 -0300

    feat: #25 Added new method for create use case for find client in service

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/client/service/ClientService.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/client/service/ClientService.java
@@ -9,8 +9,12 @@ import br.net.silva.daniel.value_object.input.ClientRequestDTO;
 import br.net.silva.daniel.value_object.input.DeactivateClient;
 import br.net.silva.daniel.value_object.input.EditAddressInput;
 import br.net.silva.daniel.value_object.input.EditClientInput;
+import br.net.silva.daniel.value_object.input.FindClientByCpf;
+import br.net.silva.daniel.value_object.output.ClientOutput;
+import br.net.silva.daniel.value_object.output.GetInformationClientResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import silva.daniel.project.app.domain.client.request.ClientRequest;
 import silva.daniel.project.app.service.FluxService;
 
 @Service
@@ -51,5 +55,12 @@ public class ClientService {
     public void updateAddress(EditAddressInput addressInput) throws Exception {
         final var source = new Source(EmptyOutput.INSTANCE, addressInput);
         fluxService.fluxUpdateAddress().exec(source);
+    }
+
+    public GetInformationClientResponse getClientByCpf(String cpf) throws Exception {
+        final var clientByCpf = new FindClientByCpf(cpf);
+        final var source = new Source(new GetInformationClientResponse(), clientByCpf);
+        fluxService.fluxFindClient().exec(source);
+        return (GetInformationClientResponse) source.output();
     }
 }
