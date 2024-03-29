69993609e1f0013c2f3396946f5419b17315fcd9
silva01 <silva20102@gmail.com>
2024-03-29 09:12:37 -0300

    refactor: #25 Refactored tests don't work and generating bad coverage in jacoco

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateAccountUseCase.java
@@ -2,9 +2,9 @@ package br.net.silva.business.usecase;
 
 import br.net.silva.business.build.AccountBuilder;
 import br.net.silva.business.exception.AccountNotExistsException;
-import br.net.silva.business.value_object.input.ChangePasswordDTO;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.interfaces.IAccountParam;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.shared.business.exception.GenericException;
@@ -20,7 +20,7 @@ public class DeactivateAccountUseCase implements UseCase<AccountOutput> {
     @Override
     public AccountOutput exec(Source param) throws GenericException {
         try {
-            var input = (ChangePasswordDTO) param.input();
+            var input = (IAccountParam) param.input();
             var optAccount = baseAccountGateway.findById(input);
 
             if (optAccount.isEmpty()) {
