df485fd7ac88499a6ad6027b5a2e7a8bf8e99bae
silva01 <silva20102@gmail.com>
2024-03-30 06:19:54 -0300

    feat: #62 Added Enum with failure message

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
@@ -0,0 +1,21 @@
+package silva.daniel.project.app.commons;
+
+import org.springframework.http.HttpStatus;
+import silva.daniel.project.app.domain.client.FailureResponse;
+
+public enum FailureMessageEnum {
+    INVALID_DATA_MESSAGE(new FailureResponse("Information is not valid", HttpStatus.NOT_ACCEPTABLE.value())),
+    CLIENT_NOT_FOUND_MESSAGE(new FailureResponse("Client not exists in database", HttpStatus.NOT_FOUND.value())),
+    CLIENT_ALREADY_DEACTIVATED(new FailureResponse("Client already deactivated", HttpStatus.NOT_ACCEPTABLE.value())),
+    ACCOUNT_NOT_FOUND_MESSAGE(new FailureResponse("Account not Found", HttpStatus.NOT_FOUND.value()));
+
+    private final FailureResponse response;
+
+    FailureMessageEnum(FailureResponse response) {
+        this.response = response;
+    }
+
+    public FailureResponse getResponse() {
+        return response;
+    }
+}
