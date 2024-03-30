bb76faad65f22d8e8622874171dd705a8100c011
silva01 <silva20102@gmail.com>
2024-03-30 05:48:11 -0300

    refactor: #62 Added body request parameter in method for deactivate credit card

--- api-manager-module/src/main/java/silva/daniel/project/app/web/CreditCardController.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/CreditCardController.java
@@ -1,10 +1,13 @@
 package silva.daniel.project.app.web;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import silva.daniel.project.app.domain.account.request.DeactivateCreditCardRequest;
 
 @RestController
 @RequestMapping("/credit-card")
@@ -12,7 +15,7 @@ public class CreditCardController {
 
     @PostMapping("/deactivate")
     @ResponseStatus(HttpStatus.OK)
-    public void deactivateCreditCard() {
+    public void deactivateCreditCard(@Valid @RequestBody DeactivateCreditCardRequest request) {
     }
 
 }
