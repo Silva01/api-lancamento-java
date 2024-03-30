c85da4d10c523562dd20883ccb564111cd9c4442
silva01 <silva20102@gmail.com>
2024-03-30 07:04:03 -0300

    feat: #62 Added new Object of failure for credit card already deactivated

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
@@ -9,4 +9,5 @@ public class FailureMessageEnum {
     public static final FailureResponse CLIENT_ALREADY_DEACTIVATED = new FailureResponse("Client already deactivated", HttpStatus.NOT_ACCEPTABLE.value());
     public static final FailureResponse ACCOUNT_NOT_FOUND_MESSAGE = new FailureResponse("Account not Found", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse CREDIT_CARD_NOT_FOUND_MESSAGE = new FailureResponse("Credit card not exists", HttpStatus.NOT_FOUND.value());
+    public static final FailureResponse CREDIT_CARD_ALREADY_DEACTIVATED_MESSAGE = new FailureResponse("Credit card already deactivated", HttpStatus.CONFLICT.value());
 }
