c8ed250b1acac8698806f10766795c6794943e31
silva01 <silva20102@gmail.com>
2024-03-30 07:18:39 -0300

    refactor: #62 Refactored status in constant CLIENT_ALREADY_DEACTIVATED to 409

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
@@ -6,7 +6,7 @@ import silva.daniel.project.app.domain.client.FailureResponse;
 public class FailureMessageEnum {
     public static final FailureResponse INVALID_DATA_MESSAGE = new FailureResponse("Information is not valid", HttpStatus.NOT_ACCEPTABLE.value());
     public static final FailureResponse CLIENT_NOT_FOUND_MESSAGE = new FailureResponse("Client not exists in database", HttpStatus.NOT_FOUND.value());
-    public static final FailureResponse CLIENT_ALREADY_DEACTIVATED = new FailureResponse("Client already deactivated", HttpStatus.NOT_ACCEPTABLE.value());
+    public static final FailureResponse CLIENT_ALREADY_DEACTIVATED = new FailureResponse("Client already deactivated", HttpStatus.CONFLICT.value());
     public static final FailureResponse CLIENT_ALREADY_EXISTS = new FailureResponse("Client already exists in database", HttpStatus.CONFLICT.value());
     public static final FailureResponse ACCOUNT_NOT_FOUND_MESSAGE = new FailureResponse("Account not Found", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse CREDIT_CARD_NOT_FOUND_MESSAGE = new FailureResponse("Credit card not exists", HttpStatus.NOT_FOUND.value());
