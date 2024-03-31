d59f402f25627a46fde700996bb89c4259ee7bd9
silva01 <silva20102@gmail.com>
2024-03-31 05:35:11 -0300

    refactor: #62 Refactored validation for validate if credit card number exists for support the gateway interfaces

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/CreditCardNumberExistsValidate.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/CreditCardNumberExistsValidate.java
@@ -1,27 +1,29 @@
 package br.net.silva.business.validations;
 
 import br.net.silva.business.build.AccountBuilder;
+import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.exception.CreditCardNotExistsException;
 import br.net.silva.business.exception.CreditCardNumberDifferentException;
 import br.net.silva.business.value_object.input.DeactivateCreditCardInput;
 import br.net.silva.business.value_object.output.AccountOutput;
-import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.IValidations;
-import br.net.silva.daniel.shared.application.gateway.Repository;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
 
 public class CreditCardNumberExistsValidate implements IValidations {
 
-    private final Repository<AccountOutput> findAccountByCpfAndAccountNumberAndAgencyRepository;
+    private final FindApplicationBaseGateway<AccountOutput> findAccountGateway;
 
-    public CreditCardNumberExistsValidate(Repository<AccountOutput> findAccountByCpfAndAccountNumberAndAgencyRepository) {
-        this.findAccountByCpfAndAccountNumberAndAgencyRepository = findAccountByCpfAndAccountNumberAndAgencyRepository;
+    public CreditCardNumberExistsValidate(FindApplicationBaseGateway<AccountOutput> findAccountGateway) {
+        this.findAccountGateway = findAccountGateway;
     }
 
     @Override
     public void validate(Source param) throws GenericException {
         var input = (DeactivateCreditCardInput) param.input();
-        var account = AccountBuilder.buildAggregate().createFrom(findAccountByCpfAndAccountNumberAndAgencyRepository.exec(input.accountNumber(), input.agency(), input.cpf()));
+        var accountOutput = findAccountGateway.findById(input).orElseThrow(() -> new AccountNotExistsException("Account not exists"));
+        var account = AccountBuilder.buildAggregate().createFrom(accountOutput);
 
         if (!account.isHaveCreditCard()) {
             throw new CreditCardNotExistsException("Credit card not exists in the account");
