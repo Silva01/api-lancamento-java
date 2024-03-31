4798a46a77415651bbab5c4c3d63eb6e1da6a9d9
silva01 <silva20102@gmail.com>
2024-03-31 07:08:17 -0300

    refactor: #62 Refactored error message at credit card not exists

--- api-manager-module/src/test/java/silva/daniel/project/app/web/CreditCardControllerTest.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/web/CreditCardControllerTest.java
@@ -60,7 +60,7 @@ class CreditCardControllerTest extends RequestAssertCommons implements RequestBu
 
     @Test
     void deactivateCreditCard_WithCreditCardNotFound_ReturnsStatus404() throws Exception {
-        doThrow(new CreditCardNotExistsException("Credit card not exists")).when(service).deactivateCreditCard(any(DeactivateCreditCardInput.class));
+        doThrow(new CreditCardNotExistsException("Credit card not exists in the account")).when(service).deactivateCreditCard(any(DeactivateCreditCardInput.class));
         failurePostAssert(buildBaseRequest(), CREDIT_CARD_NOT_FOUND_MESSAGE, status().isNotFound());
     }
 
