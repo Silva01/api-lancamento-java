f40670bc534daac9e1d8bec6718f8c19fd2315fa
silva01 <silva20102@gmail.com>
2024-03-31 06:13:19 -0300

    refactor: #62 Refactored use case for Deactivate Credit Card to use gateway interface

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateCreditCardUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateCreditCardUseCase.java
@@ -1,36 +1,34 @@
 package br.net.silva.business.usecase;
 
 import br.net.silva.business.build.AccountBuilder;
+import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.exception.CreditCardNumberDifferentException;
 import br.net.silva.business.value_object.input.DeactivateCreditCardInput;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.dto.AccountDTO;
 import br.net.silva.daniel.entity.Account;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.factory.CreateAccountByAccountDTOFactory;
+import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.EmptyOutput;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
-import br.net.silva.daniel.shared.application.gateway.Repository;
-import br.net.silva.daniel.shared.business.factory.IFactoryAggregate;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.shared.business.factory.IFactoryAggregate;
 
 public class DeactivateCreditCardUseCase implements UseCase<EmptyOutput> {
-
-    private final Repository<AccountOutput> findAccountRepository;
-    private final Repository<AccountOutput> saveAccountRepository;
+    private final ApplicationBaseGateway<AccountOutput> accountBaseGateway;
 
     private final IFactoryAggregate<Account, AccountDTO> accountFactory;
 
-    public DeactivateCreditCardUseCase(Repository<AccountOutput> findAccountRepository, Repository<AccountOutput> saveAccountRepository) {
-        this.findAccountRepository = findAccountRepository;
-        this.saveAccountRepository = saveAccountRepository;
+    public DeactivateCreditCardUseCase(ApplicationBaseGateway<AccountOutput> accountBaseGateway) {
+        this.accountBaseGateway = accountBaseGateway;
         this.accountFactory = new CreateAccountByAccountDTOFactory();
     }
 
     @Override
     public EmptyOutput exec(Source param) throws GenericException {
         var input = (DeactivateCreditCardInput) param.input();
-        var accountOutput = findAccountRepository.exec(input.accountNumber(), input.agency(), input.cpf());
+        var accountOutput = accountBaseGateway.findById(input).orElseThrow(() -> new AccountNotExistsException("Account not exists"));
 
         var account = accountFactory.create(AccountBuilder.buildFullAccountDto().createFrom(accountOutput));
 
@@ -39,7 +37,7 @@ public class DeactivateCreditCardUseCase implements UseCase<EmptyOutput> {
         }
 
         account.deactivateCreditCard();
-        saveAccountRepository.exec(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
+        accountBaseGateway.save(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
 
         return EmptyOutput.INSTANCE;
     }
