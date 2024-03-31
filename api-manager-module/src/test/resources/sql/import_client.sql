eaaf3ca52ab5071b7d07a4ca38706e58ed5917d1
silva01 <silva20102@gmail.com>
2024-03-31 06:06:42 -0300

    refactor: #62 Adjusted order of script

--- api-manager-module/src/test/resources/sql/import_client.sql
+++ api-manager-module/src/test/resources/sql/import_client.sql
@@ -10,11 +10,11 @@ INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALU
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (3, '12345678904', 'Teste', '12345678901', true, 3);
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (3, '12345678910', 'Teste With Credit Card', '12345678901', true, 4);
 
+-- Credit card of clients
+INSERT INTO credit_card (number, cvv, flag, balance, expiration_date, active) VALUES ('1234567890123456', 123, 'MASTER_CARD', 3000, '2022-12-01', true);
+
 -- Account of clients
 INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1234, 1, 3000, MD5('123456'), true, '12345678901');
 INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1235, 1, 3000, MD5('123456'), false, '12345678903');
 INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1236, 1, 3000, MD5('123456'), false, '12345678904');
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf, credit_card_number) VALUES (1236, 1, 3000, MD5('123456'), false, '12345678910', '1234567890123456');
-
--- Credit card of clients
-INSERT INTO credit_card (number, cvv, flag, balance, expiration_date, active) VALUES ('1234567890123456', 123, 3000, '2022-12-01', true);
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf, credit_card_number) VALUES (1237, 1, 3000, MD5('123456'), false, '12345678910', '1234567890123456');
