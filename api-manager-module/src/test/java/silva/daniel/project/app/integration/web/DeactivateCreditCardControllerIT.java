2a9bbb6552f350a0f99cf66272a7ad1777ac779e
silva01 <silva20102@gmail.com>
2024-03-31 06:50:04 -0300

    test: #62 Added new integration test for deactivate credit card with invalid data

--- api-manager-module/src/test/java/silva/daniel/project/app/integration/web/DeactivateCreditCardControllerIT.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/integration/web/DeactivateCreditCardControllerIT.java
@@ -1,22 +1,31 @@
 package silva.daniel.project.app.integration.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import silva.daniel.project.app.commons.FailureMessageEnum;
 import silva.daniel.project.app.commons.MysqlTestContainer;
 import silva.daniel.project.app.domain.account.repository.AccountRepository;
 import silva.daniel.project.app.domain.account.request.DeactivateCreditCardRequest;
+import silva.daniel.project.app.domain.client.FailureResponse;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static silva.daniel.project.app.commons.FailureMessageEnum.INVALID_DATA_MESSAGE;
 
 @ActiveProfiles("e2e")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "/sql/delete_client.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-@Sql(scripts = {"/sql/import_client.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/delete_client.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+@Sql(scripts = {"/sql/import_client.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
 class DeactivateCreditCardControllerIT extends MysqlTestContainer {
 
     @Autowired
@@ -34,5 +43,25 @@ class DeactivateCreditCardControllerIT extends MysqlTestContainer {
         var account = repository.findByCpf(request.cpf());
         assertThat(account.isPresent()).isTrue();
         assertThat(account.get().getCreditCard().isActive()).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidData")
+    void deactivateCreditCard_WithInvalidData_ReturnsStatus406(DeactivateCreditCardRequest request) {
+        var sut = restTemplate.postForEntity("/credit-card/deactivate", request, FailureResponse.class);
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE);
+        assertThat(sut.getBody().getMessage()).isEqualTo(INVALID_DATA_MESSAGE.getMessage());
+        assertThat(sut.getBody().getStatusCode()).isEqualTo(INVALID_DATA_MESSAGE.getStatusCode());
+    }
+
+    private static Stream<Arguments> provideInvalidData() {
+        return Stream.of(
+            Arguments.of(new DeactivateCreditCardRequest("", 1237, 1, "1234567890123456")),
+            Arguments.of(new DeactivateCreditCardRequest("12345678910", 1237, 1, "")),
+            Arguments.of(new DeactivateCreditCardRequest("12345678910", null, 1, "12345678901234567")),
+            Arguments.of(new DeactivateCreditCardRequest("12345678910", 1237, null, "12345678901234567")),
+            Arguments.of(new DeactivateCreditCardRequest("123456", 1237, 1, "1234567890123456")),
+            Arguments.of(new DeactivateCreditCardRequest("12345678910", 1237, 1, "123456"))
+        );
     }
 }
