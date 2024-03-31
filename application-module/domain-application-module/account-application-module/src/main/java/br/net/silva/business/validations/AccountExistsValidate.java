9b7c9979bd545e4d4f1917e64530b6d59daa97fb
silva01 <silva20102@gmail.com>
2024-03-31 05:27:19 -0300

    refactor: #62 Refactored validation for validate if account exists for support the gateway interfaces

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountExistsValidate.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountExistsValidate.java
@@ -2,26 +2,24 @@ package br.net.silva.business.validations;
 
 import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.value_object.output.AccountOutput;
+import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.IAccountParam;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.shared.application.interfaces.IValidations;
-import br.net.silva.daniel.shared.application.gateway.Repository;
 import br.net.silva.daniel.shared.application.value_object.Source;
-
-import java.util.Optional;
+import br.net.silva.daniel.shared.business.exception.GenericException;
 
 public class AccountExistsValidate implements IValidations {
 
-    private final Repository<Optional<AccountOutput>> findAccountRepository;
+    private final FindApplicationBaseGateway<AccountOutput> findAccountGateway;
 
-    public AccountExistsValidate(Repository<Optional<AccountOutput>> findAccountRepository) {
-        this.findAccountRepository = findAccountRepository;
+    public AccountExistsValidate(FindApplicationBaseGateway<AccountOutput> findAccountGateway) {
+        this.findAccountGateway = findAccountGateway;
     }
 
     @Override
     public void validate(Source input) throws GenericException {
         var dto = (IAccountParam) input.input();
-        var optionalAccount = findAccountRepository.exec(dto.accountNumber(), dto.agency(), dto.cpf());
+        var optionalAccount = findAccountGateway.findById(dto);
 
         if (optionalAccount.isEmpty()) {
             throw new AccountNotExistsException("Account not exists");
