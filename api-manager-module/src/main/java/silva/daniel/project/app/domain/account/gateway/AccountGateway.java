491c7a088518df8deee6f22e303b7b4e36feceed
silva01 <silva20102@gmail.com>
2024-03-31 06:32:45 -0300

    refactor: #62 Refactored for support credit card null

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/gateway/AccountGateway.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/gateway/AccountGateway.java
@@ -1,6 +1,7 @@
 package silva.daniel.project.app.domain.account.gateway;
 
 import br.net.silva.business.value_object.output.AccountOutput;
+import br.net.silva.business.value_object.output.CreditCardOutput;
 import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.gateway.ParamGateway;
 import br.net.silva.daniel.shared.application.interfaces.IAccountParam;
@@ -8,6 +9,7 @@ import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import org.springframework.stereotype.Component;
 import silva.daniel.project.app.domain.account.entity.Account;
 import silva.daniel.project.app.domain.account.entity.AccountKey;
+import silva.daniel.project.app.domain.account.entity.CreditCard;
 import silva.daniel.project.app.domain.account.repository.AccountRepository;
 
 import java.time.LocalDateTime;
@@ -40,7 +42,15 @@ public class AccountGateway implements ApplicationBaseGateway<AccountOutput> {
         if(param instanceof IAccountParam accountParam) {
             var key = new AccountKey(accountParam.accountNumber(), accountParam.agency());
             return repository.findById(key)
-                    .map(account -> new AccountOutput(account.getKeys().getNumber(), account.getKeys().getBankAgencyNumber(), account.getBalance(), account.getPassword(), account.isActive(), account.getCpf(), null, Collections.emptyList()));
+                    .map(account -> new AccountOutput(
+                            account.getKeys().getNumber(),
+                            account.getKeys().getBankAgencyNumber(),
+                            account.getBalance(),
+                            account.getPassword(),
+                            account.isActive(),
+                            account.getCpf(),
+                            buildCreditCard(account.getCreditCard()),
+                            Collections.emptyList()));
         }
 
         final var cpf = (ICpfParam) param;
@@ -56,7 +66,12 @@ public class AccountGateway implements ApplicationBaseGateway<AccountOutput> {
     @Override
     public AccountOutput save(AccountOutput entity) {
         var key = new AccountKey(entity.number(), entity.agency());
-        var account = new Account(key, entity.balance(), entity.password(), entity.active(), entity.cpf(), null, null, LocalDateTime.now());
+        CreditCard creditCard = null;
+        if (entity.creditCard() != null) {
+            creditCard = new CreditCard(entity.creditCard().number(), entity.creditCard().cvv(), entity.creditCard().flag(), entity.creditCard().balance(), entity.creditCard().expirationDate(), entity.creditCard().active());
+        }
+
+        var account = new Account(key, entity.balance(), entity.password(), entity.active(), entity.cpf(), creditCard, null, LocalDateTime.now());
         var newAccount = repository.save(account);
         return new AccountOutput(newAccount.getKeys().getNumber(), newAccount.getKeys().getBankAgencyNumber(), newAccount.getBalance(), newAccount.getPassword(), newAccount.isActive(), newAccount.getCpf(), null, Collections.emptyList());
     }
@@ -64,5 +79,19 @@ public class AccountGateway implements ApplicationBaseGateway<AccountOutput> {
     @Override
     public void saveAll(List<AccountOutput> paramList) {
 
+    }
+
+    private CreditCardOutput buildCreditCard(CreditCard creditCard) {
+        if (creditCard == null) {
+            return null;
+        }
+
+        return new CreditCardOutput(
+                creditCard.getNumber(),
+                creditCard.getCvv(),
+                creditCard.getFlag(),
+                creditCard.getBalance(),
+                creditCard.getExpirationDate(),
+                creditCard.isActive());
     }
 }
