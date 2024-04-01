3f8ef0a1b9dfc1973b97b3d9acacd30085322b17
silva01 <silva20102@gmail.com>
2024-04-01 06:11:04 -0300

    feat: #63 Added new endpoint for create credit card

--- api-manager-module/src/main/java/silva/daniel/project/app/web/CreditCardController.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/CreditCardController.java
@@ -1,5 +1,6 @@
 package silva.daniel.project.app.web;
 
+import br.net.silva.business.value_object.input.CreateCreditCardInput;
 import br.net.silva.business.value_object.input.DeactivateCreditCardInput;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import silva.daniel.project.app.domain.account.request.CreateCreditCardRequest;
 import silva.daniel.project.app.domain.account.request.DeactivateCreditCardRequest;
 import silva.daniel.project.app.domain.account.service.CreditCardService;
 
@@ -26,6 +28,13 @@ public final class CreditCardController {
     public void deactivateCreditCard(@Valid @RequestBody DeactivateCreditCardRequest request) throws Exception {
         var input = new DeactivateCreditCardInput(request.cpf(), request.accountNumber(), request.agency(), request.creditCardNumber());
         service.deactivateCreditCard(input);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createCreditCard(@Valid @RequestBody CreateCreditCardRequest request) throws Exception {
+        var input = new CreateCreditCardInput(request.cpf(), request.account(), request.agencyNumber());
+//        service.createCreditCard(input);
     }
 
 }
