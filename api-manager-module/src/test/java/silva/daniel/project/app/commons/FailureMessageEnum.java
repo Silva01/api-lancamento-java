af25096acc1dde87af57d99171b7b4b49e6be207
silva01 <silva20102@gmail.com>
2024-04-01 06:40:42 -0300

    feat: #63 Added new object failure message for when already exists credit card

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
@@ -11,4 +11,5 @@ public class FailureMessageEnum {
     public static final FailureResponse ACCOUNT_NOT_FOUND_MESSAGE = new FailureResponse("Account not Found", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse CREDIT_CARD_NOT_FOUND_MESSAGE = new FailureResponse("Credit card not exists in the account", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse CREDIT_CARD_ALREADY_DEACTIVATED_MESSAGE = new FailureResponse("Credit card deactivated in the account", HttpStatus.CONFLICT.value());
+    public static final FailureResponse CREDIT_CARD_ALREADY_EXISTS_MESSAGE = new FailureResponse("Credit card already exists", HttpStatus.CONFLICT.value());
 }
