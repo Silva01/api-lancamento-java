3b28abdf711ebae61f21b93c7da8df1e1ebd6525
silva01 <silva20102@gmail.com>
2024-04-01 07:03:12 -0300

    test: #63 Added new method for test scenario createCreditCard_WithClientNotExists_ReturnsException

--- api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/CreditCardServiceTest.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/CreditCardServiceTest.java
@@ -1,5 +1,6 @@
 package silva.daniel.project.app.domain.account.service;
 
+import br.net.silva.daniel.exception.ClientNotExistsException;
 import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import org.junit.jupiter.api.Test;
@@ -7,13 +8,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import silva.daniel.project.app.commons.FailureMessageEnum;
 import silva.daniel.project.app.commons.InputBuilderCommons;
 import silva.daniel.project.app.service.FluxService;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static silva.daniel.project.app.commons.FailureMessageEnum.CLIENT_NOT_FOUND_MESSAGE;
 
 @ExtendWith(MockitoExtension.class)
 class CreditCardServiceTest implements InputBuilderCommons {
@@ -33,5 +37,15 @@ class CreditCardServiceTest implements InputBuilderCommons {
         doAnswer((argumentsOnMock) -> null).when(facade).exec(any(Source.class));
 
         assertThatCode(() -> service.createCreditCard(buildNewBaseCreditCardInput())).doesNotThrowAnyException();
+    }
+
+    @Test
+    void createCreditCard_WithClientNotExists_ReturnsException() throws Exception {
+        when(fluxService.fluxCreateCreditCard()).thenReturn(facade);
+        doThrow(new ClientNotExistsException(CLIENT_NOT_FOUND_MESSAGE.getMessage())).when(facade).exec(any(Source.class));
+
+        assertThatCode(() -> service.createCreditCard(buildNewBaseCreditCardInput()))
+                .isInstanceOf(ClientNotExistsException.class)
+                .hasMessage(CLIENT_NOT_FOUND_MESSAGE.getMessage());
     }
 }
