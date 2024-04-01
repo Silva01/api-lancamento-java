b8b8539b173a662e2536c5d88a844674572c6636
silva01 <silva20102@gmail.com>
2024-04-01 07:16:46 -0300

    feat: #63 Added new handler for AccountDeactivatedException

--- api-manager-module/src/main/java/silva/daniel/project/app/web/ClientExceptionHandler.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/ClientExceptionHandler.java
@@ -1,6 +1,7 @@
 package silva.daniel.project.app.web;
 
 
+import br.net.silva.business.exception.AccountDeactivatedException;
 import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.exception.CreditCardAlreadyExistsException;
 import br.net.silva.business.exception.CreditCardDeactivatedException;
@@ -69,6 +70,12 @@ public class ClientExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CreditCardAlreadyExistsException.class)
     public ResponseEntity<FailureResponse> handleCreditCardAlreadyExistsException(CreditCardAlreadyExistsException ex) {
+        var failureResponse = new FailureResponse(ex.getMessage(), CONFLICT.value());
+        return ResponseEntity.status(CONFLICT).body(failureResponse);
+    }
+
+    @ExceptionHandler(AccountDeactivatedException.class)
+    public ResponseEntity<FailureResponse> handleAccountDeactivatedException(AccountDeactivatedException ex) {
         var failureResponse = new FailureResponse(ex.getMessage(), CONFLICT.value());
         return ResponseEntity.status(CONFLICT).body(failureResponse);
     }
