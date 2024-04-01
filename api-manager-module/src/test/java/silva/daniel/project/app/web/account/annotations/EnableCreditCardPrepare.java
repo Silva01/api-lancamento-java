b985e1a64cfa66f840f46bef91da661e9a526a31
silva01 <silva20102@gmail.com>
2024-04-01 06:09:44 -0300

    feat: #63 Added new annotation for enable prepare for test to credit card

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/web/account/annotations/EnableCreditCardPrepare.java
@@ -0,0 +1,20 @@
+package silva.daniel.project.app.web.account.annotations;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import silva.daniel.project.app.web.CreditCardController;
+import silva.daniel.project.app.web.account.CreateCreditCardPrepare;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@WebMvcTest(CreditCardController.class)
+@Import({
+        CreateCreditCardPrepare.class
+})
+public @interface EnableCreditCardPrepare {
+}
