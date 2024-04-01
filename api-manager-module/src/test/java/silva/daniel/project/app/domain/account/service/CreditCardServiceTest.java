46426859228228ddbff22a71edde0cf756969472
silva01 <silva20102@gmail.com>
2024-04-01 06:59:00 -0300

    test: #63 Added new test for credit card service

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/CreditCardServiceTest.java
@@ -0,0 +1,37 @@
+package silva.daniel.project.app.domain.account.service;
+
+import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
+import br.net.silva.daniel.shared.application.value_object.Source;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import silva.daniel.project.app.commons.InputBuilderCommons;
+import silva.daniel.project.app.service.FluxService;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CreditCardServiceTest implements InputBuilderCommons {
+    
+    @InjectMocks
+    private CreditCardService service;
+
+    @Mock
+    private FluxService fluxService;
+
+    @Mock
+    private GenericFacadeDelegate facade;
+
+    @Test
+    void createCreditCard_WithValidData_ExecuteWithoutErros() throws Exception {
+        when(fluxService.fluxCreateCreditCard()).thenReturn(facade);
+        doAnswer((argumentsOnMock) -> null).when(facade).exec(any(Source.class));
+
+        assertThatCode(() -> service.createCreditCard(buildNewBaseCreditCardInput())).doesNotThrowAnyException();
+    }
+}
