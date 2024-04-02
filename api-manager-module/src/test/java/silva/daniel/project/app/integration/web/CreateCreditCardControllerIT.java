5df33444aa40b4e223b1fa69d08699b791144a66
silva01 <silva20102@gmail.com>
2024-04-02 06:49:28 -0300

    test: #63 Added integration test for flux createCreditCard_WithValidData_ReturnsStatus200

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/integration/web/CreateCreditCardControllerIT.java
@@ -0,0 +1,40 @@
+package silva.daniel.project.app.integration.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import silva.daniel.project.app.commons.MysqlTestContainer;
+import silva.daniel.project.app.domain.account.repository.AccountRepository;
+import silva.daniel.project.app.domain.account.request.CreateCreditCardRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("e2e")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/delete_client.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+@Sql(scripts = {"/sql/import_client.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+class CreateCreditCardControllerIT extends MysqlTestContainer {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private AccountRepository repository;
+
+    @Test
+    void createCreditCard_WithValidData_ReturnsStatus200() {
+        var request = new CreateCreditCardRequest("12345678904", 1237, 1);
+        var response = restTemplate.postForEntity("/credit-card", request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        var account = repository.findByCpf(request.cpf());
+        assertThat(account.isPresent()).isTrue();
+        assertThat(account.get().getCreditCard()).isNotNull();
+        assertThat(account.get().getCreditCard().isActive()).isTrue();
+    }
+}
