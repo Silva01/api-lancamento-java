69011708175ff27c8f05fe89f61f03ebcc0c9b81
silva01 <silva20102@gmail.com>
2024-04-02 06:45:04 -0300

    refactor: #63 Renamed interface for credit card to ICreditCardNumberParam

---
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/ICreditCardNumberParam.java
@@ -0,0 +1,5 @@
+package br.net.silva.daniel.shared.application.interfaces;
+
+public interface ICreditCardNumberParam extends Input {
+    String creditCardNumber();
+}
--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/ICreditCardParam.java
+++
@@ -1,7 +0,0 @@
-package br.net.silva.daniel.shared.application.interfaces;
-
-public interface ICreditCardParam extends Input {
-    String creditCardNumber();
-    Integer creditCardCvv();
-    String cpf();
-}
