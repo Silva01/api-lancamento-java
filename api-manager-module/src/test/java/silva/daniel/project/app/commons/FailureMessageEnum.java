7cc48911d62f87dd855050bcd37e2b610db78ced
silva01 <silva20102@gmail.com>
2024-04-02 07:13:17 -0300

    test: #63 Added object failure for client deactived

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
@@ -7,6 +7,7 @@ public class FailureMessageEnum {
     public static final FailureResponse INVALID_DATA_MESSAGE = new FailureResponse("Information is not valid", HttpStatus.NOT_ACCEPTABLE.value());
     public static final FailureResponse CLIENT_NOT_FOUND_MESSAGE = new FailureResponse("Client not exists in database", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse CLIENT_ALREADY_DEACTIVATED = new FailureResponse("Client already deactivated", HttpStatus.CONFLICT.value());
+    public static final FailureResponse CLIENT_DEACTIVATED = new FailureResponse("Client is Deactivated", HttpStatus.CONFLICT.value());
     public static final FailureResponse CLIENT_ALREADY_EXISTS = new FailureResponse("Client already exists in database", HttpStatus.CONFLICT.value());
     public static final FailureResponse ACCOUNT_NOT_FOUND_MESSAGE = new FailureResponse("Account not Found", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse ACCOUNT_ALREADY_DEACTIVATED_MESSAGE = new FailureResponse("Account is Deactivated", HttpStatus.CONFLICT.value());
