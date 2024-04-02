324682688238728685b49965570bf11edf7fa99b
silva01 <silva20102@gmail.com>
2024-04-02 05:49:06 -0300

    refactor: #63 Refactored test for use gateway interface for class CreateNewCreditCardUseCase

--- application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/usecase/CreateNewCreditCardUseCaseTest.java
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/usecase/CreateNewCreditCardUseCaseTest.java
@@ -3,44 +3,50 @@ package br.net.silva.business.usecase;
 import br.net.silva.business.value_object.input.CreateCreditCardInput;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.business.value_object.output.CreditCardOutput;
-import br.net.silva.daniel.entity.Account;
 import br.net.silva.daniel.enuns.FlagEnum;
-import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.gateway.ParamGateway;
 import br.net.silva.daniel.shared.application.interfaces.EmptyOutput;
-import br.net.silva.daniel.shared.application.gateway.Repository;
-import br.net.silva.daniel.shared.business.utils.CryptoUtils;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.shared.business.utils.CryptoUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class CreateNewCreditCardUseCaseTest {
 
     private CreateNewCreditCardUseCase useCase;
 
     @Mock
-    private Repository<AccountOutput> findAccountByCpfAndAgencyAndAccountNumberRepository;
-
-    @Mock
-    private Repository<AccountOutput> saveAccountRepository;
+    private ApplicationBaseGateway<AccountOutput> baseGateway;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-
-        when(findAccountByCpfAndAgencyAndAccountNumberRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(buildMockAccount(true, null));
-        doAnswer(invocation -> invocation.getArgument(0)).when(saveAccountRepository).exec(any(AccountOutput.class));
-
-        useCase = new CreateNewCreditCardUseCase(findAccountByCpfAndAgencyAndAccountNumberRepository, saveAccountRepository);
+        when(baseGateway.findById(any(ParamGateway.class))).thenReturn(Optional.of(buildMockAccount(true, null)));
+        when(baseGateway.save(any(AccountOutput.class))).thenReturn(buildMockAccount(true, buildMockCreditCard()));
+        useCase = new CreateNewCreditCardUseCase(baseGateway);
     }
 
     @Test
@@ -52,23 +58,23 @@ class CreateNewCreditCardUseCaseTest {
         assertNotNull(accountDTO);
         assertNotNull(accountDTO.creditCard());
         assertTrue(accountDTO.creditCard().active());
-        assertEquals(BigDecimal.valueOf(2000), accountDTO.creditCard().balance());
+        assertEquals(BigDecimal.valueOf(1000), accountDTO.creditCard().balance());
 
-        verify(findAccountByCpfAndAgencyAndAccountNumberRepository, times(1)).exec(input.agency(), input.accountNumber(), input.cpf());
-        verify(saveAccountRepository, times(1)).exec(any(AccountOutput.class));
+        verify(baseGateway, times(1)).findById(any(ParamGateway.class));
+        verify(baseGateway, times(1)).save(any(AccountOutput.class));
     }
 
     @Test
     void shouldGenericErrorWhenTryCreateNewCreditCard() {
-        when(findAccountByCpfAndAgencyAndAccountNumberRepository.exec(anyInt(), anyInt(), anyString())).thenReturn(null);
+        when(baseGateway.findById(any(ParamGateway.class))).thenReturn(null);
         var input = new CreateCreditCardInput("99988877766", 45678, 1234);
         var source = new Source(EmptyOutput.INSTANCE, input);
 
         var response = assertThrows(GenericException.class, () -> useCase.exec(source));
         assertEquals("Generic error", response.getMessage());
 
-        verify(findAccountByCpfAndAgencyAndAccountNumberRepository, times(1)).exec(input.agency(), input.accountNumber(), input.cpf());
-        verify(saveAccountRepository, never()).exec(any(Account.class));
+        verify(baseGateway, times(1)).findById(any(ParamGateway.class));
+        verify(baseGateway, never()).save(any(AccountOutput.class));
     }
 
     private AccountOutput buildMockAccount(boolean active, CreditCardOutput creditCard) {
