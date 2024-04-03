28714b1d1132db498bca24740a9f8d5bd41b1fb8
silva01 <silva20102@gmail.com>
2024-04-03 06:16:25 -0300

    feat: #64 Added new service for process request of edit agency

---
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/service/AccountService.java
@@ -0,0 +1,12 @@
+package silva.daniel.project.app.domain.account.service;
+
+import br.net.silva.business.value_object.input.ChangeAgencyInput;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public void editAgencyOfAccount(ChangeAgencyInput changeAgency) throws Exception {
+        //TODO: Implementar
+    }
+}
--- api-manager-module/src/main/java/silva/daniel/project/app/web/AccountController.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/AccountController.java
@@ -1,5 +1,6 @@
 package silva.daniel.project.app.web;
 
+import br.net.silva.business.value_object.input.ChangeAgencyInput;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,14 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import silva.daniel.project.app.domain.account.request.EditAgencyOfAccountRequest;
+import silva.daniel.project.app.domain.account.service.AccountService;
 
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
 
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     @PutMapping("/update/agency")
     @ResponseStatus(HttpStatus.OK)
-    public void updateAgencyOfAccount(@Valid @RequestBody EditAgencyOfAccountRequest request) {
-
+    public void updateAgencyOfAccount(@Valid @RequestBody EditAgencyOfAccountRequest request) throws Exception {
+        accountService.editAgencyOfAccount(new ChangeAgencyInput(
+                request.cpf(), request.accountNumber(), request.agencyNumber(), request.newAgencyNumber())
+        );
     }
 }
