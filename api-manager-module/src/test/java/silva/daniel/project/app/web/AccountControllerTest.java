44811f099f887cb5648e62cc7f6278ca1217325e
silva01 <silva20102@gmail.com>
2024-04-03 06:26:21 -0300

    test: #64 Added new method test for flux editAgencyOfAccount_WithValidData_ReturnsStatus406

--- api-manager-module/src/test/java/silva/daniel/project/app/web/AccountControllerTest.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/web/AccountControllerTest.java
@@ -1,15 +1,22 @@
 package silva.daniel.project.app.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import silva.daniel.project.app.commons.RequestBuilderCommons;
+import silva.daniel.project.app.domain.account.request.EditAgencyOfAccountRequest;
 import silva.daniel.project.app.domain.account.service.AccountService;
 import silva.daniel.project.app.web.account.EditAgencyOfAccountPrepare;
 import silva.daniel.project.app.web.account.annotations.EnableAccountPrepare;
 
+import java.util.stream.Stream;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static silva.daniel.project.app.commons.FailureMessageEnum.INVALID_DATA_MESSAGE;
 
 @ActiveProfiles("unit")
 @EnableAccountPrepare
@@ -24,5 +31,23 @@ class AccountControllerTest implements RequestBuilderCommons {
     @Test
     void editAgencyOfAccount_WithValidData_ReturnsStatus200() throws Exception {
         editAgencyOfAccountPrepare.successPutAssert(buildBaseEditAgencyOfAccountRequest(), status().isOk());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidDataOfEditAgencyOfAccount")
+    void editAgencyOfAccount_WithValidData_ReturnsStatus406(EditAgencyOfAccountRequest request) throws Exception {
+        editAgencyOfAccountPrepare.failurePutAssert(request, INVALID_DATA_MESSAGE, status().isNotAcceptable());
+    }
+
+    private static Stream<Arguments> provideInvalidDataOfEditAgencyOfAccount() {
+        return Stream.of(
+                Arguments.of(new EditAgencyOfAccountRequest(null, 123456, 1234, 1234)),
+                Arguments.of(new EditAgencyOfAccountRequest("", 123456, 1234, 1234)),
+                Arguments.of(new EditAgencyOfAccountRequest("999", 123456, 1234, 1234)),
+                Arguments.of(new EditAgencyOfAccountRequest("9999999999999999", 123456, 1234, 1234)),
+                Arguments.of(new EditAgencyOfAccountRequest("22233344455", null, 1234, 1234)),
+                Arguments.of(new EditAgencyOfAccountRequest("22233344455", 1234, null, 1234)),
+                Arguments.of(new EditAgencyOfAccountRequest("22233344455", 1234, 1234, null))
+        );
     }
 }
