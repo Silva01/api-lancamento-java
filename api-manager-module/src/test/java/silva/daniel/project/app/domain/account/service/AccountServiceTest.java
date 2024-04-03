204ded9282a6f57d919460d726fc5afddb7fc2ff
silva01 <silva20102@gmail.com>
2024-04-03 07:16:51 -0300

    test: #64 Added method test for service for flux editAgencyOfAccount_WithClientNotExists_ReturnsException

--- api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/AccountServiceTest.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/AccountServiceTest.java
@@ -1,6 +1,6 @@
 package silva.daniel.project.app.domain.account.service;
 
-import br.net.silva.business.value_object.input.ChangeAgencyInput;
+import br.net.silva.daniel.exception.ClientNotExistsException;
 import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import org.junit.jupiter.api.Test;
@@ -8,15 +8,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import silva.daniel.project.app.commons.InputBuilderCommons;
 import silva.daniel.project.app.service.FluxService;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static silva.daniel.project.app.commons.FailureMessageEnum.CLIENT_NOT_FOUND_MESSAGE;
 
 @ExtendWith(MockitoExtension.class)
-class AccountServiceTest {
+class AccountServiceTest implements InputBuilderCommons {
 
     @InjectMocks
     private AccountService service;
@@ -33,7 +36,17 @@ class AccountServiceTest {
         doAnswer((argumentsOnMock) -> null).when(facade).exec(any(Source.class));
 
         assertThatCode(() -> service
-                .editAgencyOfAccount(new ChangeAgencyInput("12344455566", 1234, 1234, 1234)))
+                .editAgencyOfAccount(buildNewBaseChangeAgencyInput()))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void editAgencyOfAccount_WithClientNotExists_ReturnsException() throws Exception {
+        when(fluxService.fluxEditAgencyOfAccount()).thenReturn(facade);
+        doThrow(new ClientNotExistsException(CLIENT_NOT_FOUND_MESSAGE.getMessage())).when(facade).exec(any(Source.class));
+
+        assertThatCode(() -> service.editAgencyOfAccount(buildNewBaseChangeAgencyInput()))
+                .isInstanceOf(ClientNotExistsException.class)
+                .hasMessage(CLIENT_NOT_FOUND_MESSAGE.getMessage());
     }
 }
