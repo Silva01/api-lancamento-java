19c259a17a605103a9592de750bea6c396256d30
silva01 <silva20102@gmail.com>
2024-04-03 05:37:04 -0300

    refactor: #63 Added new validate when account exists and deactivated

---
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountExistsAndDeactivatedValidate.java
@@ -0,0 +1,36 @@
+package br.net.silva.business.validations;
+
+import br.net.silva.business.build.AccountBuilder;
+import br.net.silva.business.exception.AccountAlreadyActiveException;
+import br.net.silva.business.exception.AccountNotExistsException;
+import br.net.silva.business.value_object.output.AccountOutput;
+import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.interfaces.IAccountParam;
+import br.net.silva.daniel.shared.application.interfaces.IValidations;
+import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
+
+public class AccountExistsAndDeactivatedValidate implements IValidations {
+
+    private final FindApplicationBaseGateway<AccountOutput> findAccountGateway;
+
+    public AccountExistsAndDeactivatedValidate(FindApplicationBaseGateway<AccountOutput> findAccountGateway) {
+        this.findAccountGateway = findAccountGateway;
+    }
+
+    @Override
+    public void validate(Source input) throws GenericException {
+        var dto = (IAccountParam) input.input();
+        var optionalAccount = findAccountGateway.findById(dto);
+
+        if (optionalAccount.isEmpty()) {
+            throw new AccountNotExistsException("Account not exists");
+        }
+
+        var accountDto = AccountBuilder.buildFullAccountDto().createFrom(optionalAccount.get());
+        if (accountDto.active()) {
+            throw new AccountAlreadyActiveException("Account already active");
+        }
+
+    }
+}
