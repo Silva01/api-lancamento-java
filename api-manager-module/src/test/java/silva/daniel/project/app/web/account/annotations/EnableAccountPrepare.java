6cde94cd5bd6cc874fa323de626674dfa5228097
silva01 <silva20102@gmail.com>
2024-04-03 06:17:38 -0300

    test: #64 Added annotation for load prepare of account flux

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/web/account/annotations/EnableAccountPrepare.java
@@ -0,0 +1,23 @@
+package silva.daniel.project.app.web.account.annotations;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import silva.daniel.project.app.web.AccountController;
+import silva.daniel.project.app.web.CreditCardController;
+import silva.daniel.project.app.web.account.CreateCreditCardPrepare;
+import silva.daniel.project.app.web.account.DeactivateCreditCardPrepare;
+import silva.daniel.project.app.web.account.EditAgencyOfAccountPrepare;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@WebMvcTest(AccountController.class)
+@Import({
+        EditAgencyOfAccountPrepare.class
+})
+public @interface EnableAccountPrepare {
+}
