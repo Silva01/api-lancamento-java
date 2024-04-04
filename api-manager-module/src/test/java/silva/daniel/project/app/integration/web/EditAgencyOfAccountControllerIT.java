882c02925bd7875881823a8448b382da072494b8
silva01 <silva20102@gmail.com>
2024-04-04 05:58:42 -0300

    test: #64 Added method test for service for flux editAgencyOfAccount_WithInvalidData_ReturnsStatus406

--- api-manager-module/src/test/java/silva/daniel/project/app/integration/web/EditAgencyOfAccountControllerIT.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/integration/web/EditAgencyOfAccountControllerIT.java
@@ -1,6 +1,10 @@
 package silva.daniel.project.app.integration.web;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -10,13 +14,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import silva.daniel.project.app.commons.FailureMessageEnum;
 import silva.daniel.project.app.commons.MysqlTestContainer;
 import silva.daniel.project.app.domain.account.entity.AccountKey;
 import silva.daniel.project.app.domain.account.repository.AccountRepository;
 import silva.daniel.project.app.domain.account.request.EditAgencyOfAccountRequest;
 import silva.daniel.project.app.domain.client.FailureResponse;
 
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static silva.daniel.project.app.commons.FailureMessageEnum.INVALID_DATA_MESSAGE;
 
 @ActiveProfiles("e2e")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,7 +40,7 @@ class EditAgencyOfAccountControllerIT extends MysqlTestContainer {
 
     @Test
     void editAgencyOfAccount_WithValidData_ReturnsStatus200() {
-        final var request = new EditAgencyOfAccountRequest("12345678901", 1234, 1, 2);
+        final var request = buildRequestBase();
         var httpEntity = new HttpEntity<>(request);
         var sut = restTemplate.exchange("/api/account/update/agency", HttpMethod.PUT, httpEntity, Void.class);
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -42,5 +50,28 @@ class EditAgencyOfAccountControllerIT extends MysqlTestContainer {
         assertThat(account.get().getKeys().getBankAgencyNumber()).isEqualTo(request.newAgencyNumber());
     }
 
+    @ParameterizedTest
+    @MethodSource("provideInvalidData")
+    void editAgencyOfAccount_WithInvalidData_ReturnsStatus406(EditAgencyOfAccountRequest request) {
+        var httpEntity = new HttpEntity<>(request);
+        var sut = restTemplate.exchange("/api/account/update/agency", HttpMethod.PUT, httpEntity, FailureResponse.class);
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE);
+        assertThat(sut.getBody()).isEqualTo(INVALID_DATA_MESSAGE);
+    }
 
+    private static Stream<Arguments> provideInvalidData() {
+        return Stream.of(
+                Arguments.of(new EditAgencyOfAccountRequest(null, 1234, 1, 2)),
+                Arguments.of(new EditAgencyOfAccountRequest("", 1234, 1, 2)),
+                Arguments.of(new EditAgencyOfAccountRequest("12345678901", 1234, 1, null)),
+                Arguments.of(new EditAgencyOfAccountRequest("12345678901", 1234, null, 2)),
+                Arguments.of(new EditAgencyOfAccountRequest("12345678901", null, 1, 2)),
+                Arguments.of(new EditAgencyOfAccountRequest(null, null, null, null))
+        );
+    }
+
+    @NotNull
+    private static EditAgencyOfAccountRequest buildRequestBase() {
+        return new EditAgencyOfAccountRequest("12345678901", 1234, 1, 2);
+    }
 }
