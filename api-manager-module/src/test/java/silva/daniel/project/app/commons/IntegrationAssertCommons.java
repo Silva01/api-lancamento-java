abadbc202c144b784509bd98b61d69d51fa3a776
silva01 <silva20102@gmail.com>
2024-04-04 06:39:08 -0300

    test: #64 Added new method assert for interface IntegrationAssertCommons

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/IntegrationAssertCommons.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/IntegrationAssertCommons.java
@@ -5,9 +5,23 @@ import org.springframework.http.ResponseEntity;
 import silva.daniel.project.app.domain.client.FailureResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static silva.daniel.project.app.commons.FailureMessageEnum.ACCOUNT_NOT_FOUND_MESSAGE;
+import static silva.daniel.project.app.commons.FailureMessageEnum.CLIENT_NOT_FOUND_MESSAGE;
+import static silva.daniel.project.app.commons.FailureMessageEnum.INVALID_DATA_MESSAGE;
 
 public interface IntegrationAssertCommons {
 
+    default void assertInvalidData(ResponseEntity<FailureResponse> sut) {
+        assertFailureApi(sut, INVALID_DATA_MESSAGE);
+    }
+
+    default void assertClientNotExists(ResponseEntity<FailureResponse> sut) {
+        assertFailureApi(sut, CLIENT_NOT_FOUND_MESSAGE);
+    }
+
+    default void assertAccountNotExists(ResponseEntity<FailureResponse> sut) {
+        assertFailureApi(sut, ACCOUNT_NOT_FOUND_MESSAGE);
+    }
     default void assertFailureApi(ResponseEntity<FailureResponse> sut, FailureResponse expected) {
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.resolve(expected.getStatusCode()));
         assertThat(sut.getBody()).isNotNull();
