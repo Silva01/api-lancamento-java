6400921b80bb6d1de007f1d3bcac0cfaecc6ccda
silva01 <silva20102@gmail.com>
2024-04-06 11:16:24 -0300

    feat: #65 Added return for account data

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/service/AccountService.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/service/AccountService.java
@@ -2,6 +2,7 @@ package silva.daniel.project.app.domain.account.service;
 
 import br.net.silva.business.value_object.input.ChangeAgencyInput;
 import br.net.silva.business.value_object.input.GetInformationAccountInput;
+import br.net.silva.business.value_object.output.GetInformationAccountOutput;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import org.springframework.stereotype.Service;
 import silva.daniel.project.app.service.FluxService;
@@ -19,7 +20,8 @@ public class AccountService {
         fluxService.fluxEditAgencyOfAccount().exec(Source.of(changeAgency));
     }
 
-    public void getAccountByCpf(final GetInformationAccountInput input) throws Exception {
+    public GetInformationAccountOutput getAccountByCpf(final GetInformationAccountInput input) throws Exception {
         //TODO: Will be implemented
+        return null;
     }
 }
--- api-manager-module/src/main/java/silva/daniel/project/app/web/AccountController.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/AccountController.java
@@ -2,8 +2,10 @@ package silva.daniel.project.app.web;
 
 import br.net.silva.business.value_object.input.ChangeAgencyInput;
 import br.net.silva.business.value_object.input.GetInformationAccountInput;
+import br.net.silva.business.value_object.output.GetInformationAccountOutput;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,8 +35,7 @@ public class AccountController {
     }
 
     @GetMapping("/{cpf}")
-    @ResponseStatus(HttpStatus.OK)
-    public void getAccountByCpf(@PathVariable String cpf) throws Exception {
-        accountService.getAccountByCpf(new GetInformationAccountInput(cpf));
+    public ResponseEntity<GetInformationAccountOutput> getAccountByCpf(@PathVariable String cpf) throws Exception {
+        return ResponseEntity.ok(accountService.getAccountByCpf(new GetInformationAccountInput(cpf)));
     }
 }
