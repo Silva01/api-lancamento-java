ef11ce02a7bf3f3e8039e391c7036877b06035ab
silva01 <silva20102@gmail.com>
2024-04-13 08:34:24 -0300

    feat: #66 Added method for find all accounts

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/service/AccountService.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/service/AccountService.java
@@ -2,10 +2,14 @@ package silva.daniel.project.app.domain.account.service;
 
 import br.net.silva.business.value_object.input.ChangeAgencyInput;
 import br.net.silva.business.value_object.input.GetInformationAccountInput;
+import br.net.silva.business.value_object.output.AccountsByCpfResponseDto;
 import br.net.silva.business.value_object.output.GetInformationAccountOutput;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import org.springframework.stereotype.Service;
 import silva.daniel.project.app.service.FluxService;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -24,5 +28,9 @@ public class AccountService {
         var source = new Source(new GetInformationAccountOutput(), input);
         fluxService.fluxGetAccountByCpf().exec(source);
         return ((GetInformationAccountOutput) source.output());
+    }
+
+    public AccountsByCpfResponseDto getAllAccountsByCpf(String cpf) throws Exception {
+        return null;
     }
 }
