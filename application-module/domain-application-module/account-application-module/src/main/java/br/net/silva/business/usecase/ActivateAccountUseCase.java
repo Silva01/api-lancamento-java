ab8f2cf348d32ecc0e7c791b3b6cbcd26df73a3e
silva01 <silva20102@gmail.com>
2024-04-14 07:01:01 -0300

    refactor: #67 Refactored use case for activated account

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/ActivateAccountUseCase.java
@@ -1,37 +1,38 @@
 package br.net.silva.business.usecase;
 
 import br.net.silva.business.build.AccountBuilder;
+import br.net.silva.business.validations.AccountExistsAndDeactivatedValidate;
 import br.net.silva.business.value_object.input.ActivateAccount;
 import br.net.silva.business.value_object.output.AccountOutput;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.factory.CreateAccountByAccountDTOFactory;
-import br.net.silva.daniel.shared.application.interfaces.EmptyOutput;
+import br.net.silva.daniel.shared.application.annotations.ValidateStrategyOn;
+import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
-import br.net.silva.daniel.shared.application.gateway.Repository;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
 
-public final class ActivateAccountUseCase implements UseCase<EmptyOutput> {
+@ValidateStrategyOn(validations = {AccountExistsAndDeactivatedValidate.class})
+public final class ActivateAccountUseCase implements UseCase<AccountOutput> {
 
-    private final Repository<AccountOutput> activateAccountRepository;
-    private final Repository<AccountOutput> findAccountRepository;
+    private final ApplicationBaseGateway<AccountOutput> baseGateway;
 
     private final CreateAccountByAccountDTOFactory accountFactory;
 
-    public ActivateAccountUseCase(Repository<AccountOutput> activateAccountRepository, Repository<AccountOutput> findAccountRepository) {
-        this.activateAccountRepository = activateAccountRepository;
-        this.findAccountRepository = findAccountRepository;
+    public ActivateAccountUseCase(ApplicationBaseGateway<AccountOutput> baseGateway) {
+        this.baseGateway = baseGateway;
         this.accountFactory = new CreateAccountByAccountDTOFactory();
     }
 
     @Override
-    public EmptyOutput exec(Source param) throws GenericException {
+    public AccountOutput exec(Source param) throws GenericException {
         try {
             var dto = (ActivateAccount) param.input();
-            var accountOutput = findAccountRepository.exec(dto.accountNumber(), dto.agency(), dto.cpf());
+            var accountOutput = execValidate(baseGateway.findById(dto)).extract();
+
             var account = accountFactory.create(AccountBuilder.buildFullAccountDto().createFrom(accountOutput));
             account.activate();
-            activateAccountRepository.exec(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
-            return EmptyOutput.INSTANCE;
+
+            return baseGateway.save(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
         } catch (Exception e) {
             throw new GenericException(e.getMessage());
         }
