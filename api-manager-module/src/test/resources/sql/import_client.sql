120af97c5122ad7d62d80fb1daf1dff6f918d881
silva01 <silva20102@gmail.com>
2024-04-14 06:00:57 -0300

    test: #66 Added new client without account

--- api-manager-module/src/test/resources/sql/import_client.sql
+++ api-manager-module/src/test/resources/sql/import_client.sql
@@ -5,9 +5,11 @@ INSERT INTO address (id, street, number, complement, neighborhood, state, city, 
 INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES (4, 'Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
 INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES (5, 'Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
 INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES (6, 'Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
+INSERT INTO address (id, street, number, complement, neighborhood, state, city, zip_code) VALUES (7, 'Rua Teste', '123', 'Complemento', 'Bairro Teste', 'SP', 'São Paulo', '12345678');
 
 -- Clients
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (1, '12345678901', 'Teste', '12345678901', true, 1);
+INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (5, '12345678988', 'Teste', '12345678901', true, 7);
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (2, '12345678903', 'Teste', '12345678901', false, 2);
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (3, '12345678904', 'Teste', '12345678901', true, 3);
 INSERT INTO client (aggregate_id, cpf, name, telephone, active, address_id) VALUES (3, '12345678905', 'Teste 05', '12345678901', true, 6);
