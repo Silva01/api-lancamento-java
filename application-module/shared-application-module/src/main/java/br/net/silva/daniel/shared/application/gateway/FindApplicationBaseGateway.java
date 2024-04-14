ea0ce2d8aad568427254ec4b75eaae9a950474ae
silva01 <silva20102@gmail.com>
2024-04-14 05:39:28 -0300

    refactor: #66 Removed method not used

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/gateway/FindApplicationBaseGateway.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/gateway/FindApplicationBaseGateway.java
@@ -1,7 +1,5 @@
 package br.net.silva.daniel.shared.application.gateway;
 
-import br.net.silva.daniel.shared.business.utils.GenericErrorUtils;
-
 import java.util.List;
 import java.util.Optional;
 
