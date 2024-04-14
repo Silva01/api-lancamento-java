9e3d8a5ca3b5a7e049b76eca6fcce879bf82fc78
silva01 <silva20102@gmail.com>
2024-04-14 05:32:29 -0300

    refactor: #66 Added new method for get all accounts by cpf in repository

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/repository/AccountRepository.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/repository/AccountRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import silva.daniel.project.app.domain.account.entity.Account;
 import silva.daniel.project.app.domain.account.entity.AccountKey;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, AccountKey> {
     Optional<Account> findByCpf(String cpf);
+    List<Account> findAllByCpf(String cpf);
 }
