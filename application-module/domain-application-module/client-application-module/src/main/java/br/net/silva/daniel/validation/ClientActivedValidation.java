e81e2512e26ce88d0bbe03cf774232411e458d1b
silva01 <silva20102@gmail.com>
2024-04-14 07:38:04 -0300

    feat: #67 Created validation for client is active

---
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientActivedValidation.java
@@ -0,0 +1,18 @@
+package br.net.silva.daniel.validation;
+
+import br.net.silva.daniel.exception.ClientDeactivatedException;
+import br.net.silva.daniel.shared.application.interfaces.Validation;
+import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.value_object.output.ClientOutput;
+
+import java.util.Optional;
+
+public class ClientActivedValidation implements Validation<ClientOutput> {
+
+    @Override
+    public void validate(Optional<ClientOutput> opt) throws GenericException {
+        var isClientDeactivated = opt.isPresent() && !opt.get().active();
+        if (isClientDeactivated)
+            throw new ClientDeactivatedException("Client is not actived");
+    }
+}
