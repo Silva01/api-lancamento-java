b14b32685d524dc992b3b965033b6ef350681ffd
silva01 <silva20102@gmail.com>
2024-04-15 07:21:17 -0300

    test: #67 Added new method to assert account already active

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/IntegrationAssertCommons.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/IntegrationAssertCommons.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import silva.daniel.project.app.domain.client.FailureResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static silva.daniel.project.app.commons.FailureMessageEnum.ACCOUNT_ALREADY_ACTIVATED_MESSAGE;
 import static silva.daniel.project.app.commons.FailureMessageEnum.ACCOUNT_ALREADY_DEACTIVATED_MESSAGE;
 import static silva.daniel.project.app.commons.FailureMessageEnum.ACCOUNT_ALREADY_WITH_NEW_AGENCY_NUMBER_MESSAGE;
 import static silva.daniel.project.app.commons.FailureMessageEnum.ACCOUNT_NOT_FOUND_MESSAGE;
@@ -54,6 +55,10 @@ public interface IntegrationAssertCommons {
 
     default void assertAccountAlreadyDeactivated(ResponseEntity<FailureResponse> sut) {
         assertFailureApi(sut, ACCOUNT_ALREADY_DEACTIVATED_MESSAGE);
+    }
+
+    default void assertAccountAlreadyActive(ResponseEntity<FailureResponse> sut) {
+        assertFailureApi(sut, ACCOUNT_ALREADY_ACTIVATED_MESSAGE);
     }
 
     default void assertCreditCardNotExists(ResponseEntity<FailureResponse> sut) {
