b81f5f1a59b3c323d0db994216b6f6f4f4f43d23
silva01 <silva20102@gmail.com>
2024-04-15 07:02:36 -0300

    test: #67 Added integration test for flux activateAccount_WithValidData_ReturnsSuccess

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/integration/web/ActivateAccountControllerIT.java
@@ -0,0 +1,50 @@
+package silva.daniel.project.app.integration.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import silva.daniel.project.app.commons.IntegrationAssertCommons;
+import silva.daniel.project.app.commons.MysqlTestContainer;
+import silva.daniel.project.app.commons.RequestIntegrationCommons;
+import silva.daniel.project.app.domain.account.entity.AccountKey;
+import silva.daniel.project.app.domain.account.repository.AccountRepository;
+import silva.daniel.project.app.domain.account.request.ActivateAccountRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("e2e")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/delete_client.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+@Sql(scripts = {"/sql/import_client.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+class ActivateAccountControllerIT extends MysqlTestContainer implements IntegrationAssertCommons {
+
+    private static final String API_ACTIVATE_ACCOUNT = "/api/account/activate";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private AccountRepository repository;
+
+    @Autowired
+    private RequestIntegrationCommons requestCommons;
+
+    @Test
+    void activateAccount_WithValidData_ReturnsSuccess() {
+        var request = new ActivateAccountRequest("12345678903", 1, 1235);
+        var response = restTemplate.postForEntity(API_ACTIVATE_ACCOUNT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        var sut = repository.findById(new AccountKey(request.accountNumber(), request.agency()));
+        assertThat(sut).isPresent();
+        assertThat(sut.get().isActive()).isTrue();
+
+    }
+}
