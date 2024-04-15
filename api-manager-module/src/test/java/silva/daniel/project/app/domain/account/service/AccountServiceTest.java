f37407c52862cbb7a15e555791d977601ab2b285
silva01 <silva20102@gmail.com>
2024-04-15 06:31:58 -0300

    test: #67 Added test in class AccountService to flux activateAccount_WithValidData_executeWithoutThrowsException

--- api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/AccountServiceTest.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/domain/account/service/AccountServiceTest.java
@@ -2,11 +2,13 @@ package silva.daniel.project.app.domain.account.service;
 
 import br.net.silva.business.exception.AccountAlreadyExistsForNewAgencyException;
 import br.net.silva.business.exception.AccountNotExistsException;
+import br.net.silva.business.value_object.input.ActivateAccount;
 import br.net.silva.business.value_object.input.GetInformationAccountInput;
 import br.net.silva.business.value_object.output.GetInformationAccountOutput;
 import br.net.silva.daniel.exception.ClientNotExistsException;
 import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -128,6 +130,15 @@ class AccountServiceTest implements InputBuilderCommons {
         doAnswer((argumentsOnMock) -> null).when(facade).exec(any(Source.class));
 
         assertThatCode(() -> service.getAllAccountsByCpf(new GetInformationAccountInput("123")))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void activateAccount_WithValidData_executeWithoutThrowsException() throws Exception {
+        when(fluxService.fluxActivateAccount()).thenReturn(facade);
+        doAnswer((argumentsOnMock) -> null).when(facade).exec(any(Source.class));
+
+        assertThatCode(() -> service.activateAccount(new ActivateAccount(1, 2, "123444")))
                 .doesNotThrowAnyException();
     }
 }
