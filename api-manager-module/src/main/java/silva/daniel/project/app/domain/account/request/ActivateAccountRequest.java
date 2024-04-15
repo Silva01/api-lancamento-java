d91d8856653f3ebca48ee43beb55e536ffa6a517
silva01 <silva20102@gmail.com>
2024-04-15 06:00:09 -0300

    refactor: #67 Refactored validation the request to activate account

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/ActivateAccountRequest.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/ActivateAccountRequest.java
@@ -1,13 +1,13 @@
 package silva.daniel.project.app.domain.account.request;
 
-import jakarta.validation.constraints.NegativeOrZero;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record ActivateAccountRequest(
         @NotBlank @Size(min = 11, max = 11) String cpf,
-        @NotNull @NegativeOrZero Integer agency,
-        @NotNull @NegativeOrZero Integer accountNumber
+        @NotNull @Positive Integer agency,
+        @NotNull @Positive Integer accountNumber
 ) {
 }
