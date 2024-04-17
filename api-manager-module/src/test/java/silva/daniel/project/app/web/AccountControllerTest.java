72ac3a6757064e092055d912b88b906947a089fe
silva01 <silva20102@gmail.com>
2024-04-17 06:01:29 -0300

    test: #68 Added test for flux deactivateAccount_WithAccountNotExists_ReturnsStatus404

--- api-manager-module/src/test/java/silva/daniel/project/app/web/AccountControllerTest.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/web/AccountControllerTest.java
@@ -194,6 +194,12 @@ class AccountControllerTest implements RequestBuilderCommons {
         deactivateAccountTestPrepare.failurePostAssert(buildBaseDeactivateAccount(), CLIENT_NOT_FOUND_MESSAGE, status().isNotFound());
     }
 
+    @Test
+    void deactivateAccount_WithAccountNotExists_ReturnsStatus404() throws Exception {
+        doThrow(new AccountNotExistsException("Account not Found")).when(accountService).deactivateAccount(any(DeactivateAccount.class));
+        deactivateAccountTestPrepare.failurePostAssert(buildBaseDeactivateAccount(), ACCOUNT_NOT_FOUND_MESSAGE, status().isNotFound());
+    }
+
     private static Stream<Arguments> provideInvalidDataOfEditAgencyOfAccount() {
         return Stream.of(
                 Arguments.of(new EditAgencyOfAccountRequest(null, 123456, 1234, 1234)),
