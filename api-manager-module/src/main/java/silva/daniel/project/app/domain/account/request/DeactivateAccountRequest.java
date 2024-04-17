7f2683773f212321dd8dd0e2dfedd026ab2414f9
silva01 <silva20102@gmail.com>
2024-04-17 05:54:18 -0300

    refactor: #68 Added annotations for creating constructor with parameters

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/DeactivateAccountRequest.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/DeactivateAccountRequest.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class DeactivateAccountRequest {
     @NotBlank @Size(min = 11, max = 11)
     private String cpf;
