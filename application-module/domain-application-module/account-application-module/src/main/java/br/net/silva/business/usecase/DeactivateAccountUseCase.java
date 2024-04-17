7dec25b1b1038b317fab53d181f44a42e30f75d1
silva01 <silva20102@gmail.com>
2024-04-17 06:54:52 -0300

    refactor: #68 Refactored for use validation strategy pattern

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateAccountUseCase.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/usecase/DeactivateAccountUseCase.java
@@ -1,15 +1,16 @@
 package br.net.silva.business.usecase;
 
 import br.net.silva.business.build.AccountBuilder;
-import br.net.silva.business.exception.AccountDeactivatedException;
-import br.net.silva.business.exception.AccountNotExistsException;
+import br.net.silva.business.validations.AccountExistsAndActiveValidate;
 import br.net.silva.business.value_object.output.AccountOutput;
+import br.net.silva.daniel.shared.application.annotations.ValidateStrategyOn;
 import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 
+@ValidateStrategyOn(validations = {AccountExistsAndActiveValidate.class})
 public class DeactivateAccountUseCase implements UseCase<AccountOutput> {
 
     private final ApplicationBaseGateway<AccountOutput> baseAccountGateway;
@@ -20,25 +21,12 @@ public class DeactivateAccountUseCase implements UseCase<AccountOutput> {
 
     @Override
     public AccountOutput exec(Source param) throws GenericException {
-        try {
-            var input = (ICpfParam) param.input();
-            var optAccount = baseAccountGateway.findById(input);
+        var input = (ICpfParam) param.input();
+        var optAccount = baseAccountGateway.findById(input);
 
-            if (optAccount.isEmpty()) {
-                throw new AccountNotExistsException("Account not found");
-            }
-
-            if (!optAccount.get().active()) {
-                throw new AccountDeactivatedException("Account is Deactivated");
-            }
-
-            var account = AccountBuilder.buildAggregate().createFrom(optAccount.get());
-            account.deactivate();
-            return baseAccountGateway.save(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
-        } catch (AccountNotExistsException | AccountDeactivatedException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new GenericException("Error on deactivate account", e);
-        }
+        var accountOutput = execValidate(optAccount).extract();
+        var account = AccountBuilder.buildAggregate().createFrom(accountOutput);
+        account.deactivate();
+        return baseAccountGateway.save(AccountBuilder.buildFullAccountOutput().createFrom(account.build()));
     }
 }
