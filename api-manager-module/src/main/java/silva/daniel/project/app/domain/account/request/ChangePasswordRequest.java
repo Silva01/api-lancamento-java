2d1454722b61d84b432a0b7653239264cd871866
silva01 <silva20102@gmail.com>
2024-04-18 06:04:25 -0300

    refactor: #69 Added annotation for create constructor with attributes

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/ChangePasswordRequest.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/ChangePasswordRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class ChangePasswordRequest {
     @NotNull @Positive
     private Integer agency;
