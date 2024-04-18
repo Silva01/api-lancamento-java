8f30bd3309b30306fa855cf0dcd0f4ca58a71b78
silva01 <silva20102@gmail.com>
2024-04-18 05:52:40 -0300

    feat: #69 Added request for change password in the account

---
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/account/request/ChangePasswordRequest.java
@@ -0,0 +1,27 @@
+package silva.daniel.project.app.domain.account.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ChangePasswordRequest {
+    @NotNull @Positive
+    private Integer agency;
+
+    @NotNull @Positive
+    private Integer accountNumber;
+
+    @NotBlank @Size(min = 11, max = 11)
+    private String cpf;
+
+    @NotBlank @Size(min = 6)
+    private String password;
+
+    @NotBlank @Size(min = 6)
+    private String newPassword;
+}
