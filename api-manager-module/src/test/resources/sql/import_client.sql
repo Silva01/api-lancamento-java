b1e3ab31efaadf9851545a8eb0ccf74e8b8190a3
silva01 <silva20102@gmail.com>
2024-04-19 05:35:31 -0300

    test: #69 Refactored crypto that passwords in scripts

--- api-manager-module/src/test/resources/sql/import_client.sql
+++ api-manager-module/src/test/resources/sql/import_client.sql
@@ -21,9 +21,9 @@ INSERT INTO credit_card (number, cvv, flag, balance, expiration_date, active) VA
 INSERT INTO credit_card (number, cvv, flag, balance, expiration_date, active) VALUES ('1234567890123457', 123, 'MASTER_CARD', 3000, '2022-12-01', false);
 
 -- Account of clients
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1234, 1, 3000, MD5('123456'), true, '12345678901');
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1235, 1, 3000, MD5('123456'), false, '12345678903');
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1236, 1, 3000, MD5('123456'), false, '12345678904');
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1234, 5, 3000, MD5('123456'), false, '12345678905');
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf, credit_card_number) VALUES (1237, 1, 3000, MD5('123456'), true, '12345678910', '1234567890123456');
-INSERT INTO account (number, bank_agency_number, balance, password, active, cpf, credit_card_number) VALUES (1238, 1, 3000, MD5('123456'), true, '12345678911', '1234567890123457');
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1234, 1, 3000, SHA2('123456', 256), true, '12345678901');
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1235, 1, 3000, SHA2('123456', 256), false, '12345678903');
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1236, 1, 3000, SHA2('123456', 256), false, '12345678904');
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf) VALUES (1234, 5, 3000, SHA2('123456', 256), false, '12345678905');
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf, credit_card_number) VALUES (1237, 1, 3000, SHA2('123456', 256), true, '12345678910', '1234567890123456');
+INSERT INTO account (number, bank_agency_number, balance, password, active, cpf, credit_card_number) VALUES (1238, 1, 3000, SHA2('123456', 256), true, '12345678911', '1234567890123457');
