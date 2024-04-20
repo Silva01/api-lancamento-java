816e36930b52463d1dc798b4179506530da8e463
silva01 <silva20102@gmail.com>
2024-04-20 06:22:48 -0300

    refactor: #70 Refactored class for have new method for create client

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewClientUseCase.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/usecase/CreateNewClientUseCase.java
@@ -8,6 +8,7 @@ import br.net.silva.daniel.shared.application.annotations.ValidateStrategyOn;
 import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.gateway.SaveApplicationBaseGateway;
+import br.net.silva.daniel.shared.application.interfaces.IAddressParam;
 import br.net.silva.daniel.shared.application.interfaces.IClientParam;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
@@ -39,11 +40,15 @@ public final class CreateNewClientUseCase implements UseCase<ClientOutput> {
 
         var addressRequestDto = clientRequest.address();
 
-        var address = new AddressDTO(addressRequestDto.street(), addressRequestDto.number(), addressRequestDto.complement(), addressRequestDto.neighborhood(), addressRequestDto.state(), addressRequestDto.city(), addressRequestDto.zipCode());
-        var clientDto = new ClientDTO(clientRequest.cpf(), clientRequest.name(), clientRequest.telephone(), true, address);
+        final var clientDto = createNewClient(addressRequestDto, clientRequest);
 
         var clientOutput = saveRepository.save(ClientBuilder.buildFullClientOutput().createFrom(clientDto));
         factory.fillIn(clientOutput, param.output());
         return clientOutput;
+    }
+
+    private static ClientDTO createNewClient(IAddressParam addressRequestDto, IClientParam clientRequest) {
+        var address = new AddressDTO(addressRequestDto.street(), addressRequestDto.number(), addressRequestDto.complement(), addressRequestDto.neighborhood(), addressRequestDto.state(), addressRequestDto.city(), addressRequestDto.zipCode());
+        return new ClientDTO(clientRequest.cpf(), clientRequest.name(), clientRequest.telephone(), true, address);
     }
 }
