27b3e46422556955a6782168033c6a3dcab54a04
silva01 <silva20102@gmail.com>
2024-04-20 06:19:12 -0300

    refactor: #70 Refactored validate for use Validation interface

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientNotExistsValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientNotExistsValidate.java
@@ -1,15 +1,22 @@
 package br.net.silva.daniel.validation;
 
 import br.net.silva.daniel.exception.ExistsClientRegistredException;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import br.net.silva.daniel.shared.application.interfaces.IValidations;
+import br.net.silva.daniel.shared.application.interfaces.Validation;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
-public class ClientNotExistsValidate implements IValidations {
+import java.util.Optional;
+
+public class ClientNotExistsValidate implements IValidations, Validation<ClientOutput> {
     private final FindApplicationBaseGateway<ClientOutput> findClientRepository;
+
+    public ClientNotExistsValidate () {
+        this.findClientRepository = null;
+    }
 
     public ClientNotExistsValidate(FindApplicationBaseGateway<ClientOutput> findClientRepository) {
         this.findClientRepository = findClientRepository;
@@ -21,6 +28,13 @@ public class ClientNotExistsValidate implements IValidations {
         var optClient = findClientRepository.findById(param);
 
         if (optClient.isPresent()) {
+            throw new ExistsClientRegistredException("Client already exists in database");
+        }
+    }
+
+    @Override
+    public void validate(Optional<ClientOutput> opt) throws GenericException {
+        if (opt.isPresent()) {
             throw new ExistsClientRegistredException("Client already exists in database");
         }
     }
