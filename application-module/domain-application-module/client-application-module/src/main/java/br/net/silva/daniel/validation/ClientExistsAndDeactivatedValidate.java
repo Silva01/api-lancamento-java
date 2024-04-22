b54b500657a7d8292d1fd0ab8f66a072d2cca416
silva01 <silva20102@gmail.com>
2024-04-22 07:12:34 -0300

    refactor: #70 Refactored for use new interface of validation

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsAndDeactivatedValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsAndDeactivatedValidate.java
@@ -2,16 +2,24 @@ package br.net.silva.daniel.validation;
 
 import br.net.silva.daniel.exception.ClientNotExistsException;
 import br.net.silva.daniel.exception.ExistsClientRegistredException;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import br.net.silva.daniel.shared.application.interfaces.IValidations;
+import br.net.silva.daniel.shared.application.interfaces.Validation;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
-public class ClientExistsAndDeactivatedValidate implements IValidations {
+import java.util.Optional;
+
+public class ClientExistsAndDeactivatedValidate implements IValidations, Validation<ClientOutput> {
     private final FindApplicationBaseGateway<ClientOutput> findClientRepository;
 
+    public ClientExistsAndDeactivatedValidate() {
+        this.findClientRepository = null;
+    }
+
+    @Deprecated(forRemoval = true)
     public ClientExistsAndDeactivatedValidate(FindApplicationBaseGateway<ClientOutput> findClientRepository) {
         this.findClientRepository = findClientRepository;
     }
@@ -21,6 +29,17 @@ public class ClientExistsAndDeactivatedValidate implements IValidations {
         var param = (ICpfParam) input.input();
         var optClient = findClientRepository.findById(param);
 
+        if (optClient.isEmpty()) {
+            throw new ClientNotExistsException("Client not exists in database");
+        }
+
+        if (optClient.get().active()) {
+            throw new ExistsClientRegistredException("Client already activated");
+        }
+    }
+
+    @Override
+    public void validate(Optional<ClientOutput> optClient) throws GenericException {
         if (optClient.isEmpty()) {
             throw new ClientNotExistsException("Client not exists in database");
         }
