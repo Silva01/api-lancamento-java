40e4460521ce215c1c793504fffef94eccc4c80e
silva01 <silva20102@gmail.com>
2024-04-23 07:03:09 -0300

    feat: #70 Added new method for use decorator when necessary

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/UseCase.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/UseCase.java
@@ -1,14 +1,23 @@
 package br.net.silva.daniel.shared.application.interfaces;
 
 import br.net.silva.daniel.shared.application.annotations.ValidateStrategyOn;
+import br.net.silva.daniel.shared.application.decorator.ValidationDecorator;
 import br.net.silva.daniel.shared.application.exception.BuildValidationException;
 import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public interface UseCase<R> {
     R exec(Source param) throws GenericException;
+
+    default Extractor<R> execValidate(Optional<R> opt, ValidationDecorator<R> decorator) throws GenericException {
+        final var response = execValidate(opt);
+        decorator.decorator(opt.orElse(null));
+        return response;
+    }
 
     @SuppressWarnings("unchecked")
     default Extractor<R> execValidate(Optional<R> opt) throws GenericException {
