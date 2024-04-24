455766a88043039e9811f1f4ff756a27f036c537
silva01 <silva20102@gmail.com>
2024-04-24 07:26:53 -0300

    refactor: #70 Removed class and interfaces not used

--- application-module/domain-application-module/client-application-module/src/test/java/br/net/silva/daniel/facade/ClientFacadeTest.java
+++ application-module/domain-application-module/client-application-module/src/test/java/br/net/silva/daniel/facade/ClientFacadeTest.java
@@ -4,7 +4,6 @@ import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
 import br.net.silva.daniel.shared.application.gateway.ParamGateway;
 import br.net.silva.daniel.shared.application.interfaces.EmptyOutput;
 import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
-import br.net.silva.daniel.shared.application.interfaces.IValidations;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
 import br.net.silva.daniel.shared.application.value_object.Source;
@@ -12,8 +11,6 @@ import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.usecase.ActivateClientUseCase;
 import br.net.silva.daniel.usecase.CreateNewClientUseCase;
 import br.net.silva.daniel.usecase.DeactivateClientUseCase;
-import br.net.silva.daniel.validation.ClientExistsValidate;
-import br.net.silva.daniel.validation.ClientNotExistsValidate;
 import br.net.silva.daniel.value_object.input.*;
 import br.net.silva.daniel.value_object.output.AddressOutput;
 import br.net.silva.daniel.value_object.output.ClientOutput;
@@ -23,7 +20,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -31,9 +31,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class ClientFacadeTest {
-
-    private IValidations clientExistsValidate;
-    private IValidations clientNotExistsValidate;
 
     private UseCase<ClientOutput> createNewClientUseCase;
 
@@ -50,8 +47,6 @@ class ClientFacadeTest {
 
         var factory = new GenericResponseMapper(Collections.emptyList());
         this.createNewClientUseCase = new CreateNewClientUseCase(baseRepository, factory);
-        this.clientExistsValidate = new ClientExistsValidate(baseRepository);
-        this.clientNotExistsValidate = new ClientNotExistsValidate(baseRepository);
         this.deactivateClientUseCase = new DeactivateClientUseCase(baseRepository, factory);
         this.activateClientUseCase = new ActivateClientUseCase(baseRepository);
     }
@@ -67,7 +62,6 @@ class ClientFacadeTest {
         Queue<UseCase<?>> useCases = new LinkedList<>();
         useCases.add(createNewClientUseCase);
 
-        List<IValidations> validationsList = List.of();
         var clientFacade = new GenericFacadeDelegate<>(useCases);
 
         var clientRequestDto = new ClientRequestDTO("123", "99988877766", "Daniel", "6122223333", true, 222, new AddressRequestDTO("Rua 1", "123", "House", "Test", "DF", "Others", "12345-123"));
@@ -90,7 +84,6 @@ class ClientFacadeTest {
         Queue<UseCase<?>> useCases = new LinkedList<>();
         useCases.add(createNewClientUseCase);
 
-        List<IValidations> validationsList = List.of(clientNotExistsValidate);
         var clientFacade = new GenericFacadeDelegate<>(useCases);
 
         var findClientByCpf = new FindClientByCpf("99988877766");
@@ -109,7 +102,6 @@ class ClientFacadeTest {
         Queue<UseCase<?>> useCases = new LinkedList<>();
         useCases.add(deactivateClientUseCase);
 
-        List<IValidations> validationsList = List.of(clientExistsValidate);
         var clientFacade = new GenericFacadeDelegate<>(useCases);
 
         var deactivateClient = new DeactivateClient("99988877766");
@@ -127,7 +119,6 @@ class ClientFacadeTest {
         Queue<UseCase> useCases = new LinkedList<>();
         useCases.add(deactivateClientUseCase);
 
-        List<IValidations> validationsList = List.of(clientExistsValidate);
         var clientFacade = new GenericFacadeDelegate(useCases);
 
         var deactivateClient = new DeactivateClient("99988877766");
@@ -146,7 +137,6 @@ class ClientFacadeTest {
         Queue<UseCase> useCases = new LinkedList<>();
         useCases.add(activateClientUseCase);
 
-        List<IValidations> validationsList = List.of();
         var clientFacade = new GenericFacadeDelegate(useCases);
 
         var activateClient = new ActivateClient("99988877766");
