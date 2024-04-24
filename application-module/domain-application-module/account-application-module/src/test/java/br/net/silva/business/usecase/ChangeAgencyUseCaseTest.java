377e486c1a71155cb44986890d23a9929a662f35
silva01 <silva20102@gmail.com>
2024-04-24 06:49:16 -0300

    test: #70 Added new test for flux changeAgency_WithAccountNotExists_ThrowsAccountNotExistsException

--- application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/usecase/ChangeAgencyUseCaseTest.java
+++ application-module/domain-application-module/account-application-module/src/test/java/br/net/silva/business/usecase/ChangeAgencyUseCaseTest.java
@@ -1,5 +1,6 @@
 package br.net.silva.business.usecase;
 
+import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.business.value_object.input.ChangeAgencyInput;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.shared.application.gateway.ApplicationBaseGateway;
@@ -17,6 +18,7 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -41,17 +43,36 @@ class ChangeAgencyUseCaseTest {
                 .thenReturn(Optional.empty());
         doAnswer(invocation -> invocation.getArguments()[0]).when(baseGateway).save(any(AccountOutput.class));
 
-        var input = new ChangeAgencyInput("99988877766", 45678, 1234, 4321);
-        var source = new Source(EmptyOutput.INSTANCE, input);
+        final var input = buildInputBase();
+        final var source = new Source(EmptyOutput.INSTANCE, input);
 
-        var mockAccount = buildMockAccount(true);
         assertDoesNotThrow(() -> useCase.exec(source));
 
         verify(baseGateway, times(2)).save(any(AccountOutput.class));
+        verify(baseGateway, times(2)).findById(any(ParamGateway.class));
+    }
+
+    @Test
+    void changeAgency_WithAccountNotExists_ThrowsAccountNotExistsException() {
+        when(baseGateway.findById(any(ParamGateway.class)))
+                .thenReturn(Optional.empty());
+
+        final var input = buildInputBase();
+        final var source = new Source(EmptyOutput.INSTANCE, input);
+
+        assertThatThrownBy(() -> useCase.exec(source))
+                .isInstanceOf(AccountNotExistsException.class)
+                .hasMessage("Account not exists");
+
+        verify(baseGateway, never()).save(any(AccountOutput.class));
+        verify(baseGateway, times(2)).findById(any(ParamGateway.class));
     }
 
     private AccountOutput buildMockAccount(boolean active) {
         return new AccountOutput(1, 45678, BigDecimal.valueOf(1000), CryptoUtils.convertToSHA256("978534"), active, "99988877766", null, Collections.emptyList());
     }
 
+    private static ChangeAgencyInput buildInputBase() {
+        return new ChangeAgencyInput("99988877766", 45678, 1234, 4321);
+    }
 }
