0abcff9556746c52809f724ed14ed81ee5353a76
silva01 <silva20102@gmail.com>
2024-04-24 07:25:03 -0300

    refactor: #70 Removed class and interfaces not used

--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsAndActivedValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientExistsAndActivedValidate.java
@@ -2,41 +2,13 @@ package br.net.silva.daniel.validation;
 
 import br.net.silva.daniel.exception.ClientDeactivatedException;
 import br.net.silva.daniel.exception.ClientNotExistsException;
-import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
-import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
-import br.net.silva.daniel.shared.application.interfaces.IValidations;
 import br.net.silva.daniel.shared.application.interfaces.Validation;
-import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
 import java.util.Optional;
 
-public class ClientExistsAndActivedValidate implements IValidations, Validation<ClientOutput> {
-    private final FindApplicationBaseGateway<ClientOutput> findClientRepository;
-
-    public ClientExistsAndActivedValidate() {
-        this.findClientRepository = null;
-    }
-
-    @Deprecated(forRemoval = true)
-    public ClientExistsAndActivedValidate(FindApplicationBaseGateway<ClientOutput> findClientRepository) {
-        this.findClientRepository = findClientRepository;
-    }
-
-    @Override
-    public void validate(Source input) throws GenericException {
-        var param = (ICpfParam) input.input();
-        var optClient = findClientRepository.findById(param);
-
-        if (optClient.isEmpty()) {
-            throw new ClientNotExistsException("Client not exists in database");
-        }
-
-        if (!optClient.get().active()) {
-            throw new ClientDeactivatedException("Client is Deactivated");
-        }
-    }
+public class ClientExistsAndActivedValidate implements Validation<ClientOutput> {
 
     @Override
     public void validate(Optional<ClientOutput> optClient) throws GenericException {
--- application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientNotExistsValidate.java
+++ application-module/domain-application-module/client-application-module/src/main/java/br/net/silva/daniel/validation/ClientNotExistsValidate.java
@@ -1,36 +1,13 @@
 package br.net.silva.daniel.validation;
 
 import br.net.silva.daniel.exception.ExistsClientRegistredException;
-import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
-import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
-import br.net.silva.daniel.shared.application.interfaces.IValidations;
 import br.net.silva.daniel.shared.application.interfaces.Validation;
-import br.net.silva.daniel.shared.application.value_object.Source;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.value_object.output.ClientOutput;
 
 import java.util.Optional;
 
-public class ClientNotExistsValidate implements IValidations, Validation<ClientOutput> {
-    private final FindApplicationBaseGateway<ClientOutput> findClientRepository;
-
-    public ClientNotExistsValidate () {
-        this.findClientRepository = null;
-    }
-
-    public ClientNotExistsValidate(FindApplicationBaseGateway<ClientOutput> findClientRepository) {
-        this.findClientRepository = findClientRepository;
-    }
-
-    @Override
-    public void validate(Source input) throws GenericException {
-        var param = (ICpfParam) input.input();
-        var optClient = findClientRepository.findById(param);
-
-        if (optClient.isPresent()) {
-            throw new ExistsClientRegistredException("Client already exists in database");
-        }
-    }
+public class ClientNotExistsValidate implements Validation<ClientOutput> {
 
     @Override
     public void validate(Optional<ClientOutput> opt) throws GenericException {
