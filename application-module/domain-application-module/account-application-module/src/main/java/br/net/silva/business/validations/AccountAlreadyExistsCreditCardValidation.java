a832b06aa556c15fe59db74431c655e25ba5e0ef
silva01 <silva20102@gmail.com>
2024-04-24 05:44:44 -0300

    refactor: #70 Refactored for use new pattern

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountAlreadyExistsCreditCardValidation.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/validations/AccountAlreadyExistsCreditCardValidation.java
@@ -4,14 +4,23 @@ import br.net.silva.business.exception.CreditCardAlreadyExistsException;
 import br.net.silva.business.value_object.input.CreateCreditCardInput;
 import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.shared.application.gateway.FindApplicationBaseGateway;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.shared.application.interfaces.IValidations;
+import br.net.silva.daniel.shared.application.interfaces.Validation;
 import br.net.silva.daniel.shared.application.value_object.Source;
+import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.shared.business.utils.GenericErrorUtils;
 
-public class AccountAlreadyExistsCreditCardValidation implements IValidations {
+import java.util.Optional;
+
+public class AccountAlreadyExistsCreditCardValidation implements IValidations, Validation<AccountOutput> {
 
     private final FindApplicationBaseGateway<AccountOutput> findAccountGateway;
 
+    public AccountAlreadyExistsCreditCardValidation() {
+        this.findAccountGateway = null;
+    }
+
+    @Deprecated(forRemoval = true)
     public AccountAlreadyExistsCreditCardValidation(FindApplicationBaseGateway<AccountOutput> findAccountGateway) {
         this.findAccountGateway = findAccountGateway;
     }
@@ -22,6 +31,19 @@ public class AccountAlreadyExistsCreditCardValidation implements IValidations {
         var optionalAccount = findAccountGateway.findById(createCreditCardInput);
 
         var account = optionalAccount.get();
+
+        if (account.creditCard() != null){
+            throw new CreditCardAlreadyExistsException("Credit card already exists");
+        }
+    }
+
+    @Override
+    public void validate(Optional<AccountOutput> opt) throws GenericException {
+        if (opt.isEmpty()) {
+            throw GenericErrorUtils.executeExceptionNotPermissionOperation();
+        }
+
+        final var account = opt.get();
 
         if (account.creditCard() != null){
             throw new CreditCardAlreadyExistsException("Credit card already exists");
