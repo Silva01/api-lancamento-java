6a0409e3c514280813bbc4c27f420a9b34cef560
silva01 <silva20102@gmail.com>
2024-04-25 07:33:01 -0300

    refactor: #70 Marked method how deprecated

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/UseCaseBuilderSpec.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/UseCaseBuilderSpec.java
@@ -5,6 +5,8 @@ import br.net.silva.daniel.shared.application.interfaces.UseCase;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 
 public interface UseCaseBuilderSpec<T> {
+
+    @Deprecated(forRemoval = true)
     T withBuilderUseCases(Builder<UseCase<?>>... useCaseBuilders);
 
     default T andWithBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
