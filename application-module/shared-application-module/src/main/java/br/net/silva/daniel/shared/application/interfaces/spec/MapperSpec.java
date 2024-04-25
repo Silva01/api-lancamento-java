a1977aaecd0da30404ad0321c9e6ce7eebd975bb
silva01 <silva20102@gmail.com>
2024-04-25 06:06:15 -0300

    refactor: #70 Removed generic type of method withGenericMapper

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/MapperSpec.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/MapperSpec.java
@@ -4,5 +4,5 @@ import br.net.silva.daniel.shared.application.build.Builder;
 import br.net.silva.daniel.shared.application.mapper.GenericResponseMapper;
 
 public interface MapperSpec<T> {
-    Builder<T> withGenericMapper(GenericResponseMapper mapper);
+    <R> Builder<R> withGenericMapper(GenericResponseMapper mapper);
 }
