f5097b66e5ca48e18c4822cb8ff8726a82070243
silva01 <silva20102@gmail.com>
2024-04-26 06:16:56 -0300

    refactor: #70 Removed methods not used

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/UseCaseBuilderSpec.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/interfaces/spec/UseCaseBuilderSpec.java
@@ -2,18 +2,13 @@ package br.net.silva.daniel.shared.application.interfaces.spec;
 
 import br.net.silva.daniel.shared.application.build.Builder;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
-import br.net.silva.daniel.shared.business.exception.GenericException;
 
-public interface UseCaseBuilderSpec<T> {
+public interface UseCaseBuilderSpec<T> extends UseCaseBuilder<T> {
 
     @Deprecated(forRemoval = true)
-    T withBuilderUseCases(Builder<UseCase<?>>... useCaseBuilders);
+    Builder<T> withBuilderUseCases(Builder<UseCase<?>>... useCaseBuilders);
 
-    default T andWithBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
-        throw new RuntimeException("Not Permmited call");
-    }
-
-    default UseCaseBuilderSpec<T> withBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
+    default UseCaseBuilder<T> withBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
         throw new RuntimeException("Not Permmited call");
     }
 }
