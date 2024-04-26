e374107206d130ffc0984e3e430818a1d102456c
silva01 <silva20102@gmail.com>
2024-04-26 06:16:08 -0300

    refactor: #70 Refactored method of build to facade

--- application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/build/FacadeBuilder.java
+++ application-module/shared-application-module/src/main/java/br/net/silva/daniel/shared/application/build/FacadeBuilder.java
@@ -2,6 +2,7 @@ package br.net.silva.daniel.shared.application.build;
 
 import br.net.silva.daniel.shared.application.interfaces.GenericFacadeDelegate;
 import br.net.silva.daniel.shared.application.interfaces.UseCase;
+import br.net.silva.daniel.shared.application.interfaces.spec.UseCaseBuilder;
 import br.net.silva.daniel.shared.application.interfaces.spec.UseCaseBuilderSpec;
 import br.net.silva.daniel.shared.business.utils.GenericErrorUtils;
 
@@ -11,11 +12,11 @@ import java.util.stream.Stream;
 
 public interface FacadeBuilder {
 
-    static UseCaseBuilderSpec<Builder<GenericFacadeDelegate>> make() {
+    static UseCaseBuilderSpec<GenericFacadeDelegate> make() {
         return new FacadeProcessor();
     }
 
-    class FacadeProcessor implements UseCaseBuilderSpec<Builder<GenericFacadeDelegate>> {
+    class FacadeProcessor implements UseCaseBuilderSpec<GenericFacadeDelegate> {
 
         private final Queue<UseCase<?>> useCaseQueue;
 
@@ -30,15 +31,19 @@ public interface FacadeBuilder {
         }
 
         @Override
-        public UseCaseBuilderSpec<Builder<GenericFacadeDelegate>> withBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
+        public UseCaseBuilder<GenericFacadeDelegate> withBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
             addUseCase(useCaseBuilder);
             return this;
         }
 
         @Override
-        public Builder<GenericFacadeDelegate> andWithBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
-            addUseCase(useCaseBuilder);
-            return () -> new GenericFacadeDelegate(useCaseQueue);
+        public UseCaseBuilder<GenericFacadeDelegate> andWithBuilderUseCase(Builder<UseCase<?>> useCaseBuilder) {
+            return withBuilderUseCase(useCaseBuilder);
+        }
+
+        @Override
+        public GenericFacadeDelegate build() {
+            return new GenericFacadeDelegate(useCaseQueue);
         }
 
         private void addUseCase(Builder<UseCase<?>> useCaseBuilder) {
