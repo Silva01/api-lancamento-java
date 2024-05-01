c3048c0752b781f8bf82e5806563f3fdd9aebb74
silva01 <silva20102@gmail.com>
2024-05-01 08:55:11 -0300

    feat: #83 Added controller for transaction

---
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/TransactionController.java
@@ -0,0 +1,20 @@
+package silva.daniel.project.app.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import silva.daniel.project.app.domain.transaction.request.TransactionBatchRequest;
+
+@RestController
+@RequestMapping("/api/transaction")
+public final class TransactionController {
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
+    public void register(@RequestBody TransactionBatchRequest request) {
+
+    }
+}
