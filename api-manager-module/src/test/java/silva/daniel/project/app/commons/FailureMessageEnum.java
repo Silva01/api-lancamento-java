a95af21c380de08cddb77bb6bab4e0c2b2ba1f6c
silva01 <silva20102@gmail.com>
2024-05-02 07:30:07 -0300

    test: #83 Added new constant for transaction duplication

--- api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
+++ api-manager-module/src/test/java/silva/daniel/project/app/commons/FailureMessageEnum.java
@@ -19,4 +19,5 @@ public class FailureMessageEnum {
     public static final FailureResponse CREDIT_CARD_NOT_FOUND_MESSAGE = new FailureResponse("Credit card not exists in the account", HttpStatus.NOT_FOUND.value());
     public static final FailureResponse CREDIT_CARD_ALREADY_DEACTIVATED_MESSAGE = new FailureResponse("Credit card deactivated in the account", HttpStatus.CONFLICT.value());
     public static final FailureResponse CREDIT_CARD_ALREADY_EXISTS_MESSAGE = new FailureResponse("Credit card already exists", HttpStatus.CONFLICT.value());
+    public static final FailureResponse TRANSACTION_DUPLICATE_MESSAGE = new FailureResponse("Transaction already exists", HttpStatus.BAD_REQUEST.value());
 }
