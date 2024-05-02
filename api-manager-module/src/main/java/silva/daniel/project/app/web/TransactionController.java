a12d34bae1bd44312bebc82f8d8bf5710a821e41
silva01 <silva20102@gmail.com>
2024-05-02 06:59:23 -0300

    feat: #83 Refactored endpoint for register transaction

--- api-manager-module/src/main/java/silva/daniel/project/app/web/TransactionController.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/web/TransactionController.java
@@ -1,6 +1,10 @@
 package silva.daniel.project.app.web;
 
+import br.net.silva.business.value_object.input.AccountInput;
+import br.net.silva.business.value_object.input.BatchTransactionInput;
+import br.net.silva.business.value_object.input.TransactionInput;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,14 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import silva.daniel.project.app.domain.transaction.request.TransactionBatchRequest;
+import silva.daniel.project.app.domain.transaction.service.TransactionService;
 
 @RestController
 @RequestMapping("/api/transaction")
 public final class TransactionController {
 
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.OK)
-    public void register(@Valid @RequestBody TransactionBatchRequest request) {
+    public void register(@Valid @RequestBody TransactionBatchRequest request) throws Exception {
+        final var input = new BatchTransactionInput(
+                new AccountInput(request.source().account(), request.source().agency(), request.source().cpf()),
+                new AccountInput(request.destiny().account(), request.destiny().agency(), request.destiny().cpf()),
+        request.transactions().stream().map(transaction -> new TransactionInput(
+                transaction.id().longValue(),
+                transaction.description(),
+                transaction.price(),
+                transaction.quantity(),
+                transaction.type(),
+                transaction.idempotencyId(),
+                transaction.creditCardNumber(),
+                transaction.creditCardCvv()
+        )).toList());
 
+        transactionService.registerTransaction(input);
     }
 }
