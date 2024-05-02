fe986c6e1deb4d292a2e11ae4948501fc531b4ec
silva01 <silva20102@gmail.com>
2024-05-02 06:32:53 -0300

    refactor: #83 Added bean validation for attributes

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/transaction/request/TransactionBatchRequest.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/transaction/request/TransactionBatchRequest.java
@@ -1,10 +1,16 @@
 package silva.daniel.project.app.domain.transaction.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record TransactionBatchRequest(
-        String cpf,
-        Integer agency,
-        Integer account,
-        List<TransactionRequest> transactions
+        @NotBlank @Size(min = 11, max = 11) String cpf,
+        @NotNull @Positive Integer agency,
+        @NotNull @Positive Integer account,
+        @Valid @Size(min = 1) List<TransactionRequest> transactions
 ) {}
