26beeeff43ce336d4eb0079cd06fe69e3dfca537
silva01 <silva20102@gmail.com>
2024-05-02 06:59:44 -0300

    feat: #83 Refactored method for register transaction

--- api-manager-module/src/main/java/silva/daniel/project/app/domain/transaction/service/TransactionService.java
+++ api-manager-module/src/main/java/silva/daniel/project/app/domain/transaction/service/TransactionService.java
@@ -1,7 +1,10 @@
 package silva.daniel.project.app.domain.transaction.service;
 
+import br.net.silva.business.value_object.input.BatchTransactionInput;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
+    public void registerTransaction(BatchTransactionInput input) throws Exception {
+    }
 }
