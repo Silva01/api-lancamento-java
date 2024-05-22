46366b00291a2e13e1e1dcddfd21254219092be2
silva01 <silva20102@gmail.com>
2024-05-22 07:04:04 -0300

    refactor: #84 Implemented interface ICpfParam

--- application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/value_object/input/ReversalTransactionInput.java
+++ application-module/domain-application-module/account-application-module/src/main/java/br/net/silva/business/value_object/input/ReversalTransactionInput.java
@@ -1,11 +1,12 @@
 package br.net.silva.business.value_object.input;
 
+import br.net.silva.daniel.shared.application.interfaces.ICpfParam;
 import br.net.silva.daniel.shared.application.interfaces.Input;
 
 public record ReversalTransactionInput(
         String cpf,
         Long id,
         Long idempotencyId
-) implements Input {
+) implements Input, ICpfParam {
     
 }
