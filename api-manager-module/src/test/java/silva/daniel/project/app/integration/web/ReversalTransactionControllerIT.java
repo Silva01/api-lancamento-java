720aa7474d9964fa9fbf0d2002df0fa09eaaea58
silva01 <silva20102@gmail.com>
2024-05-22 06:51:02 -0300

    test: #84 Added e2e test for flux refundTransaction_WithValidData_ReturnsStatus200

---
+++ api-manager-module/src/test/java/silva/daniel/project/app/integration/web/ReversalTransactionControllerIT.java
@@ -0,0 +1,83 @@
+package silva.daniel.project.app.integration.web;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import silva.daniel.project.app.commons.IntegrationAssertCommons;
+import silva.daniel.project.app.commons.MysqlTestContainer;
+import silva.daniel.project.app.commons.RabbitMQTestContainer;
+import silva.daniel.project.app.commons.RequestIntegrationCommons;
+import silva.daniel.project.app.domain.account.request.RefundRequest;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("e2e")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/delete_client.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+@Sql(scripts = {"/sql/import_client.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, config = @SqlConfig(errorMode = SqlConfig.ErrorMode.CONTINUE_ON_ERROR))
+class ReversalTransactionControllerIT extends MysqlTestContainer implements IntegrationAssertCommons, RabbitMQTestContainer {
+
+    private static final String API_REFUND_TRANSACTION = "/api/transaction/refund";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private RequestIntegrationCommons requestCommons;
+
+    private Channel channel;
+
+    private static final String QUEUE_NAME = "transaction-reversal-queue";
+
+    @BeforeEach
+    void setUp() throws IOException, TimeoutException {
+        final var factory = new ConnectionFactory();
+
+        factory.setHost(RABBIT_MQ_CONTAINER.getHost());
+        factory.setPort(RABBIT_MQ_CONTAINER.getAmqpPort());
+        factory.setUsername(RABBIT_MQ_CONTAINER.getAdminUsername());
+        factory.setPassword(RABBIT_MQ_CONTAINER.getAdminPassword());
+
+        final var connection = factory.newConnection();
+        this.channel = connection.createChannel();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException, TimeoutException {
+        channel.close();
+    }
+
+    @Test
+    void refundTransaction_WithValidData_ReturnsStatus200() throws IOException {
+        final var request = new RefundRequest("12345678901", 1L, 2222L);
+        final var result = restTemplate.postForEntity(API_REFUND_TRANSACTION, request, Void.class);
+
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        AMQP.Queue.DeclareOk queueDeclare = channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        assertThat(queueDeclare.getMessageCount()).isEqualTo(1);
+
+        GetResponse response = channel.basicGet(QUEUE_NAME, false);
+        assertThat(response).isNotNull();
+
+        final var sut = new String(response.getBody(), StandardCharsets.UTF_8);
+        assertThat(sut).contains(request.cpf());
+        assertThat(sut).contains(request.transactionId().toString());
+        assertThat(sut).contains(request.idempotencyId().toString());
+    }
+}
