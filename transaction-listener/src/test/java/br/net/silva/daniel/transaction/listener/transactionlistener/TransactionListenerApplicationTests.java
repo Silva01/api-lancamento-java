01c22571352b38a43ff6809976ff567d363b42b7
silva01 <silva20102@gmail.com>
2024-06-01 09:58:45 -0300

    feat: #90 Added new structure for transaction listener

---
+++ transaction-listener/src/test/java/br/net/silva/daniel/transaction/listener/transactionlistener/TransactionListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.silva.daniel.transaction.listener.transactionlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
