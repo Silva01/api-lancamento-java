369ec09a1394732dd74ae9ec92d58f32b2c64e33
silva01 <silva20102@gmail.com>
2024-06-01 10:00:29 -0300

    refactor: #90 Refactored structure of project

--- transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/TransactionListenerApplication.java
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/TransactionListenerApplication.java
@@ -1,4 +1,4 @@
-package br.net.silva.daniel.transaction.listener.transactionlistener;
+package br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- transaction-listener/src/test/java/br/net/silva/daniel/transaction/listener/transactionlistener/TestTransactionListenerApplication.java
+++ transaction-listener/src/test/java/br/net/silva/daniel/transaction/listener/transactionlistener/TestTransactionListenerApplication.java
@@ -1,5 +1,6 @@
 package br.net.silva.daniel.transaction.listener.transactionlistener;
 
+import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.TransactionListenerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
