2972e6f7c9ac60254973a23ff8f3c00c84a97097
silva01 <silva20102@gmail.com>
2024-06-03 06:36:06 -0300

    refactor: #90 Refactored Account Validation

--- transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/validation/AccountValidation.java
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/validation/AccountValidation.java
@@ -1,14 +1,14 @@
 package br.net.silva.daniel.transaction.listener.transactionlistener.domain.validation;
 
 import br.net.silva.business.exception.AccountNotExistsException;
-import br.net.silva.business.value_object.output.AccountOutput;
 import br.net.silva.daniel.shared.business.exception.GenericException;
+import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.model.Account;
 
 import java.util.Optional;
 
-public class AccountValidation implements Validation<Optional<AccountOutput>> {
+public class AccountValidation implements Validation<Optional<Account>> {
     @Override
-    public void validate(Optional<AccountOutput> accountOutput) throws GenericException {
+    public void validate(Optional<Account> accountOutput) throws GenericException {
         if (accountOutput.isEmpty()) {
             throw new AccountNotExistsException("Account not found");
         }
