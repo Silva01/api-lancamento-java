e5f6654d041ed6cbe919bef957d5dfe6013ddb04
silva01 <silva20102@gmail.com>
2024-06-03 07:17:16 -0300

    refactor: #90 Added validation for account deactivate

--- transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/validation/AccountValidation.java
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/validation/AccountValidation.java
@@ -1,5 +1,6 @@
 package br.net.silva.daniel.transaction.listener.transactionlistener.domain.validation;
 
+import br.net.silva.business.exception.AccountDeactivatedException;
 import br.net.silva.business.exception.AccountNotExistsException;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.model.Account;
@@ -7,10 +8,23 @@ import br.net.silva.daniel.transaction.listener.transactionlistener.infraestruct
 import java.util.Optional;
 
 public class AccountValidation implements Validation<Optional<Account>> {
+
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
     @Override
     public void validate(Optional<Account> accountOutput) throws GenericException {
+        accountExists(accountOutput);
+        isAccountActive(accountOutput.get());
+    }
+
+    private static void accountExists(Optional<Account> accountOutput) throws AccountNotExistsException {
         if (accountOutput.isEmpty()) {
             throw new AccountNotExistsException("Account not found");
+        }
+    }
+
+    private static void isAccountActive(Account account) throws AccountDeactivatedException {
+        if (!account.isActive()) {
+            throw new AccountDeactivatedException("Account is not active");
         }
     }
 }
