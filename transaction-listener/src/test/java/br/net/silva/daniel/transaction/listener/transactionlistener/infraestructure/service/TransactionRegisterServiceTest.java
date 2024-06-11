f8ab45c5f7f06d7e63f68247d0060e65755bde59
silva01 <silva20102@gmail.com>
2024-06-11 05:54:46 -0300

    test: #90 Added unit test for flux registerTransaction_WithTransactionsDuplicated_ThrowsTransactionDuplicateException

--- transaction-listener/src/test/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/service/TransactionRegisterServiceTest.java
+++ transaction-listener/src/test/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/service/TransactionRegisterServiceTest.java
@@ -42,7 +42,7 @@ class TransactionRegisterServiceTest {
     void registerTransaction_WithValidData_RegisterWithSuccess() {
         // Arrange
         when(accountRepository.findByAccountNumberAndAgencyAndCpf(anyInt(), anyInt(), anyString())).thenReturn(Optional.of(generateMockAccount(true, BigDecimal.ONE)));
-        final var message = createMockMessageRequest();
+        final var message = createMockMessageRequest(generateMockTransactions());
 
         // Act
         final var response = service.registerTransaction(message);
@@ -62,7 +62,7 @@ class TransactionRegisterServiceTest {
     @Test
     void registerTransaction_WithSourceAccountNotExists_ThrowsAccountNotExistsException() {
         when(accountRepository.findByAccountNumberAndAgencyAndCpf(anyInt(), anyInt(), ArgumentMatchers.anyString())).thenReturn(Optional.empty());
-        final var message = createMockMessageRequest();
+        final var message = createMockMessageRequest(generateMockTransactions());
 
         final var sut = service.registerTransaction(message);
         assertThat(sut).isNotNull();
@@ -76,7 +76,7 @@ class TransactionRegisterServiceTest {
                 .thenReturn(Optional.of(generateMockAccount(true, BigDecimal.ONE)))
                 .thenReturn(Optional.empty());
 
-        final var message = createMockMessageRequest();
+        final var message = createMockMessageRequest(generateMockTransactions());
 
         final var sut = service.registerTransaction(message);
         assertThat(sut).isNotNull();
@@ -88,7 +88,7 @@ class TransactionRegisterServiceTest {
     void registerTransaction_WithSourceAccountDeactivated_ThrowsAccountDeactivateException() {
         when(accountRepository.findByAccountNumberAndAgencyAndCpf(anyInt(), anyInt(), ArgumentMatchers.anyString()))
                 .thenReturn(Optional.of(generateMockAccount(false, BigDecimal.ONE)));
-        final var message = createMockMessageRequest();
+        final var message = createMockMessageRequest(generateMockTransactions());
 
         final var sut = service.registerTransaction(message);
         assertThat(sut).isNotNull();
@@ -102,7 +102,7 @@ class TransactionRegisterServiceTest {
                 .thenReturn(Optional.of(generateMockAccount(true, BigDecimal.ONE)))
                 .thenReturn(Optional.of(generateMockAccount(false, BigDecimal.ONE)));
 
-        final var message = createMockMessageRequest();
+        final var message = createMockMessageRequest(generateMockTransactions());
 
         final var sut = service.registerTransaction(message);
         assertThat(sut).isNotNull();
@@ -115,7 +115,7 @@ class TransactionRegisterServiceTest {
         when(accountRepository.findByAccountNumberAndAgencyAndCpf(anyInt(), anyInt(), ArgumentMatchers.anyString()))
                 .thenReturn(Optional.of(generateMockAccount(true, BigDecimal.ZERO)));
 
-        final var message = createMockMessageRequest();
+        final var message = createMockMessageRequest(generateMockTransactions());
 
         final var sut = service.registerTransaction(message);
         assertThat(sut).isNotNull();
@@ -123,14 +123,32 @@ class TransactionRegisterServiceTest {
         assertThat(sut.message()).isEqualTo("Account 123 and agency 1: Insufficient balance");
     }
 
-    private BatchTransactionInput createMockMessageRequest() {
+    @Test
+    void registerTransaction_WithTransactionsDuplicated_ThrowsTransactionDuplicateException() {
+        when(accountRepository.findByAccountNumberAndAgencyAndCpf(anyInt(), anyInt(), anyString())).thenReturn(Optional.of(generateMockAccount(true, BigDecimal.valueOf(1000))));
+        final var message = createMockMessageRequest(generateMockDuplicatedTransactions());
+
+        final var sut = service.registerTransaction(message);
+        assertThat(sut).isNotNull();
+        assertThat(sut.status()).isEqualTo(ResponseStatus.ERROR);
+        assertThat(sut.message()).isEqualTo("Transaction has 2 or more equals transactions.");
+    }
+
+    private BatchTransactionInput createMockMessageRequest(List<TransactionInput> transactions) {
         final var sourceAccount = new AccountInput(123, 1, "55544433322");
         final var destinyAccount = new AccountInput(456, 2, "55544433300");
-        return new BatchTransactionInput(sourceAccount, destinyAccount, generateMockTransactions());
+        return new BatchTransactionInput(sourceAccount, destinyAccount, transactions);
     }
 
     private List<TransactionInput> generateMockTransactions() {
         return List.of(
+                new TransactionInput(1L, "Test", BigDecimal.ONE, 1, DEBIT, 123L, null, null)
+        );
+    }
+
+    private List<TransactionInput> generateMockDuplicatedTransactions() {
+        return List.of(
+                new TransactionInput(1L, "Test", BigDecimal.ONE, 1, DEBIT, 123L, null, null),
                 new TransactionInput(1L, "Test", BigDecimal.ONE, 1, DEBIT, 123L, null, null)
         );
     }
