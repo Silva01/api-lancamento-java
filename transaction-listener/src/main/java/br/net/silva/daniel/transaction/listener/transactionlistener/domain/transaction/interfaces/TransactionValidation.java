575e527ddec90359f62f6afae92acc359fb7098f
silva01 <silva20102@gmail.com>
2024-06-15 11:33:11 -0300

    refactor: #90 Created interface for config to validation

---
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/transaction/interfaces/TransactionValidation.java
@@ -0,0 +1,16 @@
+package br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.interfaces;
+
+public interface TransactionValidation {
+
+    default boolean validateIfAccountExists() {
+        return true;
+    }
+
+    default boolean validateIfAccountIsActive() {
+        return true;
+    }
+
+    default boolean validateIfBalanceIsSufficient() {
+        return true;
+    }
+}
