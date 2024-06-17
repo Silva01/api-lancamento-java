810022c97ced9ccadc4c88d6c355486771b4d9d8
silva01 <silva20102@gmail.com>
2024-06-17 07:26:38 -0300

    refactor: #90 Refactored IValidation for use BaseAccountAggregate

--- transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/transaction/interfaces/IValidation.java
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/domain/transaction/interfaces/IValidation.java
@@ -1,11 +1,11 @@
 package br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.interfaces;
 
 import br.net.silva.daniel.shared.business.exception.GenericException;
-import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.value_object.ValidatorConfigurator;
+import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.aggregate.BaseAccountAggregate;
 
 public interface IValidation {
 
     boolean isExecute(TransactionValidation transactionValidation);
 
-    void executeValidation(ValidatorConfigurator configurator) throws GenericException;
+    void executeValidation(BaseAccountAggregate configurator) throws GenericException;
 }
