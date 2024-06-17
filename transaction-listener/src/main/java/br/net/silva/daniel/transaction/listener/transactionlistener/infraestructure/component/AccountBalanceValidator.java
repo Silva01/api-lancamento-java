7c6cb56b60c2cb171fab4adba622f33eb98fd544
silva01 <silva20102@gmail.com>
2024-06-17 07:25:02 -0300

    refactor: #90 Renamed type attribute of constructor to BaseAccountAggregate

--- transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/component/AccountBalanceValidator.java
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/component/AccountBalanceValidator.java
@@ -4,7 +4,7 @@ import br.net.silva.business.exception.AccountBalanceInsufficientException;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.interfaces.IValidation;
 import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.interfaces.TransactionValidation;
-import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.value_object.ValidatorConfigurator;
+import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.aggregate.BaseAccountAggregate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +15,7 @@ public class AccountBalanceValidator implements IValidation {
     }
 
     @Override
-    public void executeValidation(ValidatorConfigurator configurator) throws GenericException {
+    public void executeValidation(BaseAccountAggregate configurator) throws GenericException {
         if (configurator.accountConfigValidation().balance().compareTo(configurator.totalTransaction()) < 0) {
             throw new AccountBalanceInsufficientException("Insufficient balance");
         }
