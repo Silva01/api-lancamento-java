2564bab2dc9f3ac74de6443c21ffc333e3bfedf3
silva01 <silva20102@gmail.com>
2024-06-20 06:00:41 -0300

    refactor: #90 Added new method for save transactions

--- transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/service/TransactionRegisterService.java
+++ transaction-listener/src/main/java/br/net/silva/daniel/transaction/listener/transactionlistener/infraestructure/service/TransactionRegisterService.java
@@ -2,6 +2,7 @@ package br.net.silva.daniel.transaction.listener.transactionlistener.infraestruc
 
 import br.net.silva.business.value_object.input.AccountInput;
 import br.net.silva.business.value_object.input.BatchTransactionInput;
+import br.net.silva.business.value_object.input.TransactionInput;
 import br.net.silva.daniel.shared.business.exception.GenericException;
 import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.aggregate.BaseAccountAggregate;
 import br.net.silva.daniel.transaction.listener.transactionlistener.domain.transaction.value_object.RegisterResponse;
@@ -10,10 +11,13 @@ import br.net.silva.daniel.transaction.listener.transactionlistener.infraestruct
 import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.enuns.ResponseStatus;
 import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.factory.ValidatorFactory;
 import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.model.Account;
+import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.model.Transaction;
 import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.repository.AccountRepository;
+import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.repository.TransactionRepository;
 import br.net.silva.daniel.transaction.listener.transactionlistener.infraestructure.utils.ExtractorUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,8 +27,10 @@ import java.util.Optional;
 public class TransactionRegisterService {
 
     private final AccountRepository repository;
+    private final TransactionRepository transactionRepository;
     private final ValidationHandler validationHandler;
 
+    @Transactional
     public RegisterResponse registerTransaction(BatchTransactionInput message) {
         final var sourceAccount = findAccountFrom(message.sourceAccount());
         final var destinyAccount = findAccountFrom(message.destinyAccount());
@@ -34,9 +40,8 @@ public class TransactionRegisterService {
             final var destinyAccountAggregate = ValidatorFactory.createValidatorConfiguratorForDestinyAccount(message, destinyAccount);
             validationHandler.executeValidations(List.of(sourceAccountAggregate, destinyAccountAggregate));
 
-            //TODO: Ao salvar o saldo calculado, é necessário gravar a transação no banco de dados
-            registerTransaction(sourceAccountAggregate);
-            registerTransaction(destinyAccountAggregate);
+            registerTransaction(List.of(sourceAccountAggregate, destinyAccountAggregate));
+            saveTransactions(message, sourceAccountAggregate, destinyAccountAggregate);
 
             return new RegisterResponse(
                     ResponseStatus.SUCCESS,
@@ -60,6 +65,10 @@ public class TransactionRegisterService {
         }
     }
 
+    private void registerTransaction(List<BaseAccountAggregate> aggregates) {
+        aggregates.forEach(this::registerTransaction);
+    }
+
     private void registerTransaction(BaseAccountAggregate aggregate) {
         final var account = ExtractorUtils.accountExtractor(aggregate);
         account.setBalance(aggregate.calculateBalance());
@@ -68,5 +77,27 @@ public class TransactionRegisterService {
 
     private Optional<Account> findAccountFrom(AccountInput accountInput) {
         return repository.findByAccountNumberAndAgencyAndCpf(accountInput.accountNumber(), accountInput.agency(), accountInput.cpf());
+    }
+
+    private void saveTransactions(BatchTransactionInput message, BaseAccountAggregate sourceAggregate, BaseAccountAggregate destinyAggregate) {
+        final var sourceAccount = ExtractorUtils.accountExtractor(sourceAggregate);
+        final var destinyAccount = ExtractorUtils.accountExtractor(destinyAggregate);
+        transactionRepository.saveAll(buildTransactions(message.batchTransaction(), sourceAccount, destinyAccount));
+    }
+
+    private List<Transaction> buildTransactions(List<TransactionInput> transactions, Account sourceAccount, Account destinyAccount) {
+        return transactions.stream()
+                .map(transaction -> new Transaction(
+                        null,
+                        transaction.description(),
+                        transaction.price(),
+                        transaction.quantity(),
+                        transaction.type(),
+                        sourceAccount,
+                        destinyAccount,
+                        transaction.idempotencyId(),
+                        null
+                ))
+                .toList();
     }
 }
